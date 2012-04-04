32c9e407edf5cfefdabf7f5325e8c0377d9326a2
Miklos Maroti <mmaroti@gmail.com>
2012-04-04 09:19:53 +0200

    reworked the treemap

--- corejs/src-old/treemap.js
+++ corejs/src-old/treemap.js
@@ -4,52 +4,52 @@
  * Author: Miklos Maroti
  */
 
-define([ "assert" ], function (ASSERT) {
+define([ "assert" ], function(ASSERT) {
 	"use strict";
 
 	// ----------------- node -----------------
 
-	var Node = function (parent, relid) {
+	var Node = function(parent, relid) {
 		ASSERT(parent instanceof Node);
 		ASSERT(typeof relid === "string");
 
-		// create non-enumerable  properties
+		// create non-enumerable properties
 		Object.defineProperties(this, {
-			parent: {
-				value: parent
+			parent : {
+				value : parent
 			},
-			relid: {
-				value: relid
+			relid : {
+				value : relid
 			},
-			level: {
-				value: parent.level + 1
+			level : {
+				value : parent.level + 1
 			},
 			refcount : {
-				value: 0,
-				writable: true
+				value : 0,
+				writable : true
 			}
 		});
 	};
 
 	var emptyNodes = [];
-	
+
 	var increaseRefcount = function(node) {
-		if( ++node.refcount === 1 ) {
+		if (++node.refcount === 1) {
 			var index = emptyNodes.indexOf(this);
 			ASSERT(index >= 0);
 			emptyNodes.splice(index, 1);
 		}
 	};
-	
+
 	// create non-enumerable methods
 	Object.defineProperties(Node.prototype, {
 		/**
 		 * The leaf property is true if the node has no children
 		 */
-		leaf: {
-			get: function () {
+		leaf : {
+			get : function() {
 				var relid;
-				for( relid in this ) {
+				for (relid in this) {
 					return false;
 				}
 				return true;
@@ -57,21 +57,21 @@ define([ "assert" ], function (ASSERT) {
 		},
 
 		/**
-		 * The empty property is true if the node has no children
-		 * and has no associated data
+		 * The empty property is true if the node has no children and has no
+		 * associated data
 		 */
-		empty: {
-			get: function () {
+		empty : {
+			get : function() {
 				return this.refcount === 0;
 			}
 		},
-		
+
 		/**
 		 * The path property contains the path of this node. This is a slow
 		 * method, you should not use it.
 		 */
-		path: {
-			get: function () {
+		path : {
+			get : function() {
 				return this.parent ? this.parent.path + "/" + this.relid : "";
 			}
 		},
@@ -80,14 +80,14 @@ define([ "assert" ], function (ASSERT) {
 		 * Returns the child with the given relid. If that child does not exist,
 		 * then it is created.
 		 */
-		child: {
-			value: function (relid) {
+		child : {
+			value : function(relid) {
 				ASSERT(typeof relid === "string");
 
 				var node = this[relid];
 				ASSERT(node === undefined || node instanceof Node);
 
-				if( !node ) {
+				if (!node) {
 					node = new Node(this, relid);
 
 					this[relid] = node;
@@ -99,47 +99,46 @@ define([ "assert" ], function (ASSERT) {
 		}
 	});
 
-	// ----------------- tree -----------------
+	// ----------------- treemap -----------------
 
 	/**
 	 * Special root object with no relid
 	 */
 	var root = Object.create(Node.prototype, {
-		parent: {
-			value: null
+		parent : {
+			value : null
 		},
-		level: {
-			value: 0
+		level : {
+			value : 0
 		},
-		refcount: {
-			value: 1,
-			writable: true
+		refcount : {
+			value : 1,
+			writable : true
 		}
 	});
 
-	var unusedTreeIds = [];
-	var treeIdCount = 0;
+	var unusedIds = [];
+	var idCount = 0;
 
-	var Tree = function () {
-		this.id = unusedTreeIds.length !== 0 ? unusedTreeIds.pop() : "#"
-		+ treeIdCount++;
+	var Treemap = function() {
+		this.id = unusedIds.length !== 0 ? unusedIds.pop() : "#" + idCount++;
 	};
 
 	/**
-	 * Removes all data associated with this tree and destroys this tree object.
+	 * Removes all data associated with this treemap and destroys this tree
+	 * object.
 	 */
-	Tree.prototype.destroy = function () {
-		this.clear();
+	Treemap.prototype.destroy = function() {
+		this.clearAll();
 
-		unusedTreeIds.push(this.id);
+		unusedIds.push(this.id);
 		delete this.id;
 	};
 
 	/**
 	 * Removes all node data associated with this tree object
 	 */
-	Tree.prototype.clearAll = function () {
-		ASSERT(this.id);
+	Treemap.prototype.clearAll = function() {
 		this.deleteData(root);
 	};
 
@@ -147,7 +146,7 @@ define([ "assert" ], function (ASSERT) {
 	 * Returns the node at the given path. This is a slow method, you should not
 	 * use it.
 	 */
-	Tree.prototype.getNode = function (path) {
+	Treemap.prototype.getNode = function(path) {
 		ASSERT(this.id);
 		ASSERT(typeof path === "string");
 
@@ -155,47 +154,29 @@ define([ "assert" ], function (ASSERT) {
 		ASSERT(relids[0] === "");
 
 		var node = root;
-		for( var i = 1; node && i < relids.length; ++i ) {
+		for ( var i = 1; node && i < relids.length; ++i) {
 			node = node.child(relids[i]);
 		}
 
 		return node;
 	};
 
-	Tree.prototype.hasData = function (node) {
-		ASSERT(this.id);
-		ASSERT(node instanceof Node);
+	Treemap.prototype.hasData = function(node) {
+		ASSERT(this.id && node instanceof Node);
 
 		return node.hasOwnProperty(this.id);
 	};
 
-	/**
-	 * Adds an empty data object to this node and its ancestors.
-	 */
-	var addEmptyData = function (node, id) {
-		while( node && !node.hasOwnProperty(id) ) {
-			Object.defineProperty(node, id, {
-				value: {},
-				writable: true,
-				configurable: true
-			});
-			node = node.parent;
-		}
-	};
+	Treemap.prototype.setData = function(node, data) {
+		ASSERT(this.id && node instanceof Node);
+		ASSERT(!node.parent || this.hasData(node.parent));
 
-	Tree.prototype.setData = function (node, data) {
-		ASSERT(this.id);
-		ASSERT(node instanceof Node);
-
-		addEmptyData(node, this.id);
 		node[this.id] = data;
 	};
 
-	Tree.prototype.getData = function (node) {
-		ASSERT(this.id);
-		ASSERT(node instanceof Node);
+	Treemap.prototype.getData = function(node) {
+		ASSERT(this.id && node instanceof Node);
 
-		addEmptyData(node, this.id);
 		return node[this.id];
 	};
 
@@ -203,14 +184,13 @@ define([ "assert" ], function (ASSERT) {
 	 * Recursively deletes data associated with this node and all of its
 	 * descendants.
 	 */
-	Tree.prototype.deleteData = function (node) {
-		ASSERT(this.id);
-		ASSERT(node instanceof Node);
+	Treemap.prototype.deleteData = function(node) {
+		ASSERT(this.id && node instanceof Node);
 
-		if( node.hasOwnProperty(this.id) ) {
+		if (node.hasOwnProperty(this.id)) {
 			delete node[this.id];
 
-			for( var relid in node ) {
+			for ( var relid in node) {
 				this.deleteData(node[relid]);
 			}
 		}
@@ -218,5 +198,5 @@ define([ "assert" ], function (ASSERT) {
 
 	// ----------------- public interface -----------------
 
-	return Tree;
+	return Treemap;
 });
---
+++ corejs/src/treemap.js
@@ -0,0 +1,243 @@
+/*
+ * Copyright (C) 2012 Vanderbilt University, All rights reserved.
+ * 
+ * Author: Miklos Maroti
+ */
+
+define([ "assert" ], function(ASSERT) {
+	"use strict";
+
+	// ----------------- node -----------------
+
+	var Node = function(parent, relid, level) {
+		ASSERT(parent === null || parent instanceof Node);
+		ASSERT(typeof relid === "string");
+
+		this.parent = parent;
+		this.relid = relid;
+		this.level = level;
+		this.refcount = 0;
+		this.children = {};
+		
+		addEmptyNode(this);
+	};
+
+	var emptyNodes = undefined;
+	
+	var removeEmptyNodes = function() {
+		ASSERT(emptyNodes);
+		
+		while(emptyNodes.length) {
+			var node = emptyNodes.pop();
+			ASSERT( node.refcount >= 0);
+			
+			if( !node.refcount ) {
+				ASSERT(node.leaf);
+				ASSERT(node.parent.children[node.relid] === node);
+
+				delete node.parent.children[node.relid];
+				delete node.parent;
+			}
+		}
+		
+		emptyNodes = undefined;
+	};
+	
+	var addEmptyNode = function(node) {
+		ASSERT(node instanceof Node);
+		ASSERT(node.refcount === 0);
+		
+		if( emptyNodes ) {
+			emptyNodes.push(node);
+		}
+		else {
+			emptyNodes = [node];
+			setTimeout(removeEmptyNodes, 0);
+		}
+	};
+
+	// create getter methods
+	Object.defineProperties(Node.prototype, {
+		/**
+		 * The leaf property is true if the node has no children
+		 */
+		leaf : {
+			get : function() {
+				var relid;
+				for (relid in this.children) {
+					return false;
+				}
+				return true;
+			}
+		},
+
+		/**
+		 * The empty property is true if the node has no associated data
+		 */
+		empty : {
+			get : function() {
+				return this.refcount === 0;
+			}
+		},
+
+		/**
+		 * The path property contains the path of this node. This is a slow
+		 * method, you should not use it.
+		 */
+		path : {
+			get : function() {
+				return this.parent ? this.parent.path + "/" + this.relid : "";
+			}
+		}
+	});
+
+	Node.prototype.getChild = function(relid) {
+		ASSERT(typeof relid === "string");
+
+		var node = this.children[relid];
+		ASSERT(node === undefined || node instanceof Node);
+
+		if (!node) {
+			node = new Node(this, relid, this.level + 1);
+
+			this.children[relid] = node;
+		}
+
+		return node;
+	};
+
+	// ----------------- treemap -----------------
+
+	/**
+	 * Special root object with no relid
+	 */
+	var root = new Node(null, "", 0);
+	root.refcount = 1;
+
+	var unusedIds = [];
+	var idCount = 0;
+
+	var Treemap = function() {
+		this.id = unusedIds.length !== 0 ? unusedIds.pop() : "+" + idCount++;
+		this.root = root;
+	};
+
+	/**
+	 * Removes all data associated with this treemap and destroys this tree
+	 * object.
+	 */
+	Treemap.prototype.destroy = function() {
+		this.clearAll();
+
+		unusedIds.push(this.id);
+		delete this.id;
+	};
+
+	/**
+	 * Removes all node data associated with this tree object
+	 */
+	Treemap.prototype.clearAll = function() {
+		this.deleteData(root);
+	};
+
+	/**
+	 * Returns the node at the given path. This is a slow method, you should not
+	 * use it.
+	 */
+	Treemap.prototype.getNode = function(path) {
+		ASSERT(this.id);
+		ASSERT(typeof path === "string");
+
+		var relids = path.split("/");
+		ASSERT(relids[0] === "");
+
+		var node = root;
+		for ( var i = 1; node && i < relids.length; ++i) {
+			node = node.getChild(relids[i]);
+		}
+
+		return node;
+	};
+
+	Treemap.prototype.hasData = function(node) {
+		ASSERT(this.id && node instanceof Node);
+
+		return node.hasOwnProperty(this.id);
+	};
+
+	Treemap.prototype.setData = function(node, data) {
+		ASSERT(this.id && node instanceof Node);
+		ASSERT(!node.parent || this.hasData(node.parent));
+
+		if( !node.hasOwnProperty(this.id) ) {
+			++node.refcount;
+		}
+		
+		node[this.id] = data;
+	};
+
+	Treemap.prototype.getData = function(node) {
+		ASSERT(this.id && node instanceof Node);
+
+		return node[this.id];
+	};
+
+	/**
+	 * Recursively deletes data associated with this node and all of its
+	 * descendants.
+	 */
+	Treemap.prototype.deleteData = function(node) {
+		ASSERT(this.id && node instanceof Node);
+
+		if (node.hasOwnProperty(this.id)) {
+			delete node[this.id];
+
+			// we have to push parent empty nodes first
+			if( --node.refcount === 0 ) {
+				
+			}
+
+			for ( var relid in node.children) {
+				this.deleteData(node.children[relid]);
+			}
+		}
+	};
+
+	var stringifyNode = function(node, id) {
+		ASSERT(node instanceof Node);
+		ASSERT(node.hasOwnProperty(id));
+
+		var c = "";
+		for ( var relid in node.children) {
+			var child = node.children[relid];
+			if (child.hasOwnProperty(id)) {
+				c += (c ? ",'" : "'") + relid + "':"
+						+ stringifyNode(child, id);
+			}
+		}
+		
+		if(c) {
+			c = ",children:{" + c + "}";
+		}
+		
+		return "{data:" + JSON.stringify(node[id]) + c + "}";
+	};
+
+	/**
+	 * Returns the JSON representation of this tree. 
+	 */
+	Treemap.prototype.saveToJson = function() {
+		ASSERT(this.id);
+
+		if(root.hasOwnProperty(this.id)) {
+			return stringifyNode(root, this.id);
+		}
+		else {
+			return "{}";
+		}
+	};
+
+	// ----------------- public interface -----------------
+
+	return Treemap;
+});
