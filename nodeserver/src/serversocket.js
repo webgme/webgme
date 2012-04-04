c2118d27199e5bfb67b8e45c8ed0c43be1e9e92e
kecso <tkecskes@isis.vanderbilt.edu>
2012-04-04 09:46:46 -0500

    error have been 'dirty' corrected that during deletion of opened node the server freeze...

--- nodeserver/src/serversocket.js
+++ nodeserver/src/serversocket.js
@@ -352,15 +352,19 @@ SocketEnhanced = function(_socket,_readstorage){
 
     /*private functions*/
     var insertIntoArray = function(list,item){
+        if (list instanceof Array){
             if(list.indexOf(item) === -1){
                 list.push(item);
             }
+        }
     };
     var removeFromArray = function(list,item){
+        if (list instanceof Array){
             var position = list.indexOf(item);
             if(position !== -1){
                 list.splice(position,1);
             }
+        }
     };
     var deepCopyObject = function(parentid,tocopyid){
         var newobj = {};
