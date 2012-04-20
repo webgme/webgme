bcb7dd353e8155554b3e871528cb05fb3701010c
RobertK <robert.kereskenyi@gmail.com>
2012-04-20 15:58:54 -0500

    Introducing CommonUtil.js for common variables for cliend and server side

---
+++ nodeserver/src/common/CommonUtil.js
@@ -0,0 +1,40 @@
+/*
+ * Utility helper functions for the client and server side
+ */
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+
+define( [], function(){
+
+    //return utility functions
+    return {
+        /*
+         * Determines if the app runs in debug or release mode
+         */
+        DEBUG : false,
+
+        /*
+         * Define the server to connect to
+         * Value is either IP of the host, or "self" for connecting to the host where the page loaded from
+         */
+        ServerIP : "self",
+
+        /*
+         * Port number of socket.io server
+         */
+        ServerPort : 8081,
+
+        /*
+         * Generated a GUID
+         */
+        guid: function() {
+            var S4 = function() {
+                return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+            };
+
+            //return GUID
+            return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+        }
+    };
+});
