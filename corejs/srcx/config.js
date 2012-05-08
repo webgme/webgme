ec51a36c7089ec461003b39ab309a3d8492fb63f
Miklos Maroti <mmaroti@isis.vanderbilt.edu>
2012-05-08 17:56:49 -0500

    nodejs work

---
+++ corejs/srcx/config.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2012 Vanderbilt University, All rights reserved.
+ * 
+ * Author: Miklos Maroti
+ */
+
+define(function () {
+	"use strict";
+
+	return {
+		mongodb: {
+			host: "localhost",
+			port: 27017
+		}
+	};
+});
