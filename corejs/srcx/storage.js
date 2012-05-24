9b91f4bb5615848521a2122fc7e3047922639604
Miklos Maroti <mmaroti@isis.vanderbilt.edu>
2012-05-23 20:40:12 -0500

    storage work

--- corejs/srcx/storage.js
+++ corejs/srcx/storage.js
@@ -58,14 +58,19 @@ define([ "assert", "mongodb", "sha1", "config" ], function (ASSERT, MONGODB, SHA
 		};
 
 		this.getKey = function (node) {
+			ASSERT(node && typeof node === "object");
 			return node._id;
 		};
 
 		this.setKey = function (node, key) {
-			ASSERT(node);
-			ASSERT(typeof key === "string");
+			ASSERT(node && typeof node === "object");
+			ASSERT(key === false || typeof key === "string");
 
-			node._id = key;
+			Object.defineProperty(node, "_id", {
+				value: key,
+				writable: true,
+				enumerable: true
+			});
 		};
 
 		this.load = function (key, callback) {
@@ -86,7 +91,8 @@ define([ "assert", "mongodb", "sha1", "config" ], function (ASSERT, MONGODB, SHA
 		};
 
 		this.save = function (node, callback) {
-			ASSERT(node);
+			ASSERT(node && typeof node === "object");
+			ASSERT(typeof node._id === "string");
 			ASSERT(collection && callback);
 
 			collection.save(node, callback);
@@ -204,7 +210,7 @@ define([ "assert", "mongodb", "sha1", "config" ], function (ASSERT, MONGODB, SHA
 
 		this.persist = function (node, callback) {
 			ASSERT(node && callback);
-			
+
 			var saver = new Saver(callback);
 			saver.save(node);
 			saver.done(null);
@@ -292,42 +298,42 @@ define([ "assert", "mongodb", "sha1", "config" ], function (ASSERT, MONGODB, SHA
 		};
 
 		/**
-		 * Nodes can become invalid in two ways. 1) We request the same
-		 * child of a parent twice and modify one of them, then the other 
-		 * will not reflect the same modified data but the stale old 
-		 * data. 2) We could remove nodes or change branches completely
-		 * which creates dangling nodes.
+		 * Nodes can become invalid in two ways. 1) We request the same child of
+		 * a parent twice and modify one of them, then the other will not
+		 * reflect the same modified data but the stale old data. 2) We could
+		 * remove nodes or change branches completely which creates dangling
+		 * nodes.
 		 */
 		this.isValid = function (node) {
 			ASSERT(node && node.data);
 
-			while(node.parent) {
+			while( node.parent ) {
 				ASSERT(typeof node.relid === "string" && node.parent.data);
 				var child = graph.getChildren(node.parent.data)[node.relid];
-				
+
 				if( typeof child === "string" ) {
 					ASSERT(child === graph.getKey(node.data));
 				}
 				else if( child !== node.data ) {
 					return false;
 				}
-				
+
 				node = node.parent;
 			}
-			
+
 			return true;
 		};
-		
+
 		this.mutate = function (node) {
 			ASSERT(this.isValid(node));
 
 			if( !graph.isMutable(node.data) ) {
 				node.data = graph.mutate(node.data);
-				
-				while(node.parent && !graph.isMutable(node.parent.data) ) {
+
+				while( node.parent && !graph.isMutable(node.parent.data) ) {
 					node.parent.data = graph.mutate(node.parent.data);
 					graph.setChild(node.parent.data, node.relid, node.data);
-					
+
 					node = node.parent;
 				}
 			}
@@ -386,13 +392,535 @@ define([ "assert", "mongodb", "sha1", "config" ], function (ASSERT, MONGODB, SHA
 
 			graph.setData(node.data, name, data);
 		};
+
+		this.delData = function (node, name) {
+			ASSERT(this.isValid(node));
+			ASSERT(this.isMutable(node));
+
+			graph.delData(node.data, name);
+		};
+	};
+
+	// ----------------- Graph2 -----------------
+
+	var Graph2 = function (storage) {
+		ASSERT(storage);
+
+		this.getKey = storage.getKey;
+
+		this.isMutable = function (node) {
+			return !storage.getKey(node);
+		};
+
+		this.isLeaf = function (node) {
+			return node._leaf;
+		};
+
+		this.createNode = function () {
+			return {};
+		};
+
+		this.createLeaf = function () {
+			return {
+				_leaf: true
+			};
+		};
+
+		this.getNode = storage.load;
+
+		this.mutate = deepclone;
+
+		var Saver = function (callback) {
+			ASSERT(callback);
+
+			var counter = 1;
+			var error = null;
+
+			this.start = function () {
+				ASSERT(callback && counter >= 1);
+
+				++counter;
+			};
+
+			this.done = function (err) {
+				ASSERT(callback && counter >= 1);
+
+				error = error || err;
+				if( --counter === 0 ) {
+					callback(error);
+					callback = null;
+				}
+			};
+		};
+
+		Saver.prototype.save = function (node) {
+			var key = storage.getKey(node);
+			if( !key ) {
+				if( !node._leaf ) {
+					for( var relid in node ) {
+						var child = node[relid];
+						if( typeof child !== "string" ) {
+							child = this.save(child);
+							ASSERT(typeof child === "string");
+							node[relid] = child;
+						}
+					}
+				}
+
+				key = SHA1(JSON.stringify(node));
+
+				ASSERT(!storage.getKey(node));
+				storage.setKey(node, key);
+
+				this.start();
+				storage.save(node, this.done);
+			}
+
+			return key;
+		};
+
+		this.persist = function (node, callback) {
+			ASSERT(node && callback);
+
+			var saver = new Saver(callback);
+			saver.save(node);
+			saver.done(null);
+		};
+
+		this.getChildren = function (node) {
+			ASSERT(node && !node._leaf);
+
+			return node;
+		};
+
+		this.getChild = function (node, relid, callback) {
+			ASSERT(node && !node._leaf);
+
+			var child = node[relid];
+			if( typeof child === "string" ) {
+				storage.load(child, callback);
+			}
+			else {
+				ASSERT(child);
+				ASSERT(this.isMutable(node));
+
+				callback(null, child);
+			}
+		};
+
+		this.setChild = function (node, relid, child) {
+			ASSERT(node && !node._leaf && child);
+			ASSERT(this.isMutable(node));
+
+			node[relid] = child;
+		};
+
+		this.delChild = function (node, relid) {
+			ASSERT(node && !node._leaf);
+			ASSERT(this.isMutable(node));
+
+			delete node[relid];
+		};
+
+		this.getData = function (node, name) {
+			ASSERT(node && node._leaf);
+			ASSERT(typeof name === "string");
+
+			return node[name];
+		};
+
+		this.setData = function (node, name, data) {
+			ASSERT(node && node._leaf);
+			ASSERT(this.isMutable(node));
+			ASSERT(typeof name === "string");
+
+			node[name] = data;
+		};
+
+		this.delData = function (node, name) {
+			ASSERT(node && node._leaf);
+			ASSERT(this.isMutable(node));
+			ASSERT(typeof name === "string");
+
+			delete node[name];
+		};
+	};
+
+	// ----------------- Tree2 -----------------
+
+	var Tree2 = function (graph) {
+		ASSERT(graph);
+
+		this.isMutable = function (node) {
+			ASSERT(node && node.data);
+			return graph.isMutable(node.data);
+		};
+
+		this.isLeaf = function (node) {
+			ASSERT(node && node.data);
+			return graph.isLeaf(node.data);
+		};
+
+		this.getRoot = function (key, callback) {
+			ASSERT(callback);
+
+			graph.getNode(key, function (err, data) {
+				callback(err, err ? undefined : {
+					data: data,
+					parent: null
+				});
+			});
+		};
+
+		/**
+		 * Nodes can become invalid in two ways. 1) We request the same child of
+		 * a parent twice and modify one of them, then the other will not
+		 * reflect the same modified data but the stale old data. 2) We could
+		 * remove nodes or change branches completely which creates dangling
+		 * nodes.
+		 */
+		this.isValid = function (node) {
+			ASSERT(node && node.data);
+
+			while( node.parent ) {
+				ASSERT(typeof node.relid === "string" && !graph.isLeaf(node.parent.data));
+				var child = node.parent.data[node.relid];
+
+				if( typeof child === "string" ) {
+					ASSERT(child === graph.getKey(node.data));
+				}
+				else if( child !== node.data ) {
+					return false;
+				}
+
+				node = node.parent;
+			}
+
+			return true;
+		};
+
+		this.mutate = function (node) {
+			ASSERT(this.isValid(node));
+
+			if( !graph.isMutable(node.data) ) {
+				node.data = graph.mutate(node.data);
+
+				while( node.parent && !graph.isMutable(node.parent.data) ) {
+					node.parent.data = graph.mutate(node.parent.data);
+					graph.setChild(node.parent.data, node.relid, node.data);
+
+					node = node.parent;
+				}
+			}
+		};
+
+		this.persist = function (node, callback) {
+			ASSERT(this.isValid(node));
+			ASSERT(this.isMutable(node));
+
+			graph.persist(node.data, callback);
+		};
+
+		this.getChildren = function (node) {
+			ASSERT(this.isValid(node));
+			ASSERT(!this.isLeaf(node));
+
+			return graph.getChildren(node.data);
+		};
+
+		this.getChild = function (node, relid, callback) {
+			ASSERT(this.isValid(node));
+			ASSERT(!this.isLeaf(node));
+			ASSERT(callback);
+
+			graph.getChild(node.data, relid, function (err, data) {
+				callback(err, err ? undefined : {
+					data: data,
+					parent: node,
+					relid: relid
+				});
+			});
+		};
+
+		this.setChild = function (node, relid, child) {
+			ASSERT(this.isValid(node));
+			ASSERT(!this.isLeaf(node));
+			ASSERT(this.isValid(child));
+			ASSERT(this.isMutable(node));
+
+			graph.setChild(node.data, relid, child.data);
+		};
+
+		this.delChild = function (node, relid) {
+			ASSERT(this.isValid(node));
+			ASSERT(!this.isLeaf(node));
+			ASSERT(this.isMutable(node));
+
+			graph.delChild(node.data, relid);
+		};
+
+		this.getData = function (node, name) {
+			ASSERT(this.isValid(node));
+
+			graph.getData(node.data, name);
+		};
+
+		this.setData = function (node, name, data) {
+			ASSERT(this.isValid(node));
+			ASSERT(this.isMutable(node));
+
+			graph.setData(node.data, name, data);
+		};
+
+		this.delData = function (node, name) {
+			ASSERT(this.isValid(node));
+			ASSERT(this.isMutable(node));
+
+			graph.delData(node.data, name);
+		};
+	};
+
+	// ----------------- PersistentGraph -----------------
+
+	var PersistentGraph = function (storage) {
+		ASSERT(storage);
+
+		this.getKey = storage.getKey;
+
+		this.isVertex = function (vertex) {
+			ASSERT(vertex && typeof vertex === "object");
+			return storage.getKey(vertex) !== undefined;
+		};
+
+		this.isMutable = function (vertex) {
+			ASSERT(this.isVertex(vertex));
+			return storage.getKey(vertex) === false;
+		};
+
+		this.load = storage.load;
+
+		this.make = function (data) {
+			storage.setKey(data, false);
+		};
+
+		this.mutate = deepclone;
+
+		var Saver = function (callback) {
+			ASSERT(callback);
+
+			var counter = 1;
+			var error = null;
+
+			this.start = function () {
+				ASSERT(callback && counter >= 1);
+
+				++counter;
+			};
+
+			this.done = function (err) {
+				ASSERT(callback && counter >= 1);
+
+				error = error || err;
+				if( --counter === 0 ) {
+					callback(error);
+					callback = null;
+				}
+			};
+		};
+
+		// TODO: try to catch cycles in the graph
+		Saver.prototype.save = function (vertex) {
+			ASSERT(vertex && typeof vertex === "object");
+			ASSERT(typeof storage.getKey(vertex) !== "string");
+
+			var relid, child, key;
+
+			for( relid in vertex ) {
+				child = vertex[relid];
+				if( child && typeof child === "object" ) {
+
+					key = storage.getKey(child);
+					ASSERT(key === undefined || key === false || typeof key === "string");
+
+					if( !key ) {
+						key = this.save(child);
+					}
+
+					ASSERT(key === undefined || typeof key === "string");
+					if( key ) {
+						vertex[relid] = key;
+					}
+				}
+			}
+
+			key = storage.getKey(vertex);
+			if( key === false ) {
+
+				key = SHA1(JSON.stringify(vertex));
+				storage.setKey(vertex, key);
+
+				this.start();
+				storage.save(vertex, this.done);
+			}
+
+			ASSERT(key === undefined || typeof key === "string");
+			return key;
+		};
+
+		this.persist = function (vertex, callback) {
+			ASSERT(this.isMutable(vertex));
+			ASSERT(callback);
+
+			var saver = new Saver(callback);
+			var key = saver.save(vertex);
+			saver.done(null);
+
+			return key;
+		};
+	};
+
+	// ----------------- ContainmentTree -----------------
+
+	var ContainmentTree = function (graph) {
+		ASSERT(graph);
+
+		this.getKey = function (node) {
+			ASSERT(this.isNode(node));
+
+			return graph.getKey(node.vertex);
+		};
+
+		this.isNode = function (node) {
+			ASSERT(node && typeof node === "object");
+
+			return node.vertex && graph.isVertex(node.vertex);
+		};
+
+		this.isMutable = function (node) {
+			ASSERT(this.isNode(node));
+
+			return graph.isMutable(node.vertex);
+		};
+
+		this.mutate = function (node) {
+			ASSERT(this.isNode(node));
+
+			if( !graph.isMutable(node.data) ) {
+				node.data = graph.mutate(node.data);
+
+				while( node.parent && !graph.isMutable(node.parent.data) ) {
+					node.parent.data = graph.mutate(node.parent.data);
+					graph.setChild(node.parent.data, node.relid, node.data);
+
+					node = node.parent;
+				}
+			}
+		};
+
+		this.persist = function (node, callback) {
+			ASSERT(this.isMutable(node));
+
+			graph.persist(node.vertex, callback);
+		};
+
+		this.getRoot = function (key, callback) {
+			ASSERT(typeof key === "string");
+			ASSERT(callback);
+
+			graph.load(key, function (err, vertex) {
+				ASSERT(err || (vertex && vertex.children));
+				callback(err, err ? undefined : {
+					vertex: vertex,
+					parent: null
+				});
+			});
+		};
+
+		this.create = function() {
+			var node = {
+				vertex: {
+					children: {}
+				},
+				parent: null
+			};
+
+			graph.make(node.vertex);
+			return node;
+		};
+		
+		this.getParent = function (node) {
+			ASSERT(this.isNode(node));
+
+			return node.parent;
+		};
+
+		this.getChildrenRelids = function (node) {
+			ASSERT(this.isNode(node));
+			ASSERT(node.vertex.children);
+
+			return node.vertex.children;
+		};
+
+		this.getChild = function (node, relid, callback) {
+			
+		};
+		
+		this.moveNode = function (node, target) {
+			ASSERT(this.isNode(node));
+			ASSERT(this.isNode(target));
+
+			if( node.parent ) {
+				this.mutate(node.parent);
+				graph.delChild(node.parent.data, node.relid);
+			}
+
+			if( target ) {
+				this.mutate(target);
+			}
+			else {
+				node.parent = null;
+				delete node.relid;
+			}
+		};
+	};
+
+	// ----------------- ReferenceTree -----------------
+
+	var ReferenceTree = function (parenttree) {
+
+		this.getKey = function (node) {
+		};
+
+		this.getRoot = function (key, callback) {
+		};
+
+		this.createRoot = function () {
+		};
+
+		this.getParent = function (node) {
+		};
+
+		this.setParent = function (node, target) {
+		};
+
+		this.getChildren = function (node) {
+		};
+
+		this.getPointer = function (node, name, callback) {
+		};
+
+		this.setPointer = function (node, name, target) {
+		};
+
+		this.getCollection = function (node, name) {
+		};
 	};
 
 	// ----------------- Interface -----------------
-	
+
 	return {
 		Mongo: Mongo,
-		Graph: Graph,
-		Tree: Tree
+		Graph: Graph2,
+		Tree: Tree2
 	};
 });
--- corejs/srcx/test.js
+++ corejs/srcx/test.js
@@ -20,9 +20,9 @@ requirejs([ "assert", "storage" ], function (ASSERT, STORAGE) {
 
 		mongo.removeAll(function (err) {
 
-			var root = graph.create();
-			graph.setData(root, "name", "root");
-			var child = graph.create();
+			var root = graph.createNode();
+//			graph.setData(root, "name", "root");
+			var child = graph.createLeaf();
 			graph.setData(child, "name", "child");
 			graph.setChild(root, "1", child);
 			graph.setChild(root, "2", child);
@@ -30,14 +30,13 @@ requirejs([ "assert", "storage" ], function (ASSERT, STORAGE) {
 			graph.persist(root, function (err) {
 
 				tree.getRoot(graph.getKey(root), function (err, rnode) {
-					tree.getChild(rnode, "1", function(err, cnode) {
-						
+					tree.getChild(rnode, "1", function (err, cnode) {
+
 						tree.mutate(cnode);
 						tree.setData(cnode, "name", "hihi");
 
-						tree.persist(rnode, function(err) {
-							console.log("hihi");
-							mongo.dumpAll(function() {
+						tree.persist(rnode, function (err) {
+							mongo.dumpAll(function () {
 								mongo.close();
 							});
 						});
