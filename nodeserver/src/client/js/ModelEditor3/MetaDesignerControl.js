7734e66fe08ee585ec04c56553368aa55415ac85
rkereskenyi <rkereskenyi@github.com>
2013-01-28 16:39:57 -0600

    MetaEditor ~ SetEditor

--- nodeserver/src/client/js/ModelEditor3/MetaDesignerControl.js
+++ nodeserver/src/client/js/ModelEditor3/MetaDesignerControl.js
@@ -5,7 +5,8 @@ define(['logManager',
     'commonUtil',
     'js/ModelEditor3/DesignerControl',
     'js/DiagramDesigner/NodePropertyNames',
-    'js/ModelEditor3/MetaDesignerControl.DEBUG'], function (logManager,
+    'js/ModelEditor3/MetaDesignerControl.DEBUG',
+    'css!ModelEditor3CSS/MetaDesignerControl'], function (logManager,
                                                      clientUtil,
                                                      commonUtil,
                                                      DesignerControl,
@@ -15,11 +16,18 @@ define(['logManager',
     var MetaDesignerControl,
         __parent__ = DesignerControl,
         __parent_proto__ = DesignerControl.prototype,
-        CONTAINMENT_TYPE_LINE_END = "diamond-wide-long",
-        INHERITANCE_TYPE_LINE_END = "block-wide-long",
-        CONTAINMENT_TYPE = "CONTAINMENT",
-        INHERITANCE_TYPE = "INHERITANCE",
-        LOAD_EVENT_NAME = "load";
+        VALIDCHILDREN_TYPE_LINE_END = "diamond-wide-long",
+        VALIDINHERITOR_TYPE_LINE_END = "block-wide-long",
+        VALIDSOURCE_TYPE_LINE_END = "oval-wide-long",
+        VALIDDESTINATION_TYPE_LINE_END = "open-wide-long",
+        GENERAL_TYPE_LINE_END = "none",
+        NOEND = "none",
+        LOAD_EVENT_NAME = "load",
+        SET_VALIDCHILDREN = 'ValidChildren',
+        SET_VALIDSOURCE = 'ValidSource',
+        SET_VALIDDESTINATION = 'ValidDestination',
+        SET_VALIDINHERITOR = 'ValidInheritor',
+        SET_GENERAL = 'General';
 
 
     MetaDesignerControl = function (options) {
@@ -31,43 +39,62 @@ define(['logManager',
         //execute parent's constructor
         __parent__.apply(this, [options]);
 
+        this._setRelations = {};
+
         //override parent's event handlers
         this.designerCanvas.onSelectionDelete = function (idList) {
-            self.logger.warning("MetaDesignerControl.onSelectionDelete NOT YET IMPLEMENTED: " + idList);
+            var len = idList.length,
+                desc;
+
+            while (len--) {
+                if (self._setRelations[idList[len]]) {
+                    desc = self._setRelations[idList[len]];
+                    self._client.removeMember(desc.owner, desc.member, desc.set);
+                    delete self._setRelations[idList[len]];
+                    self.designerCanvas.deleteComponent(idList[len]);
+
+                }
+            }
         };
 
         this.designerCanvas.onCreateNewConnection = function (params) {
-            var setName = "",
-                type = params.metaInfo.type,
+            var type = params.metaInfo.type,
                 sourceId = self._ComponentID2GmeID[params.src],
                 targetId = self._ComponentID2GmeID[params.dst];
 
             //connDesc.type has special meaning: inheritance, containment, etc
-            if (type === INHERITANCE_TYPE) {
-                setName = 'ValidInheritor';
-            } else if (type === CONTAINMENT_TYPE) {
-                setName = 'ValidChildren';
-            }
-            if (setName !== "") {
-                self._client.addMember(sourceId, targetId, setName);
+            if (type) {
+                self._client.addMember(sourceId, targetId, type);
             }
         };
 
-        this._setMetaConnectionType(CONTAINMENT_TYPE);
+        this._setMetaConnectionType(SET_VALIDCHILDREN);
 
         //add extra visual piece
         $btnGroupConnectionType = this.designerCanvas.addRadioButtonGroup(function (event, data) {
             self._setMetaConnectionType(data.mode);
         });
 
-        this.designerCanvas.addButton({ "title": "Containment",
+        this.designerCanvas.addButton({ "title": SET_VALIDCHILDREN,
             "icon": "icon-meta-containment",
             "selected": true,
-            "data": { "mode": CONTAINMENT_TYPE }}, $btnGroupConnectionType );
+            "data": { "mode": SET_VALIDCHILDREN }}, $btnGroupConnectionType );
 
-        this.designerCanvas.addButton({ "title": "Inheritance",
+        this.designerCanvas.addButton({ "title": SET_VALIDINHERITOR,
             "icon": "icon-meta-inheritance",
-            "data": { "mode": INHERITANCE_TYPE }}, $btnGroupConnectionType );
+            "data": { "mode": SET_VALIDINHERITOR }}, $btnGroupConnectionType );
+
+        this.designerCanvas.addButton({ "title": SET_VALIDSOURCE,
+            "icon": "icon-meta-set_validsource",
+            "data": { "mode": SET_VALIDSOURCE }}, $btnGroupConnectionType );
+
+        this.designerCanvas.addButton({ "title": SET_VALIDDESTINATION,
+            "icon": "icon-meta-set_validdestination",
+            "data": { "mode": SET_VALIDDESTINATION }}, $btnGroupConnectionType );
+
+        this.designerCanvas.addButton({ "title": SET_GENERAL,
+            "icon": "icon-meta-set_general",
+            "data": { "mode": SET_GENERAL }}, $btnGroupConnectionType );
 
         //in DEBUG mode add additional content to canvas
         if (DEBUG) {
@@ -173,14 +200,13 @@ define(['logManager',
         // PUBLIC METHODS
     MetaDesignerControl.prototype._onLoad = function (gmeID, objD) {
         var obj,
-            srcId, dstId,
-            i, j,GMESrcId, GMEDstId,
             objDesc,
-            handled = false,
             setMemberIds,
-            len,
             decClass,
-            uiComponent;
+            i,
+            uiComponent,
+            sets = [SET_VALIDINHERITOR, SET_VALIDCHILDREN, SET_VALIDDESTINATION, SET_VALIDSOURCE, SET_GENERAL],
+            setlen = sets.length;
 
         //component loaded
         //we are interested in the load of subcomponents of the opened component
@@ -191,52 +217,31 @@ define(['logManager',
 
                 if (this.firstRun === false) {
                     var nodeObj = this._client.getNode(gmeID);
-                    setMemberIds = nodeObj.getMemberIds('ValidInheritor');
-                    if (setMemberIds) {
 
-                        objDesc = {};
+                    while (setlen--) {
+                        setMemberIds = nodeObj.getMemberIds(sets[setlen]);
+                        if (setMemberIds) {
 
-
-                        objDesc.srcObjId = this._GmeID2ComponentID[gmeID][0];
-                        objDesc.srcSubCompId = undefined;
-
-                        objDesc.arrowStart = INHERITANCE_TYPE_LINE_END;
-                        objDesc.width = 3;
-                        objDesc.color = "#0000FF";
-
-                        i = setMemberIds.length;
-                        while (i--) {
-                            if (this._GmeID2ComponentID.hasOwnProperty(setMemberIds[i]) &&
-                                this._GmeID2ComponentID[setMemberIds[i]].length > 0) {
-                                objDesc.dstObjId = this._GmeID2ComponentID[setMemberIds[i]][0];
-                                objDesc.dstSubCompId = undefined;
-                                obj = this.designerCanvas.createConnection(objDesc);
-                            }
-                        }
-                    }
-
-                    setMemberIds = nodeObj.getMemberIds('ValidChildren');
-                    if (setMemberIds) {
-
-                        objDesc = {};
+                            objDesc = {};
 
 
-                        objDesc.arrowStart = CONTAINMENT_TYPE_LINE_END;
-                        objDesc.width = 5;
-                        objDesc.color = "#FF0000";
-                        objDesc.srcObjId = this._GmeID2ComponentID[gmeID][0];
-                        objDesc.srcSubCompId = undefined;
+                            objDesc.srcObjId = this._GmeID2ComponentID[gmeID][0];
+                            objDesc.srcSubCompId = undefined;
 
-                        i = setMemberIds.length;
-                        while (i--) {
-                            if (this._GmeID2ComponentID.hasOwnProperty(setMemberIds[i]) &&
-                                this._GmeID2ComponentID[setMemberIds[i]].length > 0) {
-                                objDesc.dstObjId = this._GmeID2ComponentID[setMemberIds[i]][0];
-                                objDesc.dstSubCompId = undefined;
-                                uiComponent = this.designerCanvas.createConnection(objDesc);
+                            _.extend(objDesc, this._getModeVisualDescriptor(sets[setlen]));
 
-                                this._GmeID2ComponentID[gmeID].push(uiComponent.id);
-                                this._ComponentID2GmeID[uiComponent.id] = gmeID;
+                            i = setMemberIds.length;
+                            while (i--) {
+                                if (this._GmeID2ComponentID.hasOwnProperty(setMemberIds[i]) &&
+                                    this._GmeID2ComponentID[setMemberIds[i]].length > 0) {
+                                    objDesc.dstObjId = this._GmeID2ComponentID[setMemberIds[i]][0];
+                                    objDesc.dstSubCompId = undefined;
+                                    obj = this.designerCanvas.createConnection(objDesc);
+
+                                    this._setRelations[obj.id] = { "owner": gmeID,
+                                                                   "member": setMemberIds[i],
+                                                                   "set": sets[setlen] };
+                                }
                             }
                         }
                     }
@@ -257,10 +262,6 @@ define(['logManager',
                 }
             }
         }
-
-/*        if (!handled) {
-            __parent_proto__._onLoad.apply(this, arguments);
-        }*/
     };
 
     MetaDesignerControl.prototype._setMetaConnectionType = function (mode) {
@@ -271,25 +272,54 @@ define(['logManager',
             metaInfo = {"type": mode };
             this.designerCanvas.connectionDrawingManager.setMetaInfo(metaInfo);
 
-            switch (mode) {
-                case CONTAINMENT_TYPE:
-                    params.arrowStart = CONTAINMENT_TYPE_LINE_END;
-                    params.width = "5";
-                    params.color = "#FF0000";
-                    break;
-                case INHERITANCE_TYPE:
-                    params.arrowStart = INHERITANCE_TYPE_LINE_END;
-                    params.width = "3";
-                    params.color = "#0000FF";
-                    break;
-                default:
-                    params.arrowStart = "none";
-                    params.width = "2";
-                    params.color = "#000000";
-                    break;
-            }
+            params = this._getModeVisualDescriptor(mode);
+
             this.designerCanvas.connectionDrawingManager.setConnectionInDrawProperties(params);
         }
+    };
+
+    MetaDesignerControl.prototype._getModeVisualDescriptor = function (mode) {
+        var params = { "arrowStart" : "none",
+                        "arrowEnd" : "none",
+                        "width" : "1",
+                        "color" :"#AAAAAA" };
+
+        switch (mode) {
+            case SET_VALIDCHILDREN:
+                params.arrowStart = VALIDCHILDREN_TYPE_LINE_END;
+                params.arrowEnd = NOEND;
+                params.width = "5";
+                params.color = "#FF0000";
+                break;
+            case SET_VALIDINHERITOR:
+                params.arrowStart = VALIDINHERITOR_TYPE_LINE_END;
+                params.arrowEnd = NOEND;
+                params.width = "3";
+                params.color = "#0000FF";
+                break;
+            case SET_VALIDSOURCE:
+                params.arrowStart = VALIDSOURCE_TYPE_LINE_END;
+                params.arrowEnd = NOEND;
+                params.width = "3";
+                params.color = "#00FF00";
+                break;
+            case SET_VALIDDESTINATION:
+                params.arrowStart = NOEND;
+                params.arrowEnd = VALIDDESTINATION_TYPE_LINE_END
+                params.width = "3";
+                params.color = "#AA03C3";
+                break;
+            case SET_GENERAL:
+                params.arrowStart = GENERAL_TYPE_LINE_END;
+                params.arrowEnd = NOEND;
+                params.width = "2";
+                params.color = "#000000";
+                break;
+            default:
+                break;
+        }
+
+        return params;
     };
 
     return MetaDesignerControl;
