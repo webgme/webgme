66f5b24f9820becff3081bef1af17b9b4b8ff759
rkereskenyi <robert.kereskenyi@gmail.com>
2013-03-04 15:20:16 -0600

    updating Loaders: - rename LoaderView to LoaderCircles (used in DiagramDesigner) - introduce LoaderProgressBar (used in VisualizerPanel)

---
+++ nodeserver/src/client/js/Loader/LoaderProgressBar.js
@@ -0,0 +1,51 @@
+"use strict";
+
+define(['css!LoaderCSS/LoaderProgressBar'], function () {
+
+    var LoaderProgressBar;
+
+    LoaderProgressBar = function (params) {
+
+        this._el = params.containerElement;
+
+        if (this._el.length === 0) {
+            throw "LoaderProgressBar's container control with id:'" + params.containerElement + "' could not be found";
+        }
+    };
+
+    LoaderProgressBar.prototype._initialize = function () {
+        this._createElements();
+    };
+
+    LoaderProgressBar.prototype._removeElements = function () {
+        if (this._loaderDiv) {
+            this._loaderDiv.remove();
+        }
+        this._loaderDiv = null;
+    };
+
+    LoaderProgressBar.prototype._createElements = function () {
+        if (!this._loaderDiv) {
+            this._loaderDiv = $('<div/>', { "class" : "loader-progressbar" });
+
+            this._el.append(this._loaderDiv);
+
+            //force reflow
+            this._el.width();
+        }
+    };
+
+    LoaderProgressBar.prototype.destroy = function () {
+        this._removeElements();
+    };
+
+    LoaderProgressBar.prototype.stop = function () {
+        this._removeElements();
+    };
+
+    LoaderProgressBar.prototype.start = function () {
+        this._createElements();
+    };
+
+    return LoaderProgressBar;
+});
