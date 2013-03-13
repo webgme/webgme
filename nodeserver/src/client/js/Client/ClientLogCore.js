5a6bd2f3c38458564decc237ef105eefa5ab0ca1
Miklos Maroti <mmaroti@isis.vanderbilt.edu>
2013-03-13 13:38:38 -0500

    do not mess with the internals of nodes

--- nodeserver/src/client/js/Client/ClientLogCore.js
+++ nodeserver/src/client/js/Client/ClientLogCore.js
@@ -36,13 +36,13 @@ define(['commonUtil','core/core'+COREVERSION,'core/assert','logManager'], functi
             return core.isValidPath(path);
         };
         var isValidNode = function (node) {
-            log("isValidNode("+node["data"]["_id"]+")");
+            log("isValidNode("+core.getKey(node)+")");
             return core.isValidNode(node);
         };
 
         //root
         var getKey = function (node) {
-            log("getKey("+node["data"]["_id"]+"))");
+            log("getKey("+core.getKey(node)+"))");
             return core.getKey(node);
         };
         var loadRoot = function (key, callback) {
@@ -58,7 +58,7 @@ define(['commonUtil','core/core'+COREVERSION,'core/assert','logManager'], functi
         var persist = function (root, callback) {
             var start = TIMESTAMP();
             var guid = "["+GUID()+"]";
-            var text = guid+"persist("+root["_id"]+")";
+            var text = guid+"persist("+core.getKey(root)+")";
             log(text);
             return core.persist(root,function(err){
                 log(text+ETIMESTRING(start));
@@ -66,21 +66,21 @@ define(['commonUtil','core/core'+COREVERSION,'core/assert','logManager'], functi
             });
         };
         var getRoot = function (node) {
-            log("getRoot("+node["data"]["_id"]+")");
+            log("getRoot("+core.getKey(node)+")");
             return core.getRoot(node);
         };
 
         // containment
         var getLevel = function (node, base) {
-            log("getLevel("+node["data"]["_id"]+","+base["_id"]+")");
+            log("getLevel("+core.getKey(node)+","+core.getKey(base)+")");
             return core.getLevel(node,base);
         };
         var getRelid = function (node) {
-            log("getRelid("+node["data"]["_id"]+")");
+            log("getRelid("+core.getKey(node)+")");
             return core.getRelid(node);
         };
         var getStringPath = function (node, base) {
-            log("getStringPath("+node["data"]["_id"]+","+ ( base === undefined ? base : base["_id"] ) +")");
+            log("getStringPath("+core.getKey(node)+","+ ( base === undefined ? base : core.getKey(base)) +")");
             return core.getStringPath(node,base);
         };
         var parseStringPath = function (path) {
@@ -88,21 +88,21 @@ define(['commonUtil','core/core'+COREVERSION,'core/assert','logManager'], functi
             return core.parseStringPath(path);
         };
         var getParent = function (node) {
-            log("getParent("+node["data"]["_id"]+")");
+            log("getParent("+core.getKey(node)+")");
             return core.getParent(node);
         };
         var getChildrenRelids = function (node) {
-            log("getChildrenRelids("+node["data"]["_id"]+")");
+            log("getChildrenRelids("+core.getKey(node)+")");
             return core.getChildrenRelids(node);
         };
         var getChildrenPaths = function (node) {
-            log("getChildrenPaths("+node["data"]["_id"]+")");
+            log("getChildrenPaths("+core.getKey(node)+")");
             return core.getChildrenPaths(node);
         };
         var loadChild = function (node, relid, callback) {
             var start = TIMESTAMP();
             var guid = "["+GUID()+"]";
-            var text = guid+"loadChild("+node["data"]["_id"]+","+relid+")";
+            var text = guid+"loadChild("+core.getKey(node)+","+relid+")";
             log(text);
             core.loadChild(node,relid,function(err,child){
                 log(text+ETIMESTRING(start));
@@ -112,7 +112,7 @@ define(['commonUtil','core/core'+COREVERSION,'core/assert','logManager'], functi
         var loadByPath = function (node, path, callback) {
             var start = TIMESTAMP();
             var guid = "["+GUID()+"]";
-            var text = guid+"loadByPath("+node["data"]["_id"]+","+path+")";
+            var text = guid+"loadByPath("+core.getKey(node)+","+path+")";
             log(text);
             core.loadByPath(node,path,function(err,node){
                 log(text+ETIMESTRING(start));
@@ -122,7 +122,7 @@ define(['commonUtil','core/core'+COREVERSION,'core/assert','logManager'], functi
         var loadChildren = function (node, callback) {
             var start = TIMESTAMP();
             var guid = "["+GUID()+"]";
-            var text = guid+"loadChildren("+node["data"]["_id"]+")";
+            var text = guid+"loadChildren("+core.getKey(node)+")";
             log(text);
             core.loadChildren(node,function(err,children){
                 log(text+ETIMESTRING(start));
@@ -133,84 +133,84 @@ define(['commonUtil','core/core'+COREVERSION,'core/assert','logManager'], functi
         // modify
         var createNode = function (parent, relid) {
             if(parent){
-                log("createNode("+parent["_id"]+","+relid+")");
+                log("createNode("+core.getKey(parent)+","+relid+")");
             } else {
                 log("createNode(null,"+relid+")");
             }
             return core.createNode(parent,relid);
         };
         var deleteNode = function (node) {
-            log("deleteNode("+node["data"]["_id"]+")");
+            log("deleteNode("+core.getKey(node)+")");
             return core.deleteNode(node);
         };
         var copyNode = function (node, parent) {
-            log("copyNode("+node["data"]["_id"]+","+parent["_id"]+")");
+            log("copyNode("+core.getKey(node)+","+core.getKey(parent)+")");
             return core.copyNode(node,parent);
         };
         var moveNode = function (node, parent) {
-            log("moveNode("+node["data"]["_id"]+","+parent["_id"]+")");
+            log("moveNode("+core.getKey(node)+","+core.getKey(parent)+")");
             return core.moveNode(node,parent);
         };
 
         // attributes
         var getAttributeNames = function (node) {
-            log("getAttributeNames("+node["data"]["_id"]+")");
+            log("getAttributeNames("+core.getKey(node)+")");
             return core.getAttributeNames(node);
         };
 
         var getAttribute = function (node, name) {
-            log("getAttribute("+node["data"]["_id"]+","+name+")");
+            log("getAttribute("+core.getKey(node)+","+name+")");
             return core.getAttribute(node,name);
         };
 
         var delAttribute = function (node, name) {
-            log("delAttribute("+node["data"]["_id"]+","+name+")");
+            log("delAttribute("+core.getKey(node)+","+name+")");
             return core.delAttribute(node,name);
 
         };
 
         var setAttribute = function (node, name, value) {
-            log("setAttribute("+node["data"]["_id"]+","+name+","+value+")");
+            log("setAttribute("+core.getKey(node)+","+name+","+value+")");
             return core.setAttribute(node,name,value);
         };
 
         var getRegistryNames = function (node) {
-            log("getRegistryNames("+node["data"]["_id"]+")");
+            log("getRegistryNames("+core.getKey(node)+")");
             return core.getRegistryNames(node);
         };
 
         var getRegistry = function (node, name) {
-            log("getRegistry("+node["data"]["_id"]+","+name+")");
+            log("getRegistry("+core.getKey(node)+","+name+")");
             return core.getRegistry(node,name);
         };
 
         var delRegistry = function (node, name) {
-            log("delRegistry("+node["data"]["_id"]+","+name+")");
+            log("delRegistry("+core.getKey(node)+","+name+")");
             return core.delRegistry(node,name);
         };
 
         var setRegistry = function (node, name, value) {
-            log("setRegistry("+node["data"]["_id"]+","+name+","+value+")");
+            log("setRegistry("+core.getKey(node)+","+name+","+value+")");
             return core.setRegistry(node,name,value);
         };
 
         // relations
         var getPointerNames = function (node) {
-            log("getPointerNames("+node["data"]["_id"]+")");
+            log("getPointerNames("+core.getKey(node)+")");
             return core.getPointerNames(node);
         };
         var getPointerPath = function (node, name) {
-            log("getPointerPath("+node["data"]["_id"]+","+name+")");
+            log("getPointerPath("+core.getKey(node)+","+name+")");
             return core.getPointerPath(node,name);
         };
         var getOutsidePointerPath = function (node, name, source) {
-            log("getOutsidePointerPath("+node["data"]["_id"]+","+name+","+source["_id"]+")");
+            log("getOutsidePointerPath("+core.getKey(node)+","+name+","+core.getKey(source)+")");
             return core.getOutsidePointerPath(node,name,source);
         };
         var loadPointer = function (node, name, callback) {
             var start = TIMESTAMP();
             var guid = "["+GUID()+"]";
-            var text = guid+"loadPointer("+node["data"]["_id"]+","+name+")";
+            var text = guid+"loadPointer("+core.getKey(node)+","+name+")";
             log(text);
             core.loadPointer(node,name,function(err,target){
                 log(text+ETIMESTRING(start));
@@ -218,25 +218,25 @@ define(['commonUtil','core/core'+COREVERSION,'core/assert','logManager'], functi
             });
         };
         var deletePointer = function (node, name) {
-            log("deletePointer("+node["data"]["_id"]+","+name+")");
+            log("deletePointer("+core.getKey(node)+","+name+")");
             return core.deletePointer(node,name);
         };
         var setPointer = function (node, name, target) {
-            log("setPointer("+node["data"]["_id"]+","+name+","+target["_id"]+")");
+            log("setPointer("+core.getKey(node)+","+name+","+core.getKey(target)+")");
             return core.setPointer(node,name,target);
         };
         var getCollectionNames = function (node) {
-            log("getCollectionNames("+node["data"]["_id"]+")");
+            log("getCollectionNames("+core.getKey(node)+")");
             return core.getCollectionNames(node);
         };
         var getCollectionPaths = function (node, name) {
-            log("getCollectionPaths("+node["data"]["_id"]+","+name+")");
+            log("getCollectionPaths("+core.getKey(node)+","+name+")");
             return core.getCollectionPaths(node,name);
         };
         var loadCollection = function (node, name, callback) {
             var start = TIMESTAMP();
             var guid = "["+GUID()+"]";
-            var text = guid+"loadCollection("+node["data"]["_id"]+","+name+")";
+            var text = guid+"loadCollection("+core.getKey(node)+","+name+")";
             log(text);
             core.loadCollection(node,name,function(err,collection){
                 log(text+ETIMESTRING(start));
@@ -245,7 +245,7 @@ define(['commonUtil','core/core'+COREVERSION,'core/assert','logManager'], functi
         };
 
         var getSingleNodeHash = function (node) {
-            log("getSingleNodeHash("+node["data"]["_id"]+")");
+            log("getSingleNodeHash("+core.getKey(node)+")");
             return core.getSingleNodeHash(node);
         };
 
