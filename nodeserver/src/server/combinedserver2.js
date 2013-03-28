f6cab3ae06cd5c3b67d384ea85a3a7b4a74f4209
kecso <tamas.kecskes@vanderbilt.edu>
2013-03-28 16:59:45 -0500

    limiting paralell fire reads of http server

--- nodeserver/src/server/combinedserver2.js
+++ nodeserver/src/server/combinedserver2.js
@@ -15,14 +15,66 @@ requirejs(['server/proxysrv',
             'common/CommonUtil'],function(PROXY,
                                            logManager,
                                            commonUtil){
-    logManager.setLogLevel(logManager.logLevels.WARNING);
+    logManager.setLogLevel(logManager.logLevels.ALL);
     logManager.useColors(true);
     var Server = function(parameters){
         var logger = logManager.create("combined-server");
         var iologger = logManager.create("socket.io");
         var iopar =  commonUtil.combinedserver.srvsocketpar;
         iopar.logger = iopar.logger || iologger;
+        var waitingList = [];
+        var ongoingReads = 0;
+        var maxParalellReads = 10;
+        var readFileEnded = function(){
+            var next = waitingList.pop();
+            if(next){
+                readFileWork(next.path,next.req,next.res,readFileEnded)
+            } else {
+                ongoingReads--;
+            }
+        };
+
+        var readFile = function(filepath,request,response){
+            if(ongoingReads<maxParalellReads){
+                ongoingReads++;
+                readFileWork(filepath,request,response,readFileEnded)
+            } else {
+                waitingList.push({path:filepath,req:request,res:response});
+            }
+        };
+
+        var readFileWork = function(filepath,request,response,callback){
+            FS.readFile(filepath, function(err,data){
+                if(err){
+                    response.writeHead(500);
+                    logger.error("Error getting the file:" +__dirname + clientsrcfolder +request.url);
+                    response.end('Error loading ' + request.url);
+                } else {
+                    if(request.url.indexOf('.js')>0){
+                        logger.debug("HTTP RESP - "+request.url);
+                        response.writeHead(200, {
+                            'Content-Length': data.length,
+                            'Content-Type': 'application/x-javascript' });
+
+                    } else if (request.url.indexOf('.css')>0) {
+                        logger.debug("HTTP RESP - "+request.url);
+                        response.writeHead(200, {
+                            'Content-Length': data.length,
+                            'Content-Type': 'text/css' });
+
+                    }
+                    else{
+                        response.writeHead(200);
+                    }
+                    response.end(data);
+                }
+                callback();
+            });
+        };
+
+
         var http = require('http').createServer(function(req, res){
+
             logger.debug("HTTP REQ - "+req.url);
 
             if(req.url==='/'){
@@ -40,31 +92,8 @@ requirejs(['server/proxysrv',
                 clientsrcfolder = "/../../../corejs";
             }
 
-            FS.readFile(__dirname + clientsrcfolder +req.url, function(err,data){
-                if(err){
-                    res.writeHead(500);
-                    logger.error("Error getting the file:" +__dirname + clientsrcfolder +req.url);
-                    return res.end('Error loading ' + req.url);
-                }
+            readFile(__dirname + clientsrcfolder +req.url,req,res);
 
-                if(req.url.indexOf('.js')>0){
-                    logger.debug("HTTP RESP - "+req.url);
-                    res.writeHead(200, {
-                        'Content-Length': data.length,
-                        'Content-Type': 'application/x-javascript' });
-
-                } else if (req.url.indexOf('.css')>0) {
-                    logger.debug("HTTP RESP - "+req.url);
-                    res.writeHead(200, {
-                        'Content-Length': data.length,
-                        'Content-Type': 'text/css' });
-
-                }
-                else{
-                    res.writeHead(200);
-                }
-                res.end(data);
-            });
         }),
 
             io = require('socket.io').listen(http,iopar);
