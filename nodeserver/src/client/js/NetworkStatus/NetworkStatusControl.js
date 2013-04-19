d4869044a9016eff8357d8e40050a9097459349e
rkereskenyi <robert.kereskenyi@gmail.com>
2013-04-19 11:56:57 -0500

    bugfix

--- nodeserver/src/client/js/NetworkStatus/NetworkStatusControl.js
+++ nodeserver/src/client/js/NetworkStatus/NetworkStatusControl.js
@@ -11,30 +11,35 @@ define(['logManager'], function (logManager) {
         this._view = myView;
 
         //override view event handlers
-        this._view.onNetworkStatusButtonClicked = function (newState) {
-            switch (newState) {
-            case "go_online":
-                self._client.goOnline();
-                break;
-            case "go_offline":
-                self._client.goOffline();
-                break;
+        this._view.onNetworkStatusButtonClicked = function (val) {
+            if (val === 'connect') {
+                self._client.connect();
             }
         };
 
         this._client.addEventListener(this._client.events.NETWORKSTATUS_CHANGED, function (__project, state) {
             switch (state) {
-            case "offline":
-                self._view.stateChanged("offline");
-                break;
-            case "online":
-                self._view.stateChanged("online");
-                break;
-            case "nonetwork":
-                self._view.stateChanged("nonetwork");
-                break;
+                case self._client.networkStates.CONNECTED:
+                    self._view.stateChanged(self._view.STATES.CONNECTED);
+                    break;
+                case self._client.networkStates.DISCONNECTED:
+                    self._view.stateChanged(self._view.STATES.DISCONNECTED);
+                    break;
             }
         });
+
+        var status = "CONNECTED";
+        self._view.stateChanged(status);
+
+        setInterval(function (){
+            if (status === "CONNECTED") {
+                status = "DISCONNECTED";
+            } else {
+                status = "CONNECTED";
+            }
+            self._view.stateChanged(status);
+
+        }, 3000);
 
         this._logger = logManager.create("NetworkStatusControl");
         this._logger.debug("Created");
