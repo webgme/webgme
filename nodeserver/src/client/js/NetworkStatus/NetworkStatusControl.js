5c153a181895826b251d418c4f99d724173901fd
rkereskenyi <robert.kereskenyi@gmail.com>
2013-04-19 14:27:50 -0500

    NetworkStatus and BranchStatus 2 separate buttons in footer

--- nodeserver/src/client/js/NetworkStatus/NetworkStatusControl.js
+++ nodeserver/src/client/js/NetworkStatus/NetworkStatusControl.js
@@ -12,6 +12,14 @@ define(['logManager'], function (logManager) {
 
         //override view event handlers
         this._view.onNetworkStatusButtonClicked = function (val) {
+            if (val === 'go_online') {
+                self._client.goOnline();
+            } else if (val === 'go_offline') {
+                self._client.goOffline();
+            }
+        };
+
+        this._view.onBranchStatusButtonClicked = function (val) {
             if (val === 'connect') {
                 self._client.connect();
             }
@@ -20,31 +28,30 @@ define(['logManager'], function (logManager) {
         this._client.addEventListener(this._client.events.NETWORKSTATUS_CHANGED, function (__project, state) {
             switch (state) {
                 case self._client.networkStates.CONNECTED:
-                    self._view.stateChanged(self._view.STATES.CONNECTED);
+                    self._view.networkStateChanged(self._view.NETWORK_STATES.CONNECTED);
                     break;
                 case self._client.networkStates.DISCONNECTED:
-                    self._view.stateChanged(self._view.STATES.DISCONNECTED);
+                    self._view.networkStateChanged(self._view.NETWORK_STATES.DISCONNECTED);
                     break;
             }
         });
 
-        var status = "CONNECTED";
-        self._view.stateChanged(status);
-
-        setInterval(function (){
-            if (status === "CONNECTED") {
-                status = "DISCONNECTED";
-            } else {
-                status = "CONNECTED";
+        this._client.addEventListener(this._client.events.BRANCHSTATUS_CHANGED, function (__project, state) {
+            switch (state) {
+                case self._client.branchStates.SYNC:
+                    self._view.branchStateChanged(self._view.BRANCH_STATES.INSYNC);
+                    break;
+                case self._client.branchStates.FORKED:
+                    self._view.branchStateChanged(self._view.BRANCH_STATES.FORKED);
+                    break;
+                case self._client.branchStates.OFFLINE:
+                    self._view.branchStateChanged(self._view.BRANCH_STATES.OFFLINE);
+                    break;
             }
-            self._view.stateChanged(status);
-
-        }, 3000);
+        });
 
         this._logger = logManager.create("NetworkStatusControl");
         this._logger.debug("Created");
-
-
     };
 
     return NetworkStatusControl;
