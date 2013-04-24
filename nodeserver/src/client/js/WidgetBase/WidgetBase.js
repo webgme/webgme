c5c5f1f4ec497a6dd957b4d3963aabc4af87aed2
rkereskenyi <robert.kereskenyi@gmail.com>
2013-04-24 15:07:24 -0500

    extending WidgetBase(Header) with support feautures for
    - following scroll position
    - resize
    - offset / size handler

--- nodeserver/src/client/js/WidgetBase/WidgetBase.js
+++ nodeserver/src/client/js/WidgetBase/WidgetBase.js
@@ -33,6 +33,21 @@ define(['jquery',
 
         //by default Widget is in EDIT mode
         this._isReadOnly = false;
+
+        //scroll position
+        this.scrollPos = {"left": 0,
+            "top": 0};
+
+        //clear content
+        this.$el.empty();
+
+        //get widget's offset
+        this.offset = this.$el.offset();
+
+        //get widget's size
+        this._size();
+
+        this.attachScrollHandler(options.fnOnScroll);
     };
 
     WidgetBase.OPTIONS = { "CONTAINER_ELEMENT" : "containerElement",
@@ -64,6 +79,24 @@ define(['jquery',
     };
     /***** END OF --- SET READ-ONLY MODE *********/
 
+    WidgetBase.prototype.attachScrollHandler = function (fnOnScroll) {
+        var self = this;
+
+        this.$el.on('scroll', function (event) {
+            self.scrollPos.left = self.$el.scrollLeft();
+            self.scrollPos.top = self.$el.scrollTop();
+
+            if (fnOnScroll) {
+                fnOnScroll(self.scrollPos);
+            }
+        });
+    };
+
+    WidgetBase.prototype._size = function () {
+        this.size = {"width": this.$el.outerWidth(true),
+            "height": this.$el.outerHeight(true)};
+    };
+
     /************** WIDGET-BASE INTERFACE *******************/
 
     /* METHOD CALLED WHEN THE WIDGET HAS TO CLEAR ITSELF */
@@ -78,6 +111,7 @@ define(['jquery',
 
     /* METHOD CALLED WHEN THE PARENT CONTAINER SIZE HAS CHANGED AND WIDGET SHOULD RESIZE ITSELF ACCORDINGLY */
     WidgetBase.prototype.parentContainerSizeChanged = function (newWidth, newHeight) {
+        this._size();
     };
 
     return WidgetBase;
