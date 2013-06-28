4294face61e6e99624e9b187b3376719ae496e5d
rkereskenyi <robert.kereskenyi@gmail.com>
2013-06-28 18:07:39 -0500

    color picker

---
+++ client/js/Controls/ColorPicker.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2013 Vanderbilt University, All rights reserved.
+ * 
+ * Author: Robert Kereskenyi
+ */
+
+"use strict";
+
+define(['jquery',
+        'jscolor',
+        'css!/css/Controls/ColorPicker'], function (_jquery,
+                              _jscolor) {
+
+    var ColorPicker,
+        DISABLED_CLASS = 'disabled';
+
+    ColorPicker = function () {
+        var self = this;
+
+        jscolor.binding = false;
+
+        this.el = $('<input type="text" class="color-picker"/>');
+        this._colorPicker = new jscolor.color(this.el[0], {'hash': true});
+
+        this.el.on('change', function (/*e*/) {
+            self.onColorChanged(self.el.val());
+        });
+
+        this.el.on('blur', function(/*e*/) {
+            setTimeout(function() {
+                if (!jscolor.picker.owner) {
+                    self.onEndColorPick();
+                }
+            }, 5);
+        });
+    };
+
+    ColorPicker.prototype.setColor = function (color) {
+        this._colorPicker.fromString(color);
+    };
+
+    ColorPicker.prototype.onColorChanged = function (color) {
+    };
+
+    ColorPicker.prototype.onEndColorPick = function () {
+    };
+
+    ColorPicker.prototype.setEnabled = function (isEnabled) {
+        if (isEnabled) {
+            this.el.removeClass(DISABLED_CLASS);
+        } else {
+            this.el.addClass(DISABLED_CLASS);
+        }
+    };
+
+    return ColorPicker;
+});
---
+++ client/js/Controls/PropertyGrid/Widgets/ColorPickerWidget.js
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2013 Vanderbilt University, All rights reserved.
+ * 
+ * Author: Robert Kereskenyi
+ */
+
+"use strict";
+
+define(['js/Controls/PropertyGrid/Widgets/WidgetBase',
+    'js/Controls/ColorPicker'],
+    function (WidgetBase,
+              ColorPicker) {
+
+        var ColorPickerWidget;
+
+        ColorPickerWidget  = function (propertyDesc) {
+            var _self = this;
+
+            ColorPickerWidget.superclass.call(this, propertyDesc);
+
+            this.__colorPicker = new ColorPicker();
+            this.__colorPicker.onColorChanged = function (color) {
+                _self.setValue(color);
+            };
+
+            this.__colorPicker.onEndColorPick = function () {
+                _self.fireFinishChange();
+            };
+
+            this.updateDisplay();
+
+            this.el.append(this.__colorPicker.el);
+        };
+
+        ColorPickerWidget.superclass = WidgetBase;
+
+        _.extend(
+            ColorPickerWidget.prototype,
+            WidgetBase.prototype
+        );
+
+        ColorPickerWidget.prototype.updateDisplay =  function () {
+            this.__colorPicker.setColor(this.getValue());
+
+            return ColorPickerWidget.superclass.prototype.updateDisplay.call(this);
+        };
+
+        ColorPickerWidget.prototype.setReadOnly = function (isReadOnly) {
+            ColorPickerWidget.superclass.prototype.setReadOnly.call(this, isReadOnly);
+
+            this.__colorPicker.setEnabled(!isReadOnly);
+        };
+
+        return ColorPickerWidget;
+
+    });
