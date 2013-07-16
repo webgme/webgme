3f8f62feed859312e31aeef530774177c52afe8c
kecso <tamas.kecskes@vanderbilt.edu>
2013-07-16 17:15:21 -0500

    continuing to implement REST api

--- bin/combined_server.js
+++ bin/combined_server.js
@@ -55,83 +55,99 @@ requirejs(['logManager',
         };
         iopar.logger = iopar.logger || iologger;
         var http = require('http').createServer(function(req, res){
-            logger.debug("HTTP REQ - "+req.url);
+            var sendNegativeResponse = function(respCode,respBody){
+                res.writeHead(respCode);
+                return res.end(respBody);
+            };
 
-            if(req.url.indexOf('/rest/') >=0){
-                var goOn = function(){
-                    console.log('fuckyeah');
-                    rest.processURI(req.url,function(err,data){
-                        console.log(err,data);
-                        if(err){
-                            data = null;
-                            res.writeHead(500);
-                        } else {
-                            data = JSON.stringify(data);
-                            res.writeHead(200, {
-                                'Content-Length': data.length,
-                                'Content-Type': 'application/json' });
-                        }
-                        res.end(data);
+            switch(req.method){
+                case 'POST':
+                    var body = '';
+                    req.on('data', function (data) {
+                        body += data;
                     });
-                }
-                if(restOpened){
-                    goOn();
-                } else {
-                    rest.open(function(err){
-                        console.log(err);
-                        if(!err){
-                            restOpened = true;
+                    req.on('end', function () {
+                        rest.processPOST(req.url,body,function(err,data){
+
+                        })
+                    });
+                    break;
+                default: //GET
+                    logger.debug("HTTP REQ - "+req.url);
+                    if(req.url.indexOf('/rest/') >=0){
+                        var goOn = function(){
+                            rest.processGET(req.url,function(err,data){
+                                console.log(err,data);
+                                if(err){
+                                    data = null;
+                                    res.writeHead(500);
+                                } else {
+                                    data = JSON.stringify(data);
+                                    res.writeHead(200, {
+                                        'Content-Length': data.length,
+                                        'Content-Type': 'application/json' });
+                                }
+                                res.end(data);
+                            });
+                        }
+                        if(restOpened){
                             goOn();
+                        } else {
+                            rest.open(function(err){
+                                console.log(err);
+                                if(!err){
+                                    restOpened = true;
+                                    goOn();
+                                }
+                            });
                         }
-                    });
-                }
-            } else {
-                if(req.url==='/'){
-                    req.url = '/index.html';
-                }
+                    } else {
+                        if(req.url==='/'){
+                            req.url = '/index.html';
+                        }
 
-                var dirname = __dirname;
-                if(dirname.charAt(dirname.length-1) !== '/') {
-                    dirname += '/';
-                }
-                dirname += "./../";
+                        var dirname = __dirname;
+                        if(dirname.charAt(dirname.length-1) !== '/') {
+                            dirname += '/';
+                        }
+                        dirname += "./../";
 
-                if (!(  req.url.indexOf('/common/') === 0 ||
-                    req.url.indexOf('/util/') === 0 ||
-                    req.url.indexOf('/storage/') === 0 ||
-                    req.url.indexOf('/core/') === 0 ||
-                    req.url.indexOf('/user/') === 0 ||
-                    req.url.indexOf('/config/') === 0 ||
-                    req.url.indexOf('/bin/') === 0)){
-                    dirname += "client";
-                }
+                        if (!(  req.url.indexOf('/common/') === 0 ||
+                            req.url.indexOf('/util/') === 0 ||
+                            req.url.indexOf('/storage/') === 0 ||
+                            req.url.indexOf('/core/') === 0 ||
+                            req.url.indexOf('/user/') === 0 ||
+                            req.url.indexOf('/config/') === 0 ||
+                            req.url.indexOf('/bin/') === 0)){
+                            dirname += "client";
+                        }
 
-                require('fs').readFile(dirname + req.url, function(err,data){
-                    if(err){
-                        res.writeHead(500);
-                        console.log("Error getting the file:" + req.url);
-                        logger.error("Error getting the file:" + dirname + req.url);
-                        return res.end('Error loading ' + req.url);
+                        require('fs').readFile(dirname + req.url, function(err,data){
+                            if(err){
+                                logger.error("Error getting the file:" + dirname + req.url);
+                                sendNegativeResponse(404,'Error loading ' + req.url);
+                            }
+
+                            if(req.url.indexOf('.js')>0){
+                                logger.debug("HTTP RESP - "+req.url);
+                                res.writeHead(200, {
+                                    'Content-Length': data.length,
+                                    'Content-Type': 'application/x-javascript' });
+
+                            } else if (req.url.indexOf('.css')>0) {
+                                logger.debug("HTTP RESP - "+req.url);
+                                res.writeHead(200, {
+                                    'Content-Length': data.length,
+                                    'Content-Type': 'text/css' });
+
+                            }
+                            else{
+                                res.writeHead(200);
+                            }
+                            res.end(data);
+                        });
                     }
-
-                    if(req.url.indexOf('.js')>0){
-                        logger.debug("HTTP RESP - "+req.url);
-                        res.writeHead(200, {
-                            'Content-Length': data.length,
-                            'Content-Type': 'application/x-javascript' });
-
-                    } else if (req.url.indexOf('.css')>0) {
-                        logger.debug("HTTP RESP - "+req.url);
-                        res.writeHead(200, {
-                            'Content-Length': data.length,
-                            'Content-Type': 'text/css' });
-
-                    }
-                    else{
-                        res.writeHead(200);
-                    }
-                    res.end(data);
-                });
+                    break;
             }
         }).listen(parameters.port);
 
--- util/rest.js
+++ util/rest.js
@@ -24,18 +24,6 @@ define([
     function Rest(_configuration){
         var _tokens = {};
         var _projects = {};
-        /*var _database = new Commit(
-                new Cache(
-                    new Failsafe(
-                        new Client(
-                            {
-                                host:_configuration.host,
-                                port:_configuration.port
-                            }
-                        ),{}
-                    ),{}
-                ),{}
-            );*/
         var _database = new Mongo({
             host: _configuration.ip,
             port: _configuration.port,
@@ -48,30 +36,212 @@ define([
             }
         };
 
+        var specialCharHandling = function(text){
+            text = text.replace(/%23/g,'#');
+            text = text.replace(/%2f/g,'/');text = text.replace(/%2F/g,'/');
+            return text;
+        };
+        var addingSpecialChars = function(text){
+            text = text.replace(/#/g,'%23');
+            text = text.replace(/\//g,'%2F');
+            return text;
+        };
+        var loadingNode = function(core,node,callback){
+            //we have to load all pointers, all collections and all the stuff which has connection to the node
+            var outnode = {};
+
+            //attributes
+            outnode.attribute={};
+            var names = core.getAttributeNames(node);
+            for(var i=0;i<names.length;i++){
+                outnode.attribute[names[i]] = core.getAttribute(node,names[i]);
+            }
+
+            //registry
+            outnode.registry={};
+            names = core.getRegistryNames(node);
+            for(var i=0;i<names.length;i++){
+                outnode.registry[names[i]] = core.getRegistry(node,names[i]);
+            }
+
+            //children
+            /*outnode.children={};
+            names = core.getChildrenRelids(node);
+            for(var i=0;i<names.length;i++){
+                outnode.children[names[i]] = node.data[names[i]];
+            }*/
+            outnode.children=core.getChildrenPaths(node);
+
+            //pointers and collections
+            outnode.pointer = {};
+            outnode.collection = {};
+            var error = null;
+            var needed = 0;
+            var allLoaded = function(){
+                if(error){
+                    callback(error,null);
+                } else {
+                    callback(null,outnode);
+                }
+            };
+
+            names = core.getPointerNames(node);
+            needed += names.length;
+
+            var names2 = core.getCollectionNames(node);
+            needed += names2.length;
+
+            var loadPointer= function(name){
+                core.loadPointer(node,name,function(err,pointer){
+                    if(err){
+                        error = err;
+                    } else {
+                        //outnode.pointer[name] = pointer.data._id;
+                        outnode.pointer[name] = core.getPath(pointer);
+                    }
+
+                    if(--needed === 0){
+                        allLoaded();
+                    }
+                });
+            };
+            var loadCollection = function(name){
+                core.loadCollection(node,name,function(err,collection){
+                    if(err){
+                        error = err;
+                    } else {
+                        outnode.collection[name] = [];
+                        for(var i=0;i<collection.length;i++){
+                            //outnode.collection[name].push(collection[i].data._id);
+                            outnode.collection[name].push(core.getPath(collection[i]));
+                        }
+                    }
+
+                    if(--needed === 0){
+                        allLoaded();
+                    }
+                });
+            };
+
+            if(needed >0){
+                for(var i=0;i<names.length;i++){
+                    loadPointer(names[i]);
+                }
+
+                for(var j=0;j<names2.length;j++){
+                    loadCollection(names2[j]);
+                }
+            } else {
+                allLoaded();
+            }
+        };
+
         //available commands
         var getProjects = function(callback){
             _database.getProjectNames(callback);
+        };
+        var getBranches = function(projName,callback){
+            _projects[projName].getBranchNames(callback);
+        };
+        var getSingleItem = function(projName,hash,callback){
+            var core = new Core(_projects[projName]);
+            core.loadRoot(hash,function(err,root){
+                if(err){
+                    callback(err,null);
+                } else {
+                    if(root.data.type && root.data.type === 'commit'){
+                        callback(null,root);
+                    } else {
+                        loadingNode(core,root,callback);
+                    }
+                }
+            });
+        };
+        var getItemByPath = function(projName,dataArray,callback){
+            var core = new Core(_projects[projName]);
+            var hash = dataArray[0];
+            var path = "";
+            for(var i=1;i<dataArray.length;i++){
+                path +='/'+dataArray[i]+"";
+            }
+            core.loadRoot(hash,function(err,root){
+                if(err){
+                    callback(err,null);
+                } else {
+                    if(root.data.type && root.data.type === 'commit'){
+                        callback(null,root);
+                    } else {
+                        core.loadByPath(root,path,function(err,node){
+                            if(err){
+                                callback(err,null);
+                            } else {
+                                loadingNode(core,node,callback);
+                            }
+                        });
+                    }
+                }
+            });
         };
 
 
 
 
-        var processURI = function(uri,callback){
+        var processGET = function(uri,callback){
+            var myCallback = function(err,data){
+                if(!err && data){
+                    data = JSON.stringify(data);
+                    data = addingSpecialChars(data);
+                    data = JSON.parse(data);
+                }
+                callback(err,data);
+            };
             var uriArray = uri.split('/');
             var startindex = uriArray.indexOf('rest')+1;
             if(startindex>0 && startindex<uriArray.length){
                 if(uriArray[startindex] === 'projects'){
-                    getProjects(callback);
+                    getProjects(myCallback);
                 } else {
-                    callback('not implemented yet',null);
+                    var projName = uriArray[startindex++];
+                    var gotProject = function(){
+                        if(uriArray[startindex] === 'branches'){
+                            getBranches(projName,myCallback);
+                        } else {
+                            var base = uriArray[startindex];
+                            base = specialCharHandling(base);
+                            base = base.split('/');
+                            if(base.length === 1){
+                                //single hashed item
+                                getSingleItem(projName,base[0],myCallback);
+                            } else {
+                                //hash followed by path
+                                getItemByPath(projName,base,myCallback);
+                            }
+                        }
+                    };
+                    if(_projects[projName]){
+                        gotProject();
+                    } else {
+                        _database.openProject(projName,function(err,proj){
+                            if(!err && proj){
+                                _projects[projName] = proj;
+                                gotProject();
+                            } else {
+                                callback(err,null);
+                            }
+                        });
+                    }
                 }
             } else {
                 callback('wrong URI',null);
             }
         };
+        var processPOST = function(uri,data,callback){
+
+        };
 
         return {
-            processURI : processURI,
+            processGET : processGET,
+            processPOST : processPOST,
             open: _database.openDatabase
         }
     }
