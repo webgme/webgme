13ff57ece836e15c870f6b54ad066d0275a1cd7c
rkereskenyi <robert.kereskenyi@gmail.com>
2013-08-30 14:40:17 -0500

    introduce getURLParameterByName to get query string URL parameter by its name

--- client/js/util.js
+++ client/js/util.js
@@ -93,6 +93,13 @@ define([], function () {
             }
 
             return result;
+        },
+
+        getURLParameterByName: function (name) {
+            name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                results = regex.exec(location.search);
+            return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
         }
     }
 });
