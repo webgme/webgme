8e5c18acb3e8efe42e849266a4393f822d404562
kecso <tamas.kecskes@vanderbilt.edu>
2013-09-05 15:51:11 -0500

    user data manager command line tool have been completed

--- bin/usermanager.js
+++ bin/usermanager.js
@@ -72,7 +72,22 @@ if (typeof define !== "function") {
             if(COMMON.getParameters("info")){
                 //info command
                 done = TASYNC.call(infoPrint,core,_startHash);
+            } else if(COMMON.getParameters("addproject")){
+                var projpars = COMMON.getParameters("addproject")
+                done = TASYNC.call(addProject,core,_startHash,projpars[0],projpars[1],projpars[2]);
+            } else if(COMMON.getParameters("removeproject")){
+                var projpars = COMMON.getParameters("removeproject")
+                done = TASYNC.call(removeProject,core,_startHash,projpars[0],projpars[1]);
+            } else if(COMMON.getParameters("adduser")){
+                var projpars = COMMON.getParameters("adduser")
+                done = TASYNC.call(addUser,core,_startHash,projpars[0],projpars[1],projpars[2]);
+            } else if(COMMON.getParameters("removeuser")){
+                var projpars = COMMON.getParameters("removeuser")
+                done = TASYNC.call(removeUser,core,_startHash,projpars[0],projpars[1]);
             }
+
+            done = TASYNC.call(COMMON.closeProject, done);
+            done = TASYNC.call(COMMON.closeDatabase, done);
             return done;
         }
 
@@ -81,25 +96,49 @@ if (typeof define !== "function") {
             var commitHash = project.getBranchHash(branch,null);
             var done = TASYNC.call(project.loadObject,commitHash);
             done = TASYNC.call(function(object){
-                console.log('getRootHashOfBranch',branch,object.root);
                 _branch = branch;
                 return object.root;
             },done);
             return done;
         }
 
-        function getRootHashOfCommit (commit){
+        function getCommitHashOfBranch (branch){
             var project = COMMON.getProject();
-            return TASYNC.call(function(object){
-                console.log('getRootHashOfCommit',commit,object.root);
-                return object.root;
-            },TASYNC.call(project.loadObject,commit));
+            return project.getBranchHash(branch,null);
         }
 
+        function makeCommit (newroothash, parentcommithash, msg) {
+            var project = COMMON.getProject();
+            return project.makeCommit([parentcommithash], newroothash, msg);
+        }
+
+        function writeBranch (oldhash,newhash) {
+            var project = COMMON.getProject();
+            var done = project.setBranchHash(_branch, oldhash, newhash);
+            return TASYNC.call(function () {
+                console.log("Commit " + newhash + " written to branch " + _branch);
+            }, done);
+        }
+
+        function persist (core,root) {
+            console.log("Waiting for objects to be saved ...");
+            var done = core.persist(root);
+            var hash = core.getHash(root);
+            return TASYNC.join(hash, done);
+        }
+
+        function saveModifications(newroothash,msg){
+            var oldcommithash = getCommitHashOfBranch(_branch);
+            var newcommit = TASYNC.call(makeCommit,newroothash,oldcommithash,msg);
+            return TASYNC.call(writeBranch,oldcommithash,newcommit);
+
+        }
+
+        //commands
         function infoPrint(core,roothash){
             function printUser(userObject){
                 var outstring = "";
-                outstring+="username: "+core.getAttribute(userObject,'name')+" | ";
+                outstring+="username: "+core.getAttribute(userObject,'name')+"  \t| ";
                 var userProjects = core.getRegistry(userObject,'projects');
                 var hasproject=false;
                 for(var i in userProjects){
@@ -109,6 +148,9 @@ if (typeof define !== "function") {
                     }
                     outstring+= " "+i;
                 }
+
+                outstring += " \t| can create: "+ (core.getRegistry(userObject,'create') === true ? "true" : "false");
+
                 return outstring;
             }
 
@@ -128,9 +170,172 @@ if (typeof define !== "function") {
             return done;
 
         }
+        function addProject(core,roothash,username,projectname,rights){
+            function iterateChildren(parentObject){
+                var children = core.loadChildren(parentObject);
+                return TASYNC.call(function(objectArray){
+                    var i= 0,child=null;
+                    while(i<objectArray.length && child === null){
+                        if(core.getAttribute(objectArray[i],'name') === username){
+                            child = objectArray[i];
+                        }
+                        i++;
+                    }
+
+                    if(child){
+                        var projects = core.getRegistry(child,'projects');
+                        if(projects === null || projects === undefined){
+                            projects = {};
+                        } else {
+                            projects = JSON.parse(JSON.stringify(projects));
+                        }
+
+                        if(projects[projectname] === null || projects[projectname] === undefined){
+                            projects[projectname] = {};
+                        }
+
+                        if(rights.indexOf('r') !== -1){
+                            projects[projectname].read = true;
+                        } else {
+                            projects[projectname].read = false;
+                        }
+                        if(rights.indexOf('w') !== -1){
+                            projects[projectname].write = true;
+                        } else {
+                            projects[projectname].write = false;
+                        }
+                        if(rights.indexOf('d') !== -1){
+                            projects[projectname].delete = true;
+                        } else {
+                            projects[projectname].delete = false;
+                        }
+
+                        core.setRegistry(child,'projects',projects);
+                        var newroothash = persist(core,parentObject);
+                        return TASYNC.call(saveModifications,newroothash,"project "+projectname+" has been added to user "+username);
+
+                    } else {
+                        return null;
+                    }
+                },children);
+            }
+
+
+
+            var root = core.loadRoot(roothash);
+            var done = TASYNC.call(iterateChildren,root);
+
+            return done;
+        }
+        function removeProject(core,roothash,username,projectname){
+            function iterateChildren(parentObject){
+                var children = core.loadChildren(parentObject);
+                return TASYNC.call(function(objectArray){
+                    var i= 0,child=null;
+                    while(i<objectArray.length && child === null){
+                        if(core.getAttribute(objectArray[i],'name') === username){
+                            child = objectArray[i];
+                        }
+                        i++;
+                    }
+
+                    if(child){
+                        var projects = core.getRegistry(child,'projects');
+                        if(projects === null || projects === undefined){
+                            projects = {};
+                        } else {
+                            projects = JSON.parse(JSON.stringify(projects));
+                        }
+                        if(projects[projectname]){
+                            delete projects[projectname];
+                            core.setRegistry(child,'projects',projects);
+                            var newroothash = persist(core,parentObject);
+                            return TASYNC.call(saveModifications,newroothash,"project "+projectname+" has been removed from user "+username);
+                        } else {
+                            return null;
+                        }
+                    } else {
+                        return null;
+                    }
+                },children);
+            }
+
+
+
+            var root = core.loadRoot(roothash);
+            var done = TASYNC.call(iterateChildren,root);
+
+            return done;
+        }
+        function addUser(core,roothash,username,puk,cancreate){
+            function iterateChildren(parentObject){
+                var children = core.loadChildren(parentObject);
+                return TASYNC.call(function(objectArray){
+                    var i= 0,child=null;
+                    while(i<objectArray.length && child === null){
+                        if(core.getAttribute(objectArray[i],'name') === username){
+                            child=objectArray[i];
+                        }
+                        i++;
+                    }
+
+                    if(child === null){
+                        child = core.createNode(parentObject);
+                        core.setAttribute(child,'name',username);
+                        core.setRegistry(child,'create',cancreate === 'true');
+                        core.setRegistry(child,'projects',{});
+                        var key = require('fs').readFileSync(puk,'utf8');
+                        core.setRegistry(child,'puk',key);
+                        var newroothash = persist(core,parentObject);
+                        return TASYNC.call(saveModifications,newroothash,"a new user"+username+" has been added to the database");
+                    } else {
+                        core.setRegistry(child,'create',cancreate === true);
+                        var key = require('fs').readFileSync(puk,'utf8');
+                        core.setRegistry(child,'puk',key);
+                        var newroothash = persist(core,parentObject);
+                        return TASYNC.call(saveModifications,newroothash,"the basic data of user "+username+" has been changed");
+                    }
+                    return null;
+                },children);
+            }
+
+
+
+            var root = core.loadRoot(roothash);
+            var done = TASYNC.call(iterateChildren,root);
+
+            return done;
+        }
+        function removeUser(core,roothash,username){
+            function iterateChildren(parentObject){
+                var children = core.loadChildren(parentObject);
+                return TASYNC.call(function(objectArray){
+                    var i= 0,child=null;
+                    while(i<objectArray.length && child === null){
+                        if(core.getAttribute(objectArray[i],'name') === username){
+                            child=objectArray[i];
+                        }
+                        i++;
+                    }
+
+                    if(child){
+                        core.deleteNode(child);
+                        var newroothash = persist(core,parentObject);
+                        return TASYNC.call(saveModifications,newroothash,"user "+username+" has been removed from database");
+                    }
+                    return null;
+                },children);
+            }
+
+
+
+            var root = core.loadRoot(roothash);
+            var done = TASYNC.call(iterateChildren,root);
+
+            return done;
+        }
 
     });
-
     return;
 }
 
