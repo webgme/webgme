a337b1a9bc5c48c2ce46a0435d28786e83c071de
kecso <tamas.kecskes@vanderbilt.edu>
2013-10-08 09:19:53 -0500

    bugfix (descriptorcore was missing)

--- core/newcore.js
+++ core/newcore.js
@@ -4,15 +4,15 @@
  * Author: Tamas Kecskes
  */
 
-define([ "core/newcorerel",'core/newcorewrap','core/newsetcore','core/newrootcore','core/guidcore','core/nullpointercore','core/coreunwrap'], function (Rel,Wrap,Set,Root,Guid,NullPtr,UnWrap) {
+define([ "core/newcorerel",'core/newcorewrap','core/newsetcore','core/newrootcore','core/guidcore','core/nullpointercore','core/coreunwrap', 'core/descriptorcore'], function (Rel,Wrap,Set,Root,Guid,NullPtr,UnWrap,Descriptor) {
     "use strict";
     function syncCore(storage,options){
-        var core = new Root(new Guid(new NullPtr(new Set( new Wrap(new Rel(storage,options))))));
+        var core = new Root(new Guid(new Descriptor(new NullPtr(new Set( new Wrap(new Rel(storage,options)))))));
         return core;
     }
 
     function asyncCore(storage,options){
-        var core = new UnWrap(new Root(new Guid(new NullPtr(new Set( new Wrap(new Rel(storage,options)))))));
+        var core = new UnWrap(new Root(new Guid(new Descriptor(new NullPtr(new Set( new Wrap(new Rel(storage,options))))))));
         return core;
     }
 
