0bed791642685d5b970b7250a5f558573311f993
kecso <tamas.kecskes@vanderbilt.edu>
2013-10-09 11:31:09 -0500

    cleanup of core directory

--- core/core.js
+++ core/core.js
@@ -4,7 +4,7 @@
  * Author: Tamas Kecskes
  */
 
-define([ "core/newcorerel",'core/newcorewrap','core/newsetcore','core/newrootcore','core/guidcore','core/nullpointercore','core/coreunwrap', 'core/descriptorcore'], function (Rel,Wrap,Set,Root,Guid,NullPtr,UnWrap,Descriptor) {
+define([ "core/corerel",'core/corewrap','core/setcore','core/rootcore','core/guidcore','core/nullpointercore','core/coreunwrap', 'core/descriptorcore'], function (Rel,Wrap,Set,Root,Guid,NullPtr,UnWrap,Descriptor) {
     "use strict";
     function syncCore(storage,options){
         var core = new Root(new Guid(new Descriptor(new NullPtr(new Set( new Wrap(new Rel(storage,options)))))));
