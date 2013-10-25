b1d8a3504b75e6015610a0521f44e7d09ac50c25
rkereskenyi <robert.kereskenyi@gmail.com>
2013-10-25 11:18:23 -0500

    cleanup

--- client/js/Widgets/DiagramDesigner/DiagramDesignerWidget.ContextMenu.js
+++ client/js/Widgets/DiagramDesigner/DiagramDesignerWidget.ContextMenu.js
@@ -13,18 +13,12 @@ define(['lib/jquery/jquery.contextMenu'], function () {
 
         $.contextMenu({
             selector: '.diagram-designer',
-            /*position: function(selector, x, y) {
-             var _offset = selector.$trigger.find('.dynatree-title').offset();
-             selector.$menu.css({top: _offset.top + 10, left: _offset.left - 10});
-             },*/
             build: function($trigger, e) {
                 // this callback is executed every time the menu is to be shown
                 // its results are destroyed every time the menu is hidden
                 // e is the original contextmenu event, containing e.pageX and e.pageY (amongst other data)
                 return {
                     callback: function(key, options) {
-                        /*var node = $.ui.dynatree.getNode(options.$trigger),
-                         m = "clicked: '" + key + "' on '" + node.data.title + " (" + node.data.key + ")'";*/
                         alert('clicked: ' + key + ' on: ' + JSON.stringify(options.$trigger));
                     },
                     items: itemsCallback($trigger, e)
