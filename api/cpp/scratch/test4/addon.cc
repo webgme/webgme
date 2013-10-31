880e2bd4170fda00107c6ef9f8353860b1074bf7
Addisu Z. Taddese <addisu@gmail.com>
2013-10-31 16:50:03 -0500

    Made C++ synchronous using fibers

--- api/cpp/scratch/test4/addon.cc
+++ api/cpp/scratch/test4/addon.cc
@@ -16,27 +16,35 @@ namespace Api {
     HandleScope scope;
     return scope.Close(object->ToObject()->Get(String::New(attr)));
   }
+  inline Handle<Function> GetObjectMethod(Handle<Value> object, const char * attr){
+    HandleScope scope;
+    return scope.Close(Handle<Function>::Cast(object->ToObject()->Get(String::New(attr))));
+  }
 
   Handle<Value> CallTasyncMethod(Handle<Function> method, const unsigned int argc = 0, Handle<Value> argv[] = NULL){
     HandleScope scope;
-    CERR << "Calling with Tasync: " << std::endl;
     // create a new array of arguments, with the first argument as the function.
     Handle<Value> new_argv[argc+1] ;
     new_argv[0] = method;
     for(unsigned int i=0; i < argc; i++){
       new_argv[i+1] = argv[i];
     }
-    return scope.Close(node::MakeCallback(tasync, "debugcall", argc+1, new_argv));
+    return scope.Close(node::MakeCallback(tasync, "call_sync", argc+1, new_argv));
   }
 
   Handle<Value> CallCommonMethod(const char * method, int argc = 0, Handle<Value> argv[] = NULL){
     HandleScope scope;
-    CERR << "Calling: " << method << ", argc: " << argc << std::endl;
+    //CERR << "Calling: " << method << ", argc: " << argc << std::endl;
     Local<Function> func = Local<Function>::Cast(common->Get(String::New(method)));
     return scope.Close(CallTasyncMethod(func, argc, argv));
   }
-
-
+  Handle<Value> CallObjectMethod(Handle<Value> object, const char * method, int argc = 0, Handle<Value> argv[] = NULL){
+    HandleScope scope;
+    //CERR << "Calling: " << method << ", argc: " << argc << std::endl;
+    Handle<Function> func = GetObjectMethod(object, method);
+    return scope.Close(CallTasyncMethod(func, argc, argv));
+    
+  }
 }
 
 /*
@@ -76,72 +84,44 @@ class Interpreter: public node::ObjectWrap {
       Api::common = Persistent<Object>::New(args[0]->ToObject());
       Api::tasync= Persistent<Object>::New(args[1]->ToObject());
 
-      CERR << "THIS: " << args.This()->GetIdentityHash() << std::endl;
       intrp->Wrap(args.This());
-      CERR << "THIS2: " << args.This()->GetIdentityHash() << std::endl;
 
       return args.This();
     }
 
 
-    static Handle<Value> PrintChildrenImpl(const Arguments& args){
+    static Handle<Value> InvokeEx(const Arguments& args){
       HandleScope scope;
-      CERR << args.Length() << std::endl;
-      Local<Array> children =  Local<Array>::Cast(args[0]);
-      CERR << "Array length: " << children->Length() << std::endl;
+      Handle<Value> proj = Api::CallCommonMethod("getProject");
+
+      const int argc = 2;
+
+      Handle<Value> argv[argc] = {String::New("master"), Null()};
+      Handle<Value> hash = Api::CallObjectMethod(proj, "getBranchHash", argc, argv);
+
+      CERR << "Hash: " << *String::AsciiValue(hash->ToString()) << std::endl;
+      argv[0] = hash;
+
+      Handle<Value> commit =Api::CallObjectMethod(proj, "loadObject", 1, argv); 
       Handle<Value> core = Api::CallCommonMethod("getCore");
-      CERR << "Attributes" << std::endl;
-      const unsigned argc = 2;
-      Handle<Value> argv[argc]={children->Get(0),String::New("name")};
+
+      argv[0] = Api::GetObjectAttr(commit, "root");
+      Handle<Value> root = Api::CallObjectMethod(core, "loadRoot", 1, argv);
+      argv[0] = root;
+      Handle<Array> children = Handle<Array>::Cast(Api::CallObjectMethod(core, "loadChildren", 1, argv));
+
+      CERR << "Children length: " << children->Length() << std::endl;
+      argv[1]=String::New("name");
+
       for(unsigned int i=0; i < children->Length(); i++){
         argv[0] = children->Get(i);
-        Handle<Value> name = node::MakeCallback(core->ToObject(), "getAttribute", argc, argv);
+        Handle<Value> name = Api::CallObjectMethod(core, "getAttribute", argc, argv);
         std::cout << *String::AsciiValue(name) << std::endl;
       }
+
       return scope.Close(Undefined());
     }
 
-    static Handle<Value> TraverseTree(const Arguments& args){
-      HandleScope scope;
-      CERR << args.Length() << std::endl;
-      CERR << "THIS: " << args.This()->GetIdentityHash() << std::endl;
-      //CERR << "Hash: " << *String::AsciiValue(args[0]->ToString()) << std::endl;
-      Handle<Value> core = Api::CallCommonMethod("getCore");
-      const int argc = 1;
-      Handle<Value> argv[argc] = {Api::GetObjectAttr(args[0],"root")};
-      Handle<Value> rootFuture = node::MakeCallback(core->ToObject(), "loadRoot", argc, argv);
-      Handle<Function> loadChildren = Handle<Function>::Cast(Api::GetObjectAttr(core, "loadChildren"));
-      argv[0] = rootFuture;
-      Handle<Value> childrenFuture = Api::CallTasyncMethod(loadChildren, argc, argv);
-      argv[0] = childrenFuture;
-      return scope.Close(Api::CallTasyncMethod(FunctionTemplate::New(PrintChildrenImpl)->GetFunction(), argc, argv));
-    }
-
-    static Handle<Value> InvokeEx(const Arguments& args){
-      HandleScope scope;
-      //CERR << args.Length() << std::endl;
-      //CERR << "THIS: " << args.This()->GetIdentityHash() << std::endl;
-      //CERR << "Common: " << Api::common->GetIdentityHash() << std::endl;
-      //CERR << "Tasync: " << Api::tasync->GetIdentityHash() << std::endl;
-      return scope.Close(PrintChildren(args));
-    }
-
-    static Handle<Value> GetCommit(const Arguments& args){
-      HandleScope scope;
-      CERR << "THIS: " << args.This()->GetIdentityHash() << std::endl;
-      Handle<Value> proj = Api::CallCommonMethod("getProject");
-      ListPropertyNames(proj->ToObject());
-      const int argc = 2;
-      Handle<Value> argv[argc] = {String::New("master"), Null()};
-      Handle<Value> hashFuture = node::MakeCallback(proj->ToObject(), "getBranchHash", argc, argv);
-      const int argc_2 = 1;
-      Handle<Value> argv_2[argc_2] = {hashFuture};
-      Handle<Function> loadObject = Handle<Function>::Cast(Api::GetObjectAttr(proj, "loadObject"));
-      Handle<Value> newObject = Api::CallTasyncMethod(loadObject, argc_2, argv_2);
-      argv_2[0] = newObject;
-      // TASYNC.call(traversetree, newObject)
-      return scope.Close(Api::CallTasyncMethod(FunctionTemplate::New(TraverseTree)->GetFunction(), argc_2, argv_2));
-    }
 
     static Handle<Value> PrintChildren(const Arguments& args){
       HandleScope scope;
