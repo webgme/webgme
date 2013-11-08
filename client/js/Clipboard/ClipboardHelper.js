2b493bd5ab77b419fa5d58388d54e28e8be37506
rkereskenyi <robert.kereskenyi@gmail.com>
2013-11-08 10:19:58 -0600

    bugfix: message to the user if feature not available in browser

--- client/js/Clipboard/ClipboardHelper.js
+++ client/js/Clipboard/ClipboardHelper.js
@@ -8,7 +8,9 @@
 define([], function () {
 
     var CLIPBOARD_HELPER_CLASS = 'webgme-clipboard-helper',
-        _txtArea;
+        _txtArea,
+        TEXT_AREA_CONTENT = 'webgme-clipboard',
+        UPDATE_BROWSER_MESSAGE = 'Your browser seems to be out of date :(. Please update your browser to the latest and greatest version!';
 
     var _initializeClipboardHelper = function () {
         if (_txtArea) {
@@ -46,35 +48,45 @@ define([], function () {
             _initializeClipboardHelper();
         }
 
-        _txtArea.val('webgme-hack').select().focus();
+        _txtArea.val(TEXT_AREA_CONTENT).select().focus();
     };
 
 
     var _onCopy = function (event) {
-        var activePanel = WebGMEGlobal.PanelManager.getActivePanel();
-        if (activePanel && _.isFunction(activePanel.onCopy)) {
-            var clipboardData = _getClipboardData(event);
+        var activePanel = WebGMEGlobal.PanelManager.getActivePanel(),
+            clipboardData = _getClipboardData(event);
 
-            var data = activePanel.onCopy();
-            if (typeof data !== "string") {
-                data = JSON.stringify(data);
+        if (clipboardData) {
+            if (activePanel && _.isFunction(activePanel.onCopy)) {
+                var data = activePanel.onCopy();
+                if (typeof data !== "string") {
+                    data = JSON.stringify(data);
+                }
+                clipboardData.setData('text', data);
             }
-            clipboardData.setData('text', data);
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            alert(UPDATE_BROWSER_MESSAGE);
         }
-        event.preventDefault();
-        event.stopPropagation();
     };
 
     var _onPaste = function (event) {
-        var activePanel = WebGMEGlobal.PanelManager.getActivePanel();
-        if (activePanel && _.isFunction(activePanel.onPaste)) {
-            var clipboardData = _getClipboardData(event);
+        var activePanel = WebGMEGlobal.PanelManager.getActivePanel(),
+            clipboardData = _getClipboardData(event);
 
-            var data = clipboardData.getData('text');
-            activePanel.onPaste(data);
+        if (clipboardData) {
+            if (activePanel && _.isFunction(activePanel.onPaste)) {
+
+
+                var data = clipboardData.getData('text');
+                activePanel.onPaste(data);
+            }
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            alert(UPDATE_BROWSER_MESSAGE);
         }
-        event.preventDefault();
-        event.stopPropagation();
     };
 
     var _getClipboardData = function (event) {
