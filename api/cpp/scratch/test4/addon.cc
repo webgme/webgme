fc8075c9dccc37dd9c61dae42d0d56a4b2507ce0
Addisu Z. Taddese <addisu@gmail.com>
2013-11-12 17:44:58 -0600

    Implemented functors for javascript functions

--- api/cpp/scratch/test4/addon.cc
+++ api/cpp/scratch/test4/addon.cc
@@ -78,77 +78,69 @@ namespace Api {
 }
 
 /**
- * Create a hash/map of all available javascript methods of an object so that these methods can be called from C++
+ * Create a hash/map of all available javascript methods of an object so object_ these methods can be called from C++
  * conveniently. The Call method of this class looks for a javascript method with a name 'method_name' in the underlying
  * map. If found, this javascript method will be called using a synchronous calling method utilizing Tasync. The
  * class overloads the () operator to provide the same functionality as the Call method.
  */
-class FunctionMap
-{
+class ObjectMap {
 public:
   /**
    * Build map of all available javascript methods of the object
    */
-  typedef std::map<std::string, Persistent<Function> > StringToFunctionMap;
-  FunctionMap (Handle<Object> o){
+  ObjectMap (Handle<Value> o){
     HandleScope scope;
-    that = o;
-    Local<Array> propNames = o->GetPropertyNames();
-    for(size_t i=0; i < propNames->Length(); i++){
-      Local<Value> name = propNames->Get(i);
-      if(o->Get(name)->IsFunction()){
-        // Have to convert to string so that char * is copied from the Handle. This wouldn't work if the char * is
-        // passed to the map to build a char * -> Function map. Instead we now have a string -> Function map.
-        std::string key = *String::AsciiValue(name);
-        Handle<Function> func = Handle<Function>::Cast(o->Get(name));
-        func->SetName(String::New(key.c_str()));
-        //CERR << "Is method? " << *String::AsciiValue(func->GetName()) << std::endl;
-        func_map_[key] = Persistent<Function>::New(func);
-      }
-    }
-
-    CERR << "Mapped "<< func_map_.size() << " functions"<< std::endl;
-    //for(StringToFunctionMap::iterator it=func_map_.begin(); it!= func_map_.end(); it++){
-    //  //CERR << "Key: " << *String::AsciiValue(it->first) << std::endl;
-    //  CERR << "Key: " << it->first << " Value: " <<*String::AsciiValue(it->second->GetName())  << std::endl;
-    //}
-
+    object_ = o->ToObject();
   }
-  virtual ~FunctionMap (){};
+  virtual ~ObjectMap (){};
 
-  /**
-   * Search for javascript method method_name in func_map_ and call it
-   */
-  Handle<Value> Call(const char * method_name, 
-                     const unsigned int argc = 0, 
+  Handle<Value> operator[](const char* key){
+    HandleScope scope;
+    return scope.Close(object_->Get(String::New(key)));
+  }
+
+  Handle<Value> GetObject(){
+    return object_;
+  }
+
+protected:
+  Handle<Object> object_;
+};
+
+class TasyncCallable: public ObjectMap{
+public:
+
+  TasyncCallable (Handle<Value> o, const char* key):ObjectMap(o){
+    func_ = Handle<Function>::Cast(o->ToObject()->Get(String::New(key)));
+  }
+
+  Handle<Value> Call(const unsigned int argc = 0, 
                      Handle<Value> argv[] = NULL){
 
     HandleScope scope;
-    //StringToFunctionMap::iterator it = func_map_.find(String::New(method_name));
-    StringToFunctionMap::iterator it = func_map_.find(std::string(method_name));
-    if(it == func_map_.end()){
-      // TODO: Throw an exception
-      CERR << "Method: " << method_name << " not found" << std::endl;
-    } else {
-      //CERR << "Calling Method: " << method_name << std::endl;
-      return scope.Close(Api::CallTasyncMethod(it->second, argc, argv, that));
-    }
-    return scope.Close(Undefined());
+
+    return scope.Close(Api::CallTasyncMethod(func_, argc, argv, object_));
   }
 
   /**
    * Operator () override
    */
-
-  Handle<Value> operator()(const char * method_name, 
-                     const unsigned int argc = 0, 
+  Handle<Value> operator()(const unsigned int argc = 0, 
                      Handle<Value> argv[] = NULL){
-    return Call(method_name, argc, argv);
+    return Call(argc, argv);
   }
+protected:
+  Handle<Function> func_;
+};
 
-private:
-  StringToFunctionMap func_map_;
-  Handle<Object> that;
+class FunctionMap: public ObjectMap
+{
+public:
+  FunctionMap (Handle<Value> o):ObjectMap(o){}
+  TasyncCallable operator[](const char* key){
+    //return TasyncCallable(object_->Get(String::New(key)));
+    return TasyncCallable(object_, key);
+  }
 };
 
 class Interpreter: public node::ObjectWrap {
@@ -185,38 +177,37 @@ class Interpreter: public node::ObjectWrap {
     static Handle<Value> InvokeEx(const Arguments& args){
       HandleScope scope;
       Handle<Value> proj = Api::CallCommonMethod("getProject");
+      Handle<Value> core = Api::CallCommonMethod("getCore");
       FunctionMap projmap(proj->ToObject());
+      FunctionMap coremap(core->ToObject());
 
       const int argc = 2;
 
       Handle<Value> argv[argc] = {String::New("master"), Null()};
-      Handle<Value> hash = projmap("getBranchHash", argc, argv);
+      Handle<Value> hash = projmap["getBranchHash"](argc, argv);
 
       CERR << "Hash: " << *String::AsciiValue(hash->ToString()) << std::endl;
       argv[0] = hash;
 
-      //Handle<Value> commit =Api::CallObjectMethod(proj, "loadObject", 1, argv); 
-      Handle<Value> commit = projmap("loadObject", 1, argv); 
+      //Handle<Value> commit = projmap("loadObject", 1, argv); 
+      ObjectMap commit(projmap["loadObject"](1, argv));
 
-      //FunctionMap* coremap = new FunctionMap(core->ToObject());
-      Handle<Value> core = Api::CallCommonMethod("getCore");
-      FunctionMap coremap(core->ToObject());
+      //argv[0] = Api::GetObjectAttr(commit, "root");
+      argv[0] = commit["root"];
+      Handle<Value> root = coremap["loadRoot"](1, argv);
 
-      argv[0] = Api::GetObjectAttr(commit, "root");
-      Handle<Value> root = coremap("loadRoot", 1, argv);
       argv[0] = root;
-      Handle<Array> children = Handle<Array>::Cast(coremap("loadChildren", 1, argv));
+      Handle<Array> children = Handle<Array>::Cast(coremap["loadChildren"](1, argv));
 
       CERR << "Children length: " << children->Length() << std::endl;
       argv[1]=String::New("name");
 
       for(unsigned int i=0; i < children->Length(); i++){
         argv[0] = children->Get(i);
-        Handle<Value> name = coremap("getAttribute", argc, argv);
+        Handle<Value> name = coremap["getAttribute"](argc, argv);
         std::cout << *String::AsciiValue(name) << std::endl;
       }
 
-      //delete coremap;
       return scope.Close(Undefined());
     }
 
