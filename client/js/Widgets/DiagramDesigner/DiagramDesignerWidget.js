5918f39717539c8e70cd47bd81836a023b2ad264
rkereskenyi <robert.kereskenyi@gmail.com>
2013-11-13 11:59:20 -0600

    fire ON_COMPONENT_CREATE and ON_COMPONENT_UPDATE for connections too

--- client/js/Widgets/DiagramDesigner/DiagramDesignerWidget.js
+++ client/js/Widgets/DiagramDesigner/DiagramDesignerWidget.js
@@ -767,7 +767,9 @@ define(['logManager',
             }
         };
         dispatchEvents(this._insertedDesignerItemIDs, this.events.ON_COMPONENT_CREATE);
+        dispatchEvents(this._insertedConnectionIDs, this.events.ON_COMPONENT_CREATE);
         dispatchEvents(this._updatedDesignerItemIDs, this.events.ON_COMPONENT_UPDATE);
+        dispatchEvents(this._updatedConnectionIDs, this.events.ON_COMPONENT_UPDATE);
         /*********************/
 
 
