6dbfecb722278e250bc5b86bdfeb203af2646689
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2013-12-01 02:46:07 -0600

    alert messages are shown after validations

--- client/js/ConstraintManager.js
+++ client/js/ConstraintManager.js
@@ -6,6 +6,7 @@ define(['logManager', 'js/NodePropertyNames', 'text!./ConstraintManager.html'],
   var self, logger, client;
   var territoryId, selfPatterns, toBeValidated;
   var constraintManagerDialog;
+  var $cmb;
 
   var ConstraintManager = function(client_) {
     self = this;
@@ -13,15 +14,23 @@ define(['logManager', 'js/NodePropertyNames', 'text!./ConstraintManager.html'],
     logger = logManager.create('ConstraintManager');
     client = client_;
     constraintManagerDialog = $(constraintManagerDialogTemplate);
+    constraintManagerDialog.on('hidden', function() {
+      $cmb.html('');
+    });
   };
 
   ConstraintManager.prototype.validateAll = function(id) {
+
+    constraintManagerDialog.one('shown', function () {
+      toBeValidated = id;
+      territoryId = client.addUI(self, true);
+      selfPatterns = {};
+      selfPatterns[id] = { children: Number.MAX_VALUE };
+      $cmb = $("#ConstraintModalBody");
+      client.updateTerritory(territoryId, selfPatterns);
+    });
     constraintManagerDialog.modal('show');
-    toBeValidated = id;
-    territoryId = client.addUI(this, true);
-    selfPatterns = {};
-    selfPatterns[id] = { children: Number.MAX_VALUE };
-    client.updateTerritory(territoryId, selfPatterns);
+
   };
 
   ConstraintManager.prototype.validate = function(id) {
@@ -29,7 +38,7 @@ define(['logManager', 'js/NodePropertyNames', 'text!./ConstraintManager.html'],
     var node = client.getNode(id);
     var node_name = node.getAttribute('name');
     // logger.info('Validating node: ' + id + ' ' + node_name);
-
+    
     var constraints = node.getConstraintNames().map(function(constraint_name) {
       var constraint_obj = node.getConstraint(constraint_name);
       constraint_obj.name = constraint_name;
@@ -38,29 +47,36 @@ define(['logManager', 'js/NodePropertyNames', 'text!./ConstraintManager.html'],
       return a.priority - b.priority;
     });
 
+    if (constraints.length == 0 ) {
+      create_alert('There are no constraints to run on <strong>{' +
+        node_name + '}</strong>', 'alert-success').appendTo($cmb);
+    }
+
     var flag = false;
     constraints.forEach(function(constraint) {
       if (flag) return;
       var result = eval("(" + constraint.script + ")(client, node);");
-      var msg = '[[ <' + constraint.name + '> of <' + node_name + '> ]]';
+
+      var msg = '<strong>{' + node_name  + ' ' + constraint.name+ '}</strong>';
+      
       if (result) {
-          logger.info('No violation of a constraint: ' + msg);
-          // alert('No violation of a constraint: ' + msg);
+        var no_violation_message = 'No violation: ' + msg;
+        create_alert(no_violation_message, 'alert-success').appendTo($cmb);
       } else {
-          logger.error('!!Violation of a constraint: ' + msg + "!!!");
-          if ( constraint.priority == 0) {
-            logger.error('This is a serious error, please fix it first to run'
-              + ' remaining constraints, if there are any');
-            flag = true;
-          }
-          // alert('!!Violation of a constraint: ' + msg + "!!!");
+        var violation_message = '!!!Violation: ' + msg;
+        create_alert(violation_message).appendTo($cmb);
+        if ( constraint.priority == 0) {
+          create_alert('Please fix the last constraint violation to run'
+            + ' the remaining constraints, if there are more', 'alert-error')
+            .appendTo($cmb);
+          flag = true;
+        }
       }
     });
-
+    
     if (flag) {
       logger.warning('no more validation');
     }
-
 
     // var y = function(input, callback) {
     //   //
@@ -68,10 +84,25 @@ define(['logManager', 'js/NodePropertyNames', 'text!./ConstraintManager.html'],
     // }
 
     // Call validation for the node's children
-    var children = node.getChildrenIds();
-    children.forEach(function(child) {
-      self.validate(child);
-    });
+    // var children = node.getChildrenIds();
+    // children.forEach(function(child) {
+    //   self.validate(child);
+    // });
+
+    function create_alert(msg, type) {
+      var $but = $('<button/>', {
+        type: 'button',
+        class: 'close',
+        'data-dismiss': 'alert',
+        html: '&times;'
+      });
+      var $div = $('<div/>', {
+        class: 'alert ' + type,
+        html: msg
+      }).append($but);
+      return $div;
+    };
+
   }
 
   ConstraintManager.prototype.onOneEvent = function (events) {
