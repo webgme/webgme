86ebef0895010e14b48dad23a8d1a279d7778a01
rkereskenyi <robert.kereskenyi@gmail.com>
2013-12-03 16:21:31 -0600

    rename 'source' and 'target' pointers to 'src' and 'dst'

--- common/Constants.js
+++ common/Constants.js
@@ -28,8 +28,8 @@ define([], function () {
         /*
          * Dedicated POINTER names
          */
-         POINTER_SOURCE : 'source',     //dedicated connection source pointer name
-         POINTER_TARGET : 'target',     //dedicated connection target pointer name
+         POINTER_SOURCE : 'src',     //dedicated connection source pointer name
+         POINTER_TARGET : 'dst',     //dedicated connection target pointer name
          POINTER_REF: 'ref',    //dedicated reference pointer name
          POINTER_BASE: 'base',  //dedicated inheritance pointer name
 
