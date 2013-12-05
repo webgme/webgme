17dae4249ee02a045ca5fcda5d10cde833c7e957
rkereskenyi <robert.kereskenyi@gmail.com>
2013-12-05 16:37:03 -0600

    add textual help for drag feedback

--- client/js/Widgets/ModelEditor/ModelEditorWidget.js
+++ client/js/Widgets/ModelEditor/ModelEditorWidget.js
@@ -41,9 +41,9 @@ define(['js/DragDrop/DragHelper',
 
         if (dragEffects.length === 1) {
             if (dragEffects[0] === DragHelper.DRAG_EFFECTS.DRAG_CREATE_INSTANCE) {
-                helperEl.html($('<i class="icon-share-alt"></i>'));
+                helperEl.html($('<i class="icon-share-alt"></i>')).append(' Create instance...');
             } else if (dragEffects[0] === DragHelper.DRAG_EFFECTS.DRAG_CREATE_REFERENCE) {
-                helperEl.html($('<i class="icon-share"></i>'));
+                helperEl.html($('<i class="icon-share"></i>')).append(' Create reference...');
             }
         }
 
