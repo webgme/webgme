4536c82cfb221d319cc0395a613e8c49b4071e0e
rkereskenyi <robert.kereskenyi@gmail.com>
2014-01-22 12:03:31 -0600

    implement getDisabledConnectionAreas and setDisabledConnectionAreas

--- client/js/Widgets/DiagramDesigner/DiagramDesignerWidget.DecoratorBase.ConnectionArea.js
+++ client/js/Widgets/DiagramDesigner/DiagramDesignerWidget.DecoratorBase.ConnectionArea.js
@@ -6,8 +6,9 @@
 
 "use strict";
 
-define(['raphaeljs',
-    'css!/css/Widgets/DiagramDesigner/DiagramDesignerWidget.DecoratorBase.ConnectionArea'], function (raphaeljs) {
+define(['js/Constants',
+    'raphaeljs',
+    'css!/css/Widgets/DiagramDesigner/DiagramDesignerWidget.DecoratorBase.ConnectionArea'], function (CONSTANTS) {
 
     var DiagramDesignerWidgetDecoratorBaseConnectionArea,
         EVENT_POSTFIX = 'DiagramDesignerWidgetDecoratorBaseConnectionArea',
@@ -16,23 +17,22 @@ define(['raphaeljs',
         CONN_AREA_EDIT_BACKGROUND = 'conn-area-edit-bg',
         DISABLED = 'disabled',
         DATA_CONN_AREA_ID = 'CONN_AREA_ID',
-        CONN_AREA_SIZE = 8;
+        CONN_AREA_SIZE = 8,
+        DIAGRAM_DESIGNER_WIDGET_DECORATOR_DISABLED_CONNECTION_AREAS_REGISTRY_KEY = 'DIAGRAM_DESIGNER_WIDGET_DECORATOR_DISABLED_CONNECTION_AREAS_REGISTRY_KEY';
 
     DiagramDesignerWidgetDecoratorBaseConnectionArea = function () {
     };
 
 
     DiagramDesignerWidgetDecoratorBaseConnectionArea.prototype._initializeConnectionAreaUserSelection = function () {
-        var widget = this.hostDesignerItem.canvas,
-            self = this;
+        var self = this;
 
         //hook up right click on the decorator
         this.$el.on('mousedown.' + EVENT_POSTFIX, function (event) {
-            var rightClick = event.which === 3,
-                enableEdit = widget.getIsReadOnlyMode() !== true;
+            var rightClick = event.which === 3;
 
-            if (enableEdit && rightClick) {
-                self.editConnectionAreas();
+            if (rightClick) {
+                self._editConnectionAreas();
                 event.preventDefault();
                 event.stopPropagation();
                 event.stopImmediatePropagation();
@@ -41,12 +41,17 @@ define(['raphaeljs',
     };
 
 
-    DiagramDesignerWidgetDecoratorBaseConnectionArea.prototype.editConnectionAreas = function () {
+    DiagramDesignerWidgetDecoratorBaseConnectionArea.prototype._editConnectionAreas = function () {
         var w = this.$el.outerWidth(true),
             h = this.$el.outerHeight(true),
             j,
             self = this,
             shiftVal = (CONN_AREA_SIZE / 2);
+
+        //do not edit in ReadOnly mode
+        if (this.hostDesignerItem.canvas.getIsReadOnlyMode() === true) {
+            return;
+        }
 
         this._connAreaEditBackground = $('<div/>', { 'class': CONN_AREA_EDIT_BACKGROUND});
         this._connAreaEditBackground.css({'left': -shiftVal + 'px',
@@ -66,7 +71,7 @@ define(['raphaeljs',
             //save selection on right-click
             var rightClick = event.which === 3;
             if (rightClick) {
-                self.endEditConnectionAreas();
+                self._endEditConnectionAreas();
             }
         });
 
@@ -90,6 +95,8 @@ define(['raphaeljs',
         //get the connection areas from the decorator and render them
         this._areas = this.getConnectionAreas();
 
+        var disabledAreas = this._getDisabledConnectionAreas();
+
         j = this._areas.length;
         while (j--) {
             var a = this._areas[j];
@@ -106,26 +113,71 @@ define(['raphaeljs',
             }
 
             var path = this._svg.path('M ' + a.x1 + ',' + a.y1 + 'L' + a.x2 + ',' + a.y2);
-            $(path.node).attr({ "class": CONN_AREA_EDIT_CLASS });
             $(path.node).data(DATA_CONN_AREA_ID, a.id);
             path.attr({ "stroke-width": CONN_AREA_SIZE });
+
+            if (disabledAreas.indexOf(a.id) !== -1) {
+                //disabled as of now
+                $(path.node).attr({ "class":  CONN_AREA_EDIT_CLASS + ' ' + DISABLED });
+            } else {
+                //enabled
+                $(path.node).attr({ "class": CONN_AREA_EDIT_CLASS });
+            }
         }
     };
 
-    DiagramDesignerWidgetDecoratorBaseConnectionArea.prototype.endEditConnectionAreas = function () {
+    DiagramDesignerWidgetDecoratorBaseConnectionArea.prototype._endEditConnectionAreas = function () {
         var connAreas = this._connAreaEditBackground.find('path.' + CONN_AREA_EDIT_CLASS),
-            logger = this.logger;
+            logger = this.logger,
+            disabledAreas = [];
 
         connAreas.each(function(index, value) {
             value = $(value);
-            var aID = value.data(DATA_CONN_AREA_ID),
+            var id = value.data(DATA_CONN_AREA_ID),
                 enabled = value.attr("class") === CONN_AREA_EDIT_CLASS;
-            logger.warning(aID + ' enabled: ' + enabled);
+
+            logger.debug('Connection area: "' + id + '" enabled: ' + enabled);
+            if (!enabled) {
+                disabledAreas.push(id);
+            }
         });
 
         //finish editing
         this.$el.removeClass(DECORATOR_EDIT_CLASS);
         this._connAreaEditBackground.remove();
+
+        this._setDisabledConnectionAreas(disabledAreas);
+    };
+
+    DiagramDesignerWidgetDecoratorBaseConnectionArea.prototype._getDisabledConnectionAreas = function () {
+        var result = [],
+            client = this._control._client,
+            nodeObj = client.getNode(this._metaInfo[CONSTANTS.GME_ID]),
+            decoratorID = this.DECORATORID,
+            diagramDesignerWidgetDecoratorDisabledConnectionAreasRegistry;
+
+        if (nodeObj) {
+            diagramDesignerWidgetDecoratorDisabledConnectionAreasRegistry = nodeObj.getRegistry(DIAGRAM_DESIGNER_WIDGET_DECORATOR_DISABLED_CONNECTION_AREAS_REGISTRY_KEY) || {};
+            if (diagramDesignerWidgetDecoratorDisabledConnectionAreasRegistry[decoratorID]) {
+                result = diagramDesignerWidgetDecoratorDisabledConnectionAreasRegistry[decoratorID].slice(0);
+            }
+        }
+
+        return result;
+    };
+
+    DiagramDesignerWidgetDecoratorBaseConnectionArea.prototype._setDisabledConnectionAreas = function (disabledAreaIdList) {
+        var client = this._control._client,
+            nodeObj = client.getNode(this._metaInfo[CONSTANTS.GME_ID]),
+            decoratorID = this.DECORATORID,
+            diagramDesignerWidgetDecoratorDisabledConnectionAreasRegistry;
+
+        if (nodeObj) {
+            diagramDesignerWidgetDecoratorDisabledConnectionAreasRegistry = nodeObj.getEditableRegistry(DIAGRAM_DESIGNER_WIDGET_DECORATOR_DISABLED_CONNECTION_AREAS_REGISTRY_KEY) || {};
+            diagramDesignerWidgetDecoratorDisabledConnectionAreasRegistry[decoratorID] = disabledAreaIdList.slice(0);
+
+            client.setRegistry(this._metaInfo[CONSTANTS.GME_ID], DIAGRAM_DESIGNER_WIDGET_DECORATOR_DISABLED_CONNECTION_AREAS_REGISTRY_KEY, diagramDesignerWidgetDecoratorDisabledConnectionAreasRegistry);
+        }
     };
 
 
