3dc4a5d922edbcf13028b2f31c61251c0bbc50b1
rkereskenyi <robert.kereskenyi@gmail.com>
2014-02-28 17:21:17 -0600

    introducing popver box and using popover box for NetworkStatus and BranchStatus widgets

---
+++ client/js/Controls/PopoverBox.js
@@ -0,0 +1,48 @@
+"use strict";
+
+define(['jquery',
+        'css!/css/Controls/PopoverBox'], function () {
+
+    var PopoverBox,
+        AUTO_HIDE_MILLISEC = 2000;
+
+    PopoverBox = function (el) {
+        this._el = el;
+    };
+
+    PopoverBox.prototype.show = function (message, alertLevel, autoHide) {
+        var el = this._el;
+
+        el.addClass('pobox');
+
+        //remove existing
+        el.popover('destroy');
+
+        //show new
+        el.popover({'placement': 'top',
+            'content': message,
+            'container': this._el});
+
+        el.popover('show');
+        el.find('.popover').addClass('pobox');
+        el.popover('show');
+
+        if (alertLevel) {
+            el.find('.popover').addClass(alertLevel);
+        }
+
+        if (autoHide === true) {
+            setTimeout(function () {
+                el.popover('destroy');
+            }, AUTO_HIDE_MILLISEC);
+        }
+    };
+
+    PopoverBox.prototype.alertLevels = { 'SUCCESS': 'ddwa-success',
+                                                    'WARNING': 'ddwa-warning',
+                                                    'ERROR': 'ddwa-error',
+                                                    'INFO': 'ddwa-info'
+    };
+
+    return PopoverBox;
+});
