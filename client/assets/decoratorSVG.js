300ed7eb83454fd98246cccc083b347d1d6ffc43
kecso <tkecskes@isis.vanderbilt.edu>
2014-03-11 14:33:38 -0500

    icon modifications

--- client/assets/decoratorSVG.js
+++ client/assets/decoratorSVG.js
@@ -8,6 +8,6 @@ define([], function () {
     'use strict';           
                             
     return {               
-        'DecoratorSVGIconList': ["Chip.svg","Circle.svg","CloudComputing.svg","Ellipse.svg","Example.svg","LargeRectangle.svg","Network.svg","NetworkGraph.svg","Port.svg","Rectangle.svg","Router.svg","Server.svg","Signal.svg","SignalProcessor.svg","SmallRectangle.svg","Wireless.svg"]  
+        'DecoratorSVGIconList': ["Chip.svg","Circle.svg","CloudComputing.svg","Compound.svg","Ellipse.svg","Example.svg","LargeRectangle.svg","Network.svg","NetworkGraph.svg","Port.svg","Rectangle.svg","Router.svg","Server.svg","Signal.svg","SignalProcessor.svg","SmallRectangle.svg","Wireless.svg"]  
     };                      
 });
