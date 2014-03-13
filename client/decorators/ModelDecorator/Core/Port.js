08106195c157149c148c248179ffd366458ba16f
rkereskenyi <robert.kereskenyi@gmail.com>
2014-03-13 14:43:12 -0500

    port icon size is 5x11 pixels

--- client/decorators/ModelDecorator/Core/Port.js
+++ client/decorators/ModelDecorator/Core/Port.js
@@ -8,8 +8,10 @@ define(['logManager',
 
     var Port,
         PORT_CONNECTOR_LEN = 20,
-        PORT_DOM_HEIGHT = 16,
-        PORT_TITLE_WRAPPER_WITH_ICON_CLASS = 'w-icon';
+        PORT_DOM_HEIGHT = 15,
+        PORT_TITLE_WRAPPER_WITH_ICON_CLASS = 'w-icon',
+        PORT_DOT_WIDTH = 3,
+        PORT_ICON_WIDTH = 5;   //_Port.scss: $svg-icon-width: 5px;
 
     Port = function (id, options) {
         this.id = id;
@@ -174,9 +176,15 @@ define(['logManager',
         }
 
         this.connectionArea.x1 = this.orientation === "W" ? 0 : this.decorator.hostDesignerItem.getWidth();
+        //fix the x coordinate for the dot/svg icon's width
+        if (this.icon) {
+            this.connectionArea.x1 += (this.orientation === "W" ? -1 : 1) * (PORT_ICON_WIDTH - 1);
+        } else {
+            this.connectionArea.x1 += (this.orientation === "W" ? -1 : 1) * (PORT_DOT_WIDTH - 1);
+        }
         this.connectionArea.x2 = this.connectionArea.x1;
-        this.connectionArea.y1 = i * PORT_DOM_HEIGHT + 6;
-        this.connectionArea.y2 = this.connectionArea.y1 + 7;
+        this.connectionArea.y1 = i * PORT_DOM_HEIGHT + 9;
+        this.connectionArea.y2 = this.connectionArea.y1;
         this.connectionArea.angle1 = this.orientation === "W" ? 180 : 0;
         this.connectionArea.angle2 = this.orientation === "W" ? 180 : 0;
 
