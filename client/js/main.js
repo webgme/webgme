d2a510a5bc6a2d2de4fb8c03ac8591b9c439f666
Laszlo Juracz <laszlo.juracz@gmail.com>
2014-07-14 16:01:11 -0500

    making require configs for ng

--- client/js/main.js
+++ client/js/main.js
@@ -14,7 +14,7 @@ var DEBUG = false,
 
 // configure require path and modules
 require.config({
-    baseUrl: "/",
+    baseUrl: '/',
 
     map: {
          '*': {
@@ -26,49 +26,61 @@ require.config({
 
     paths: {
 
-        "domReady":	'lib/require/require-domready/domReady',
+        'domReady':	'lib/require/require-domready/domReady',
 
         //jQuery and stuff
-        "jquery": 'lib/jquery/jquery-' + _jqueryVersion + ( DEBUG ? '.min' : '' ),
-        "jquery-ui": 'lib/jquery/jquery-ui-' + _jqueryUIVersion + ( DEBUG ? '.min' : '' ),
-        "jquery-ui-iPad": 'lib/jquery/jquery.ui.ipad',
-        "jquery-WebGME": 'js/jquery.WebGME',
-        "jquery-dataTables": 'lib/jquery/jquery.dataTables.min',
-        "jquery-dataTables-bootstrapped": 'lib/jquery/jquery.dataTables.bootstrapped',
-        "jquery-spectrum": 'lib/jquery/jquery.spectrum',
+        'jquery': 'lib/jquery/jquery-' + _jqueryVersion + ( DEBUG ? '.min' : '' ),
+        'jquery-ui': 'lib/jquery/jquery-ui-' + _jqueryUIVersion + ( DEBUG ? '.min' : '' ),
+        'jquery-ui-iPad': 'lib/jquery/jquery.ui.ipad',
+        'jquery-WebGME': 'js/jquery.WebGME',
+        'jquery-dataTables': 'lib/jquery/jquery.dataTables.min',
+        'jquery-dataTables-bootstrapped': 'lib/jquery/jquery.dataTables.bootstrapped',
+        'jquery-spectrum': 'lib/jquery/jquery.spectrum',
 
         //Bootsrap stuff
-        "bootstrap": 'lib/bootstrap/' + _bootsrapVersion + '/js/bootstrap' + ( DEBUG ? '.min' : '' ),
+        'bootstrap': 'lib/bootstrap/' + _bootsrapVersion + '/js/bootstrap' + ( DEBUG ? '.min' : '' ),
 
         //Other modules
-        "underscore": 'lib/underscore/underscore-min',
-        "backbone": 'lib/backbone/backbone.min',
-        "d3": 'lib/d3/d3.v3.min',
-        "jscolor": 'lib/jscolor/jscolor',
+        'underscore': 'lib/underscore/underscore-min',
+        'backbone': 'lib/backbone/backbone.min',
+        'd3': 'lib/d3/d3.v3.min',
+        'jscolor': 'lib/jscolor/jscolor',
 
         //RaphaelJS family
-        "eve": 'lib/raphael/eve',   //needed because of raphael.core.js uses require with 'eve'
-        "raphaeljs": 'lib/raphael/raphael.amd',
-        "raphael_core": 'lib/raphael/raphael.core',
-        "raphael_svg": 'lib/raphael/raphael.svg_fixed',
-        "raphael_vml": 'lib/raphael/raphael.vml',
+        'eve': 'lib/raphael/eve',   //needed because of raphael.core.js uses require with 'eve'
+        'raphaeljs': 'lib/raphael/raphael.amd',
+        'raphael_core': 'lib/raphael/raphael.core',
+        'raphael_svg': 'lib/raphael/raphael.svg_fixed',
+        'raphael_vml': 'lib/raphael/raphael.vml',
 
         //WebGME custom modules
-        "logManager": 'common/LogManager',
-        "eventDispatcher": 'common/EventDispatcher',
-        "notificationManager": 'js/NotificationManager',
-        "clientUtil": 'js/util',
-        "loaderCircles": "js/Loader/LoaderCircles",
-        "loaderProgressBar": "js/Loader/LoaderProgressBar",
+        'logManager': 'common/LogManager',
+        'eventDispatcher': 'common/EventDispatcher',
+        'notificationManager': 'js/NotificationManager',
+        'clientUtil': 'js/util',
+        'loaderCircles': 'js/Loader/LoaderCircles',
+        'loaderProgressBar': 'js/Loader/LoaderProgressBar',
 
-        "codemirror": 'lib/codemirror/codemirror.amd',
-        "jquery-csszoom": 'lib/jquery/jquery.csszoom',
+        'codemirror': 'lib/codemirror/codemirror.amd',
+        'jquery-csszoom': 'lib/jquery/jquery.csszoom',
 
-        "jszip": 'lib/jszip/jszip',
+        'jszip': 'lib/jszip/jszip',
 
-        "moment": 'lib/moment/moment.min'
+        'moment': 'lib/moment/moment.min',
+
+        // Angular and modules
+        'angular': 'lib/angular/angular-1.2.19/angular' + ( DEBUG ? '.min' : '' ),
+        'angular-route': 'lib/angular/angular-1.2.19/angular-route' + ( DEBUG ? '.min' : '' ),
+        'angular-route-styles': 'lib/angular/angular-route-styles/route-styles',
+        'ui-bootstrap': 'lib/angular/ui-bootstrap/ui-bootstrap-tpls-0.11.0.min'
     },
+
     shim: {
+        
+        'angular-route': ['angular'],
+        'angular-route-styles': ['angular'],
+        'ui-bootstrap': ['angular'],
+        
         'jquery-ui': ['jquery'],
         'jquery-ui-iPad': ['jquery','jquery-ui'],
 
@@ -115,7 +127,7 @@ require(
     function (domReady, jQuery, jQueryUi, jQueryUiiPad, jqueryWebGME, jqueryDataTables, bootstrap, underscore,
               backbone, webGME, util, CONFIG) {
 
-        "use strict";
+        'use strict';
 
         domReady(function () {
 
