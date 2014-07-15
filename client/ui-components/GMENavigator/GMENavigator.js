173d3018cf9fb72cc89ba4a0d47f31a20b9f391c
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2014-07-15 12:45:38 -0500

    #73 add placeholder for controller.

--- client/ui-components/GMENavigator/GMENavigator.js
+++ client/ui-components/GMENavigator/GMENavigator.js
@@ -15,15 +15,111 @@ define([
 
     "use strict";
 
-    var GMENavigator = function() {
+    var GMENavigatorController = function ($scope, gmeClient) {
+
+        var self = this;
+
+        self.$scope = $scope;
+        self.gmeClient = gmeClient;
+
+        self.initialize();
 
     };
+
+    GMENavigatorController.prototype.update = function () {
+        if (!this.$scope.$$phase) {
+            this.$scope.$apply();
+        }
+    };
+
+    GMENavigatorController.prototype.initialize = function () {
+        var self = this;
+
+        // initialize model structure
+        self.$scope.items = {};
+
+        if (self.gmeClient) {
+            self.initWithClient();
+        } else {
+            self.initTestData();
+        }
+    };
+
+    GMENavigatorController.prototype.initTestData = function () {
+        var self = this,
+            createNewProject,
+            exportProject;
+
+        // Function handlers
+        exportProject = function (id, branch) {
+            console.log(JSON.stringify(self.$scope.items.root.items[id].items[branch || 'master']));
+        };
+
+        createNewProject = function () {
+            self.$scope.items.root.items['new_project'] = {
+                id: 'new_project',
+                name: 'New project Name',
+                items: {
+                    'master': {
+                        id: 'master',
+                        properties: {
+                            hashTag: '34535435',
+                            lastCommiter: 'petike',
+                            lastCommitTime: new Date()
+                        },
+                        directive: 'branch-selector'
+                    }
+
+                },
+                actions: {
+                    exportProject: {
+                        label: 'Export',
+                        iconClass: 'glyphicon glyphicon-export',
+                        action: exportProject
+                    }
+                }
+            };
+        };
+
+
+        // dummy object structure
+        self.$scope.items = {
+
+            root: {
+                id: 'root',
+                name: 'GME',
+                iconClass: 'gme-navi-icon',
+                actions: {
+                    createProject: {
+                        label: 'Create new project',
+                        iconClass: 'fa fa-add',
+                        action: createNewProject
+                    },
+                    importProject: {
+                        label: 'Import project',
+                        action: createNewProject
+                    }
+                },
+
+                items: {}
+            }
+        };
+
+        self.update();
+
+    };
+
+    GMENavigatorController.prototype.initWithClient = function () {
+        var self = this;
+        console.error('Not implemented yet.');
+    };
+
 
     angular.module(
         'gme.ui.gmeNavigator', []
     ).controller(
-        'GMENavigator',
-        function($scope) {
+        'GMENavigator', GMENavigatorController
+        /*function($scope) {
             $scope.items = {
 
                 root: {
@@ -71,7 +167,7 @@ define([
                 }
 
             };
-        }
+        }*/
     ).directive(
         'gmeNavigator',
          function($compile) {
