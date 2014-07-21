e1c08e231c5aa3589f32ba7d318a31127c2a7aa4
kecso <tkecskes@isis.vanderbilt.edu>
2014-07-21 15:59:10 -0500

    no message

--- worker/constants.js
+++ worker/constants.js
@@ -1,27 +1,29 @@
 define([],
 function(){
-
+    'use strict';
     return {
         'msgTypes':{
-            'request': 'request',
-            'result': 'result',
-            'info': 'info',
-            'initialize': 'initialize',
-            'initialized': 'initialized'
+            'request'     : 'request',
+            'result'      : 'result',
+            'info'        : 'info',
+            'initialize'  : 'initialize',
+            'initialized' : 'initialized',
+            'query'       : 'query'
         },
         'workerStates':{
-            'initializing': 'initializing',
-            'free': 'free',
-            'working': 'working',
-            'waiting': 'waiting'
+            'initializing' : 'initializing',
+            'free'         : 'free',
+            'working'      : 'working',
+            'waiting'      : 'waiting'
         },
         'workerCommands':{
-            'initialize': 'initialize',
-            'getResult': 'getResult',
-            'dumpMoreNodes': 'dumpMoreNodes',
-            'generateJsonURL': 'generateJsonURL',
-            'executePlugin': 'executePlugin',
-            'exportLibrary': 'exportLibrary'
+            'initialize'      : 'initialize',
+            'getResult'       : 'getResult',
+            'dumpMoreNodes'   : 'dumpMoreNodes',
+            'generateJsonURL' : 'generateJsonURL',
+            'executePlugin'   : 'executePlugin',
+            'exportLibrary'   : 'exportLibrary',
+            'connectedWorker' : 'connectedWorker' //this command starts the addOn
         }
-    }
+    };
 });
