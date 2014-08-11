df159dab09ab290ae17d28df140101e5c29376af
Laszlo Juracz <laszlo.juracz@gmail.com>
2014-08-11 17:03:09 -0500

    confirm dialog template

---
+++ src/client/ui-components/ConfirmDialog/ConfirmDialog.js
@@ -0,0 +1,76 @@
+/*globals define, console, window, angular*/
+
+define( [
+  'angular',
+
+  'text!./templates/ConfirmDialog.html'
+
+], function ( ng, ConfirmDialogTemplate ) {
+  "use strict";
+
+  angular.module(
+      'isis.ui.confirmDialog',
+      [ 'ui.bootstrap' ]
+    ).provider( '$confirmDialog', function () {
+
+      var $confirmDialogProvider = {
+
+        options: {
+        },
+        $get: ['$modal',
+          function ( $modal ) {
+
+            var $confirmDialog = {},
+              ConfirmDialogController;
+
+            ConfirmDialogController = function ( $scope, $modalInstance,
+                                                 dialogTitle, dialogContentTemplate, onOk, onCancel, validator ) {
+
+              $scope.dialogTitle = dialogTitle;
+              $scope.dialogContentTemplate = dialogContentTemplate;
+
+              $scope.ok = function () {
+
+                if ( angular.isFunction(validator) ? validator($scope) : true ) {
+                  $modalInstance.close();
+                  if ( angular.isFunction( onOk ) ) {
+                    onOk();
+                  }
+                }
+              };
+
+              $scope.cancel = function () {
+                $modalInstance.dismiss( 'cancel' );
+                if ( angular.isFunction( onCancel ) ) {
+                  onCancel();
+                }
+              };
+            };
+
+            $confirmDialog.open = function ( options ) {
+
+              var confirmDialogInstance = $modal.open( {
+                template: ConfirmDialogTemplate,
+                controller: ConfirmDialogController,
+                resolve: {
+                  dialogTitle: function() { return options.dialogTitle; },
+                  dialogContentTemplate: function() {  return options.dialogContentTemplate; },
+                  onOk: function() { return options.onOk; },
+                  onCancel: function() { return options.onCancel; },
+                  validator: function() { return options.validator; }
+                }
+              });
+
+              return confirmDialogInstance;
+
+            };
+
+            return $confirmDialog;
+
+          }]
+      };
+
+      return $confirmDialogProvider;
+
+    } );
+} );
