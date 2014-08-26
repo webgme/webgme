970be67775b7951737ac6c6043c8c39ec50264ca
kecso <tkecskes@isis.vanderbilt.edu>
2014-08-26 12:32:36 -0500

    no message

--- src/common/core/corediff.js
+++ src/common/core/corediff.js
@@ -14,8 +14,8 @@ define(['util/canon'], function (CANON) {
         }
 
         function atr_diff(source,target){
-            var sNames = _core.getOwnAttributeNames(source),
-                tNames = _core.getOwnAttributeNames(target),
+            var sNames = _core.getAttributeNames(source),
+                tNames = _core.getAttributeNames(target),
                 i,
                 diff = {updated:{},removed:[],added:[]};
 
@@ -25,10 +25,43 @@ define(['util/canon'], function (CANON) {
                 }
             }
 
+            for(i=0;i<tNames.length;i++){
+                if(_core.getAttribute(source,tNames[i]) === undefined){
+                    diff.updated[tNames[i]] = _core.getAttribute(target,tNames[i]);
+                    diff.added.push(tNames[i]);
+                } else {
+                    if(CANON.stringify(_core.getAttribute(source,tNames[i])) !== CANON.stringify(_core.getAttribute(target,tNames[i]))){
+                        diff.updated[tNames[i]] = _core.getAttribute(target,tNames[i]);
+                    }
+                }
+            }
 
+            return diff;
         }
         function reg_diff(source,target){
+            var sNames = _core.getRegistryNames(source),
+                tNames = _core.getRegistryNames(target),
+                i,
+                diff = {updated:{},removed:[],added:[]};
 
+            for(i=0;i<sNames.length;i++){
+                if(tNames.indexOf(sNames[i]) === -1){
+                    diff.removed.push(sNames[i]);
+                }
+            }
+
+            for(i=0;i<tNames.length;i++){
+                if(_core.getRegistry(source,tNames[i]) === undefined){
+                    diff.updated[tNames[i]] = _core.getRegistry(target,tNames[i]);
+                    diff.added.push(tNames[i]);
+                } else {
+                    if(CANON.stringify(_core.getRegistry(source,tNames[i])) !== CANON.stringify(_core.getRegistry(target,tNames[i]))){
+                        diff.updated[tNames[i]] = _core.getRegistry(target,tNames[i]);
+                    }
+                }
+            }
+
+            return diff;
         }
         function children_diff(source,target){
 
