e7301a7d562c3b4257217b8bc587f95958895466
kecso <tkecskes@isis.vanderbilt.edu>
2014-10-15 14:08:11 -0500

    separating branch update broadcaster from cache functionality -> solves the repeater problem (probably there should be a more sophisticated one)

--- src/common/storage/serverstorage.js
+++ src/common/storage/serverstorage.js
@@ -4,10 +4,10 @@
  * Author: Tamas Kecskes
  */
 
-define([ 'storage/mongo', 'storage/server', 'storage/cache', 'storage/log'], function (Mongo,Server,Cache,Log) {
+define([ 'storage/mongo', 'storage/server', 'storage/cache', 'storage/log', 'storage/broadcaster'], function (Mongo,Server,Cache,Log,Broadcaster) {
     "use strict";
     function server(options){
-        return new Server(new Log(new Cache(new Mongo(options),options),options),options);
+        return new Server(new Log(new Cache(new Broadcaster(new Mongo(options),options),options),options),options);
     }
 
 
