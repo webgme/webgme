026cb36a7756024db1635d00ccaed16c94aed9f4
Kevin Smyth <kevin.m.smyth@gmail.com>
2014-12-04 17:57:17 -0600

    Fix race with opening db connection

--- src/common/storage/server.js
+++ src/common/storage/server.js
@@ -28,6 +28,7 @@ define([ "util/assert","util/guid","util/url","socket.io","worker/serverworkerma
             _objects = {},
             _projects = {},
         /*_references = {},*/
+            _databaseOpenCallbacks = [],
             _databaseOpened = false,
             ERROR_DEAD_GUID = 'the given object does not exists',
             _workerManager = null,
@@ -52,18 +53,27 @@ define([ "util/assert","util/guid","util/url","socket.io","worker/serverworkerma
             if(_databaseOpened){
                 callback();
             } else {
-                _databaseOpened = true;
-                _database.openDatabase(function(err){
-                    if(err){
-                        _databaseOpened = false;
-                        //this error has to be put to console as well
-                        console.log('Error in mongoDB connection initiation!!! - ', err);
-                        options.log.error(err);
-                        callback(err);
-                    } else {
-                        callback(null);
-                    }
-                });
+                if (_databaseOpenCallbacks.length === 0) {
+                    _databaseOpenCallbacks = [callback];
+                    _database.openDatabase(function (err) {
+                        if (err) {
+                            _databaseOpened = false;
+                            //this error has to be put to console as well
+                            console.log('Error in mongoDB connection initiation!!! - ', err);
+                            options.log.error(err);
+                            while (_databaseOpenCallbacks.length) {
+                                _databaseOpenCallbacks.pop()(err);
+                            }
+                        } else {
+                            _databaseOpened = true;
+                            while (_databaseOpenCallbacks.length) {
+                                _databaseOpenCallbacks.pop()(null);
+                            }
+                        }
+                    });
+                } else {
+                    _databaseOpenCallbacks.push(callback);
+                }
             }
         }
 
@@ -592,20 +602,26 @@ define([ "util/assert","util/guid","util/url","socket.io","worker/serverworkerma
                 _socket = null;
             }
 
-            if(_databaseOpened){
-                //close projects
-                for(var i in _projects){
-                    _projects[i].closeProject(null);
-                }
+            var cleanup = function () {
+                _objects = {};
+                _projects = {};
+                //_references = {};
+                _databaseOpened = false;
+            };
+            if (_databaseOpened || _databaseOpenCallbacks.length) {
+                checkDatabase(function (err) {
+                    //close projects
+                    for (var i in _projects) {
+                        _projects[i].closeProject(null);
+                    }
 
-                //close database
-                _database.closeDatabase(null);
+                    //close database
+                    _database.closeDatabase(null);
+                    cleanup();
+                });
+            } else {
+                cleanup();
             }
-
-            _objects = {};
-            _projects = {};
-            //_references = {};
-            _databaseOpened = false;
         }
 
         function getWorkerResult(resultId,callback){
