12ecfd48f7a54cfa841dffdf8b831f3af5ccdda4
Brian Broll <brian.broll@gmail.com>
2015-01-16 13:21:06 -0600

    initial commit

---
+++ test/autorouter.chaos.js
@@ -0,0 +1,7 @@
+/*
+ * This will generate events randomly and apply them, storing a queue 
+ * of the applied events. If/when it encounters an error, it will print the 
+ * event queue to allow for easy replication.
+ *
+ * The events are 
+ */
