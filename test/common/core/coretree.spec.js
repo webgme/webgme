22862242bb5fca1128ae0081375de3af48b9e815
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2015-02-05 16:13:40 -0600

    Add core tree test cases for 5 functions.
    - getRelid
    - getLevel
    - getRoot
    - getPath
    - isValidPath

--- test/common/core/coretree.spec.js
+++ test/common/core/coretree.spec.js
@@ -40,7 +40,7 @@ describe('CoreTree', function () {
         });
     });
 
-    describe('getParent', function () {
+    describe('core.getParent', function () {
 
         it('should return with the parent object reference', function () {
             var node = {parent: {}};
@@ -63,4 +63,105 @@ describe('CoreTree', function () {
         });
     });
 
+    describe('core.getRelid', function () {
+        it('should return with the relid', function () {
+            var node = {relid: '123344567771231234567890'};
+
+            coreTree.getRelid(node).should.be.equal('123344567771231234567890');
+        });
+    });
+
+    describe('core.getLevel', function () {
+        it('should return with level 0 for root', function () {
+            var node = {parent: null};
+
+            coreTree.getLevel(node).should.be.equal(0);
+        });
+
+        it('should return with level 1 for child of root', function () {
+            var root = {parent: null},
+                child = {parent: root};
+
+            coreTree.getLevel(child).should.be.equal(1);
+        });
+
+        it('should return with level 2 for grandchild of root', function () {
+            var root = {parent: null},
+                child = {parent: root},
+                grandChild = {parent: child};
+
+            coreTree.getLevel(grandChild).should.be.equal(2);
+        });
+
+
+    });
+
+    describe('core.getRoot', function () {
+        it('should return with root', function () {
+            var root = {parent: null};
+
+            coreTree.getRoot(root).should.be.equal(root);
+        });
+
+        it('should return with root of a grandchild', function () {
+            var root = {parent: null},
+                child = {parent: root},
+                grandChild = {parent: child};
+
+            coreTree.getRoot(grandChild).should.be.equal(root);
+        });
+    });
+
+    describe('core.getPath', function () {
+        it('should return with the path of the root', function () {
+            var root = {parent: null, relid: null},
+                child = {parent: root, relid: '1'},
+                grandChild = {parent: child, relid: '2'};
+
+            coreTree.getPath(root).should.be.equal('');
+        });
+
+        it('should return with the path of the child', function () {
+            var root = {parent: null, relid: null},
+                child = {parent: root, relid: '1'},
+                grandChild = {parent: child, relid: '2'};
+
+            coreTree.getPath(child).should.be.equal('/1');
+        });
+
+        it('should return with the path of the grandchild', function () {
+            var root = {parent: null, relid: null},
+                child = {parent: root, relid: '1'},
+                grandChild = {parent: child, relid: '2'};
+
+            coreTree.getPath(grandChild).should.be.equal('/1/2');
+        });
+    });
+
+    describe('core.isValidPath', function () {
+        // invalid path
+        it('should return false for any non-string type', function () {
+            coreTree.isValidPath(42).should.be.false;
+            coreTree.isValidPath({a:42}).should.be.false;
+            coreTree.isValidPath([]).should.be.false;
+            coreTree.isValidPath(null).should.be.false;
+            coreTree.isValidPath(undefined).should.be.false;
+            coreTree.isValidPath('1/2/3/4').should.be.false;
+        });
+
+        // valid paths
+        it('should return true for empty string', function () {
+            coreTree.isValidPath('').should.be.true;
+        });
+
+        it('should return true for any string starting with /', function () {
+            coreTree.isValidPath('/').should.be.true;
+            coreTree.isValidPath('/asdf').should.be.true;
+            coreTree.isValidPath('/ adf dsaf dsafds ').should.be.true;
+            coreTree.isValidPath('/ 012454/ asdf 23 adsf 2554/87').should.be.true;
+            coreTree.isValidPath('/                              ').should.be.true;
+        });
+
+    });
+    
 });
