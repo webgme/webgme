31a29aecf75cd5cf82abd3905b517f797aed1f34
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2015-02-06 16:43:36 -0600

    #121 extracts the mga and xme file info

--- utils/GMEModelStatistics/GMEModelStatisticsExporter/Program.cs
+++ utils/GMEModelStatistics/GMEModelStatisticsExporter/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GME.MGA;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,46 +10,189 @@ namespace GMEModelStatisticsExporter
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            // TODO: verify the input arguments
+            bool errorInUsage = false;
+            bool mgaIsGiven = true;
+            bool generateTemplate = false;
+            bool formattedOutput = false;
+            string outputDirectory = Path.Combine(Environment.CurrentDirectory, "output");
+            string statFileName = "sample_stats.json";
+            string inputFilename = string.Empty;
 
-            // TODO: move the file to a temp directory
+            Directory.CreateDirectory(outputDirectory);
 
-            // TODO: get mga and xme versions
-
-            // TODO: read in file stats
-
-            // TODO: process the project
-
-            Statistics.Statistics stats = Process();
-
-            // TODO: serialize the object
-
-            var settings = new Newtonsoft.Json.JsonSerializerSettings()
+            // verify the input arguments
+            for (int i = 0; i < args.Length; i++)
             {
-                Formatting = Newtonsoft.Json.Formatting.Indented
-            };
+                if (args[i].StartsWith("-"))
+                {
+                    switch (args[i].Substring(1)) {
+                        case "p":
+                        case "-prettyPrint":
+                            formattedOutput = true;
+                            break;
+                        case "t":
+                        case "-template":
+                            generateTemplate = true;
+                            break;
+                        default:
+                            errorInUsage = true;
+                            break;
+                    }
+
+                }
+                else
+                {
+                    inputFilename = args[i];
+
+                    if (i < args.Length - 1)
+                    {
+                        Console.WriteLine("Arguments after filename are ignored.");
+                    }
+
+                    break;
+                }
+            }
+
+            if (File.Exists(inputFilename))
+            {
+                if (Path.GetExtension(inputFilename).ToLowerInvariant() == ".mga")
+                {
+                    mgaIsGiven = true;
+                }
+                else if (Path.GetExtension(inputFilename).ToLowerInvariant() == ".xme")
+                {
+                    mgaIsGiven = false;
+                }
+                else
+                {
+                    errorInUsage = true;
+                }
+            }
+            else
+            {
+                errorInUsage = true;
+            }
+
+
+            if (generateTemplate)
+            {
+                Statistics.Statistics stats = Process(null);
+                SerializeStats(stats, Path.Combine(outputDirectory, statFileName), formattedOutput);
+                return 0;
+            }
+
+            if (errorInUsage)
+            {
+                Console.WriteLine("Usage: {0} [arguments] inputfilename.[mga|xme]", Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+                return 1;
+            }
+
+
+            // copy the file to a temp directory
+            string tempXmeFile = Path.Combine(outputDirectory, Path.GetFileNameWithoutExtension(inputFilename) + ".xme");
+            string tempMgaFile = Path.Combine(outputDirectory, Path.GetFileNameWithoutExtension(inputFilename) + ".mga");
+            statFileName = Path.GetFileNameWithoutExtension(inputFilename) + "_stat.json";
+
+            MgaProject project = new MgaProject();
+
+            if (mgaIsGiven) {
+                Console.WriteLine("Copying MGA file to temp directory: {0}", tempMgaFile);
+                File.Copy(inputFilename, tempMgaFile, true);
+
+                Console.WriteLine("Opening project");
+                int mgaversion;
+                string paradigmName;
+                string paradigmVersion;
+                object paradigmGUID;
+                bool ro_mode;
+
+                project.QueryProjectInfo("MGA=" + tempMgaFile, out mgaversion, out paradigmName, out paradigmVersion, out paradigmGUID, out ro_mode);
+
+                project.OpenEx("MGA=" + tempMgaFile, paradigmName, true);
+
+                Console.WriteLine("Saving project as xme: {0}", tempXmeFile);
+                // export to xme
+                GME.MGA.Parser.MgaDumper dumper = new GME.MGA.Parser.MgaDumper();
+                dumper.DumpProject(project, tempXmeFile);
+            }
+            else
+            {
+                Console.WriteLine("Copying XME file to temp directory: {0}", tempXmeFile);
+                File.Copy(inputFilename, tempXmeFile, true);
+
+                GME.MGA.Parser.MgaParser parser = new GME.MGA.Parser.MgaParser();
+                string paradigmName;
+
+                parser.GetXMLParadigm(tempXmeFile, out paradigmName);
+
+                Console.WriteLine("Creating project");
+                project.CreateEx("MGA=" + tempMgaFile, paradigmName, null);
+
+                // import xme file
+                Console.WriteLine("Importing project");
+                
+                parser.ParseProject(project, tempXmeFile);
+
+                Console.WriteLine("Saving project as mga: {0}", tempMgaFile);
+                project.Save("MGA=" + tempMgaFile);
+            }
+
+            if (project == null) {
+                throw new Exception("Project is null.");
+            }
+
+        
+            // process the project
+            var statistics = Process(project);
+
+            // read in file stats
+            FileInfo mgaInfo = new FileInfo(tempMgaFile);
+            FileInfo xmeInfo = new FileInfo(tempXmeFile);
+
+            statistics.MgaSizeInBytes = mgaInfo.Length;
+            statistics.XmeSizeInBytes = xmeInfo.Length;
+
+            // serialize the object
+            SerializeStats(statistics, Path.Combine(outputDirectory, statFileName), formattedOutput);
+
+
+            return 0;
+        }
+
+        private static void SerializeStats(Statistics.Statistics stats, string filename, bool formattedOutput)
+        {
+            var settings = new Newtonsoft.Json.JsonSerializerSettings();
+
+            if (formattedOutput)
+            {
+                settings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            }
 
             var serializer = Newtonsoft.Json.JsonSerializer.Create(settings);
-            using (StreamWriter writer = new StreamWriter("sample_stats.json"))
+            using (StreamWriter writer = new StreamWriter(filename))
             {
                 serializer.Serialize(writer, stats);
             }
-
         }
 
-        private static Statistics.Statistics Process()
+        private static Statistics.Statistics Process(IMgaProject project)
         {
             Statistics.Statistics stats = new Statistics.Statistics();
 
 
-            GenerateDummyData(stats);
+            if (project == null)
+            {
+                GenerateDummyData(stats);
+                return stats;
+            }
 
 
             return stats;
         }
 
+        #region Dummy data generators
         private static void GenerateDummyData(Statistics.Statistics stats)
         {
             var random = new Random();
@@ -118,6 +262,6 @@ namespace GMEModelStatisticsExporter
                 GenerateTree(tree[id], random.Next(0, depth), maxChildOnEachLevel);
             }
         }
-
+        #endregion
     }
 }
