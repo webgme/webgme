9b2a64cee4ef3d47291a9f3a9807f1315f24c2dc
kecso <tkecskes@isis.vanderbilt.edu>
2015-02-10 15:29:22 -0600

    #125 some basic test have been added

---
+++ test/common/storage/local.spec.js
@@ -0,0 +1,126 @@
+/**
+ * Created by tkecskes on 2/10/2015.
+ */
+//These tests intend to check the functionality of the local implementation of the storage
+//we use the globally available Storage as that is identical with the local.js
+
+require('../../_globals.js');
+var should = require('chai').should(),
+  requirejs= require('requirejs'),
+  GENKEY = requirejs('util/key');
+
+describe('local',function(){
+  var storage = new global.Storage();
+  describe('Database',function(){
+    it('should be empty initially',function(done){
+      storage.getProjectNames(function(err,names){
+        if(err){
+          done(err);
+          return;
+        }
+        names.should.be.empty;
+        done();
+      });
+    });
+
+    it('should be possible to call meaningless (in local) functions',function(done){
+      storage.openDatabase(function(err){
+        if(err){
+          done(err);
+          return;
+        }
+        storage.closeDatabase(function(err){
+          if(err){
+            done(err);
+            return;
+          }
+          storage.fsyncDatabase(function(err){
+            if(err){
+              done(err);
+              return;
+            }
+            done();
+          });
+        });
+      });
+    });
+
+    it('opening/creating a project is not creates until a real data is saved',function(done){
+      storage.openProject('test',function(err,p){
+        if(err){
+          done(err);
+          return;
+        }
+        storage.getProjectNames(function(err,names){
+          if(err){
+            done(err);
+            return;
+          }
+          names.should.be.empty;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('Project',function(){
+    var project;
+    before(function(done){
+      storage.openDatabase(function(err){
+        if(err){
+          done(err);
+          return;
+        }
+        storage.openProject('localTestProject',function(err,p){
+          if(err){
+            done(err);
+            return;
+          }
+          project = p;
+          done();
+        });
+      });
+    });
+    /*
+     fsyncDatabase: fsyncDatabase,
+     getDatabaseStatus: getDatabaseStatus,
+     closeProject: closeProject,
+     loadObject: loadObject,
+     insertObject: insertObject,
+     getInfo: getInfo,
+     setInfo: setInfo,
+     findHash: findHash,
+     dumpObjects: dumpObjects,
+     getBranchNames: getBranchNames,
+     getBranchHash: getBranchHash,
+     setBranchHash: setBranchHash,
+     getCommits: getCommits,
+     ID_NAME: "_id"
+     */
+    it('the ID field inside the database should be \'_id\'',function(){
+      project.ID_NAME.should.be.equal('_id');
+    });
+
+    it('should be possible to insert valid objects then read them out',function(done){
+      var object = {},id;
+      object[project.ID_NAME] = "";
+      object.value = "my value";
+      id = "#" + GENKEY(object);
+      object[project.ID_NAME] = id;
+      project.insertObject(object,function(err){
+        if(err){
+          done(err);
+          return;
+        }
+        project.loadObject(id,function(err,obj){
+          if(err){
+            done(err);
+            return;
+          }
+          obj.value.should.be.equal("my value");
+          done();
+        });
+      });
+    });
+  });
+});
