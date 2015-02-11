e3a597fec02d549df192455c21cbc08504c173a7
pmeijer <patrik85@isis.vanderbilt.edu>
2015-02-11 10:29:02 -0600

    #141 jshint rules checked in. Example in PluginGenerator.

--- src/plugin/coreplugins/PluginGenerator/PluginGenerator.js
+++ src/plugin/coreplugins/PluginGenerator/PluginGenerator.js
@@ -1,3 +1,5 @@
+/*jshint node:true, browser:true*/
+/*globals define */
 /**
  * Copyright (C) 2014 Vanderbilt University, All rights reserved.
  *
@@ -25,79 +27,79 @@ define(['plugin/PluginConfig',
         PluginGeneratorPlugin.prototype.constructor = PluginGeneratorPlugin;
 
         PluginGeneratorPlugin.prototype.getName = function () {
-            return "Plugin Generator";
+            return 'Plugin Generator';
         };
 
         PluginGeneratorPlugin.prototype.getVersion = function () {
-            return "0.1.1";
+            return '0.1.1';
         };
 
         PluginGeneratorPlugin.prototype.getConfigStructure = function () {
             return [
                 {
-                    "name": "pluginID",
-                    "displayName": "Unique plugin identifier",
-                    "regex": '^(?!(?:do|if|in|for|let|new|try|var|case|else|enum|eval|false|null|this|true|void|with|break|catch|class|const|super|throw|while|yield|delete|export|import|public|return|static|switch|typeof|default|extends|finally|package|private|continue|debugger|function|arguments|interface|protected|implements|instanceof)$)[a-zA-Z_$][0-9a-zA-Z_$]*',
-                    "regexMessage": 'No spaces and special characters allowed. This value is used as the name of the generated plugin class.',
-                    "description": 'Unique ID for the plugin.',
-                    "value": 'NewPlugin',
-                    "valueType": "string",
-                    "readOnly": false
+                    'name': 'pluginID',
+                    'displayName': 'Unique plugin identifier',
+                    'regex': '^(?!(?:do|if|in|for|let|new|try|var|case|else|enum|eval|false|null|this|true|void|with|break|catch|class|const|super|throw|while|yield|delete|export|import|public|return|static|switch|typeof|default|extends|finally|package|private|continue|debugger|function|arguments|interface|protected|implements|instanceof)$)[a-zA-Z_$][0-9a-zA-Z_$]*',
+                    'regexMessage': 'No spaces and special characters allowed. This value is used as the name of the generated plugin class.',
+                    'description': 'Unique ID for the plugin.',
+                    'value': 'NewPlugin',
+                    'valueType': 'string',
+                    'readOnly': false
                 },
                 {
-                    "name": "pluginName",
-                    "displayName": "Name",
-                    "description": 'Short readable plugin name; spaces are allowed',
-                    "value": 'New Plugin',
-                    "valueType": "string",
-                    "readOnly": false
+                    'name': 'pluginName',
+                    'displayName': 'Name',
+                    'description': 'Short readable plugin name; spaces are allowed',
+                    'value': 'New Plugin',
+                    'valueType': 'string',
+                    'readOnly': false
                 },
                 {
-                    "name": "description",
-                    "displayName": "Description",
-                    "description": 'Optional description of the plugin.',
-                    "value": '',
-                    "valueType": "string",
-                    "readOnly": false
+                    'name': 'description',
+                    'displayName': 'Description',
+                    'description': 'Optional description of the plugin.',
+                    'value': '',
+                    'valueType': 'string',
+                    'readOnly': false
                 },
                 {
-                    "name": "test",
-                    "displayName": "Include testing script",
-                    "description": 'Generate template for unit-tests.',
-                    "value": true,
-                    "valueType": "boolean",
-                    "readOnly": false
+                    'name': 'test',
+                    'displayName': 'Include testing script',
+                    'description': 'Generate template for unit-tests.',
+                    'value': true,
+                    'valueType': 'boolean',
+                    'readOnly': false
                 },
                 {
-                    "name": "templateType",
-                    "displayName": "Example template",
-                    "description": 'Ejs template for code generation, also illustrates how to save' +
-                        ' artifacts using the blobClient.',
-                    "value": 'None',
-                    "valueType": "string",
-                    "valueItems": [
-                        "None",
-                        "JavaScript",
-                        "Python",
-                        "CSharp"
+                    'name': 'templateType',
+                    'displayName': 'Example template',
+                    'description': 'Ejs template for code generation, also illustrates how to save' +
+                    ' artifacts using the blobClient.',
+                    'value': 'None',
+                    'valueType': 'string',
+                    'valueItems': [
+                        'None',
+                        'JavaScript',
+                        'Python',
+                        'CSharp'
                     ],
-                    "readOnly": false
+                    'readOnly': false
                 },
                 {
-                    "name": "configStructure",
-                    "displayName": "Include Configuration Structure.",
-                    "description": 'Configuration structure will populate this GUI with controls.',
-                    "value": false,
-                    "valueType": "boolean",
-                    "readOnly": false
+                    'name': 'configStructure',
+                    'displayName': 'Include Configuration Structure.',
+                    'description': 'Configuration structure will populate this GUI with controls.',
+                    'value': false,
+                    'valueType': 'boolean',
+                    'readOnly': false
                 },
                 {
-                    "name": "core",
-                    "displayName": "Include core example",
-                    "description": '',
-                    "value": false,
-                    "valueType": "boolean",
-                    "readOnly": false
+                    'name': 'core',
+                    'displayName': 'Include core example',
+                    'description': '',
+                    'value': false,
+                    'valueType': 'boolean',
+                    'readOnly': false
                 }
             ];
         };
@@ -135,7 +137,7 @@ define(['plugin/PluginConfig',
             // Add the plugin file.
             pluginFileContent = ejs.render(TEMPLATES['plugin.js.ejs'], self.currentConfig);
             pluginFileName = self.pluginDir + self.currentConfig.pluginID + '.js';
-            self.filesToAdd[pluginFileName] =  pluginFileContent;
+            self.filesToAdd[pluginFileName] = pluginFileContent;
 
 
             // Add the file at the end.
@@ -143,6 +145,7 @@ define(['plugin/PluginConfig',
             fileKeys = Object.keys(self.filesToAdd);
             nbrOfFiles = fileKeys.length;
             artifact = self.blobClient.createArtifact('pluginFiles');
+
             for (i = 0; i < fileKeys.length; i += 1) {
                 artifact.addFile(fileKeys[i], self.filesToAdd[fileKeys[i]], function (err, hash) {
                     error = err ? error + err : error;
@@ -162,7 +165,7 @@ define(['plugin/PluginConfig',
                             self.createMessage(null, 'Append "' + './src/plugins/' + self.projectName +
                                 '" to "pluginBasePaths" in config.js.');
                             self.createMessage(self.rootNode, 'Select the root-node and add ' +
-                                self.currentConfig.pluginID + ' to the validPlugins attribute (separate with space).');
+                            self.currentConfig.pluginID + ' to the validPlugins attribute (separate with space).');
                             self.createMessage(null, 'For more information go to https://github.com/webgme/' +
                                 'webgme-domain-tools/wiki/DeveloperQA');
                             self.logger.info('Artifacts are saved here: ' + hashes.toString());
@@ -215,7 +218,7 @@ define(['plugin/PluginConfig',
                 self.currentConfig.templateExt = 'cs';
                 fileName = self.pluginDir + 'Templates/CSharp.cs.ejs';
                 fileContent = 'using System;\nnamespace Hey {\n\tclass Hi {\n\t\tstatic void Main()' +
-                    ' {\n\t\t\tConsole.WriteLine("<%=a%> and <%=b%> provided.");\n\t\t}\n\t}\n}';
+                ' {\n\t\t\tConsole.WriteLine("<%=a%> and <%=b%> provided.");\n\t\t}\n\t}\n}';
             } else if (self.currentConfig.templateType === 'JavaScript') {
                 self.currentConfig.templateExt = 'js';
                 fileName = self.pluginDir + 'Templates/JavaScript.js.ejs';
