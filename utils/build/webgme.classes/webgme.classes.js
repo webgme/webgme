98a9ec93d89c05eda459c9d5031dd7b3f3793d23
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2015-02-11 18:08:47 -0600

    #148 Add on-gme-init support for body tag
    Fix webgme.classes.js expose Client, BlobClient, ExecutorClient,
    InterpreterManager.
    Use fragment files start.frag and end.frag
    See test\test_dist.html for usage.

--- utils/build/webgme.classes/webgme.classes.js
+++ utils/build/webgme.classes/webgme.classes.js
@@ -2,9 +2,11 @@ define('webgme.classes',
   [
     'client',
     'blob/BlobClient',
+    'executor/ExecutorClient',
     'js/Utils/InterpreterManager'
-  ], function (Client, BlobClient, InterpreterManager) {
+  ], function (Client, BlobClient, ExecutorClient, InterpreterManager) {
     GME.classes.Client = Client;
     GME.classes.BlobClient = BlobClient;
+    GME.classes.ExecutorClient = ExecutorClient;
     GME.classes.InterpreterManager = InterpreterManager;
   });
