f9b3f7aa176c27f9029049a80e01f39532b8634c
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2015-02-12 09:44:59 -0600

    $148 remove setTimeouts; warning->warn; add test
    Calls on-gme-init when all classes are ready and downloaded to the
    client.

--- utils/build/webgme.classes/webgme.classes.js
+++ utils/build/webgme.classes/webgme.classes.js
@@ -1,12 +1,20 @@
+/*globals define, document, console, window, GME*/
+
 define('webgme.classes',
-  [
-    'client',
-    'blob/BlobClient',
-    'executor/ExecutorClient',
-    'js/Utils/InterpreterManager'
-  ], function (Client, BlobClient, ExecutorClient, InterpreterManager) {
-    GME.classes.Client = Client;
-    GME.classes.BlobClient = BlobClient;
-    GME.classes.ExecutorClient = ExecutorClient;
-    GME.classes.InterpreterManager = InterpreterManager;
-  });
+    [
+        'client',
+        'blob/BlobClient',
+        'executor/ExecutorClient',
+        'js/Utils/InterpreterManager'
+    ], function (Client, BlobClient, ExecutorClient, InterpreterManager) {
+        GME.classes.Client = Client;
+        GME.classes.BlobClient = BlobClient;
+        GME.classes.ExecutorClient = ExecutorClient;
+        GME.classes.InterpreterManager = InterpreterManager;
+
+        if (document.body.getAttribute("on-gme-init")) {
+            window[document.body.getAttribute("on-gme-init")](GME);
+        } else {
+            console.warn('To use GME, define a javascript function and set the body element\'s on-gme-init property.');
+        }
+    });
