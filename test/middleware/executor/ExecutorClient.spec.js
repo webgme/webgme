482c869e45b0e78672b8c81d630e03bd9d17e1e7
pmeijer <patrik85@isis.vanderbilt.edu>
2015-02-24 17:04:24 -0600

    #178 Added tests for ExecutorClient

---
+++ test/middleware/executor/ExecutorClient.spec.js
@@ -0,0 +1,80 @@
+/*globals require, requirejs, describe, it, before, after, WebGMEGlobal, WebGME*/
+
+/**
+ * @author pmeijer / https://github.com/pmeijer
+ */
+
+require('../../_globals.js');
+
+var requirejs = require('requirejs'),
+    should = require('chai').should(),
+    ExecutorClient = requirejs('executor/ExecutorClient'),
+    executorClient,
+    server,
+    serverBaseUrl;
+
+describe('ExecutorClient', function () {
+    'use strict';
+
+    before(function (done) {
+        // we have to set the config here
+        var config = WebGMEGlobal.getConfig(),
+            param = {};
+        config.port = 9001;
+        config.authentication = false;
+        config.enableExecutor = true;
+
+        param.serverPort = config.port;
+
+        serverBaseUrl = 'http://127.0.0.1:' + config.port;
+
+        server = WebGME.standaloneServer(config);
+        server.start(function () {
+            executorClient = new ExecutorClient(param);
+            done();
+        });
+    });
+
+    after(function (done) {
+        server.stop(done);
+    });
+
+    it('getWorkersInfo should return empty object', function (done) {
+        executorClient.getWorkersInfo(function (err, res) {
+            if (err) {
+                done(err);
+                return;
+            }
+            should.equal(typeof res, 'object');
+            should.equal(Object.keys(res).length, 0);
+            done();
+        });
+    });
+
+    it('createJob followed by getInfo should return CREATED jobInfo', function (done) {
+        var jobInfo = {
+            hash: '77704f10a36aa4214f5b0095ba8099e729a10f46'
+        };
+        executorClient.createJob(jobInfo, function (err, res) {
+            var createTime;
+            if (err) {
+                done(err);
+                return;
+            }
+            should.equal(typeof res, 'object');
+            createTime = res.createTime;
+            should.equal(res.status, 'CREATED');
+            should.equal(typeof createTime, 'string');
+            executorClient.getInfo(jobInfo.hash, function (err, res) {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                should.equal(typeof res, 'object');
+                should.equal(res.createTime, createTime);
+                should.equal(res.status, 'CREATED');
+                done();
+            });
+        });
+    });
+});
