b0887d6eed09e6f2802fcaff5aeb3ccd4e937f72
pmeijer <patrik85@isis.vanderbilt.edu>
2015-02-26 08:33:46 -0600

    #178 remove test for unimplemented call.

--- test/middleware/executor/ExecutorClient.spec.js
+++ test/middleware/executor/ExecutorClient.spec.js
@@ -92,10 +92,10 @@ describe('ExecutorClient', function () {
         });
     });
 
-    it('getInfoByStatus SUCCESS should succeed', function (done) {
-        executorClient.getInfoByStatus('SUCCESS', function(err, res) {
-            should.equal(err, null);
-            done();
-        });
-    });
+    //it('getInfoByStatus SUCCESS should succeed', function (done) {
+    //    executorClient.getInfoByStatus('SUCCESS', function(err, res) {
+    //        should.equal(err, null);
+    //        done();
+    //    });
+    //});
 });
