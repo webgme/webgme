0582686ace212fe39a4db992036c55da109dc03f
pmeijer <patrik85@isis.vanderbilt.edu>
2015-03-05 13:24:55 -0600

    WIP fix listAllMetadata in BlobServer tests succeeds.

--- test/middleware/blob/BlobServer.spec.js
+++ test/middleware/blob/BlobServer.spec.js
@@ -94,7 +94,7 @@ describe('BlobServer', function () {
         });
     });
 
-    it.skip('should return metadata at /rest/blob/metadata/ with public metadata', function (done) {
+    it('should return metadata at /rest/blob/metadata/ with public metadata', function (done) {
         var bc = new BlobClient(bcParam),
             artifact = new Artifact('public', bc, {
                 name: 'public.zip',
@@ -122,9 +122,55 @@ describe('BlobServer', function () {
                         return;
                     }
                     should.equal(res.status, 200);
-                    should.equal(Object.keys(res.body).length, 0);
+                    should.equal(res.body[hash].name, 'public.zip');
                     done();
                 });
+            });
+        });
+    });
+
+    it('should download non-public artifact at /rest/blob/download/valid-hash', function (done) {
+        var bc = new BlobClient(bcParam),
+            artifact = new Artifact('notPublic', bc);
+        artifact.addFile('tt.txt', 'ttt', function (err/*, fHash*/) {
+            if (err) {
+                done(err);
+                return;
+            }
+            artifact.save(function (err, hash) {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                agent.get(serverBaseUrl + '/rest/blob/download/' + hash).end(function (err, res) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+                    should.equal(res.status, 200);
+                    should.equal(res.header['content-disposition'], 'attachment; filename=notPublic.zip');
+                    done();
+                });
+            });
+        });
+    });
+
+    it.skip('should download empty artifact at /rest/blob/download/valid-hash', function (done) {
+        var bc = new BlobClient(bcParam),
+            artifact = new Artifact('notPublic', bc);
+        artifact.save(function (err, hash) {
+            if (err) {
+                done(err);
+                return;
+            }
+            agent.get(serverBaseUrl + '/rest/blob/download/' + hash).end(function (err, res) {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                should.equal(res.status, 200);
+                should.equal(res.header['content-disposition'], 'attachment; filename=notPublic.zip');
+                done();
             });
         });
     });
