c9b826c6079e9f43b134c56a2a489d499acfff32
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2015-04-06 10:43:06 -0500

    Fixes #296 use 0 for int and float if no valid default value.

--- src/client/decorators/MetaDecorator/DiagramDesigner/AttributeDetailsDialog.js
+++ src/client/decorators/MetaDecorator/DiagramDesigner/AttributeDetailsDialog.js
@@ -105,14 +105,14 @@ define(['js/util',
             switch (type) {
                 case 'integer':
                     result = parseInt(value, 10);
-                    if (result === 'NaN') {
-                        result = undefined;
+                    if (isNaN(result)) {
+                        result = 0;
                     }
                     break;
                 case 'float':
                     result = parseFloat(value, 10);
-                    if (result === 'NaN') {
-                        result = undefined;
+                    if (isNaN(result)) {
+                        result = 0;
                     }
                     break;
                 case 'boolean':
