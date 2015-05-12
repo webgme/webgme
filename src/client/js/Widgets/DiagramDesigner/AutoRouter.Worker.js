774865d85bba29f7fa296abe0cb3cf20d0acfe1d
Brian Broll <brian.broll@vanderbilt.edu>
2015-05-12 14:11:47 -0500

    Added basic autorouting structure

--- src/client/js/Widgets/DiagramDesigner/AutoRouter.Worker.js
+++ src/client/js/Widgets/DiagramDesigner/AutoRouter.Worker.js
@@ -1,6 +1,7 @@
-/*globals require,importScripts*/
+/*globals console,require,importScripts*/
 // This is the code for using the autorouter as a web worker.
 
+console.log('Starting to read ARWorker.js...');
 importScripts('../../../lib/require/require.min.js');
 
 var worker = this,
@@ -15,6 +16,7 @@ var worker = this,
 var startWorker = function() {
     'use strict';
 
+    console.log('Starting the worker...');
     require({
         baseUrl: '.',
         paths: {
@@ -78,5 +80,86 @@ worker.onmessage = function(msg) {
     WebGMEGlobal.gmeConfig.client = msg.data;
     startWorker();
 };
+
+console.log('Finished reading ARWorker.js...');
+importScripts('../../../lib/require/require.min.js');
+
+var worker = this,
+    window = {},  //jshint ignore: line
+    WebGMEGlobal = {gmeConfig: {}};
+
+/**
+ * Start the worker. This is done after the relevant config has been received.
+ *
+ * @return {undefined}
+ */
+var startWorker = function() {
+    'use strict';
+
+    console.log('Starting the worker...');
+    require({
+        baseUrl: '.',
+        paths: {
+            common: '../../../../common',
+            assert: '../../../../common/util/assert',
+            js: '../..',
+            underscore: '../../../lib/underscore/underscore-min',
+            debug: '../../../lib/debug/debug'
+        }, 
+        shim: {
+            debug: {
+                exports: 'window.debug'
+            }
+        }
+    }, 
+    [
+        'AutoRouter.ActionApplier', 
+        'js/logger',
+        'underscore'
+    ],
+    function(
+        ActionApplier, 
+        Logger,
+        _
+    ) {
+
+        var AutoRouterWorker = function() {
+            // Add recording actions?
+            this.logger = Logger.create('gme:Widgets:DiagramDesigner:AutoRouter:Worker', WebGMEGlobal.gmeConfig.client.log);
+            this._recordActions = true;
+            this.init();
+        };
+
+        AutoRouterWorker.prototype.handleMessage = function(msg) {
+            console.log('Received msg:', msg.data);
+            var result = this._invokeAutoRouterMethod.apply(this, msg.data);
+
+            msg.data.push(result);
+            console.log('Responding to', msg.data[0], 'with', result);
+            worker.postMessage(msg.data);
+
+        };
+
+        _.extend(AutoRouterWorker.prototype, ActionApplier.prototype);
+
+        var autorouterWorker = new AutoRouterWorker();
+
+        console.log('Posting "Ready!"');
+        worker.postMessage('READY');
+
+        worker.onmessage = autorouterWorker.handleMessage.bind(autorouterWorker);
+    }.bind(this));
+};
+
+// Set the WebGMEGlobal.gmeConfig.client config value for use
+// in the loggers
+worker.onmessage = function(msg) {
+    'use strict';
+    
+    WebGMEGlobal.gmeConfig.client = msg.data;
+    startWorker();
+};
+
+console.log('Starting...');
 
 
