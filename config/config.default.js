f0704460c6b41a21ce2031f0cb0aecefa7412df1
pmeijer <patrik85@isis.vanderbilt.edu>
2015-05-19 14:49:55 -0500

    #370 Add smoke-test page and point to it from config.

--- config/config.default.js
+++ config/config.default.js
@@ -43,7 +43,7 @@ var path = require('path'),
         },
 
         client: {
-            appDir: path.join(__dirname, '../src/client'),
+            appDir: './teststorage',
             log: {
                 level: 'debug' // To see log messages in the browser inspector set:
                                // localStorage.debug = '*' (or 'gme*', 'gme:core*')
