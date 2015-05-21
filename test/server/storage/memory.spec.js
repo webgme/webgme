c2801102fb5341bc1b9f914d7d9baab821f9a18e
Zsolt Lattmann <zsolt.lattmann@gmail.com>
2015-05-21 15:04:34 -0500

    #370 add import test for storage/memory.js

--- test/server/storage/memory.spec.js
+++ test/server/storage/memory.spec.js
@@ -14,7 +14,7 @@ describe('Memory storage', function () {
         Q = testFixture.Q,
 
         getMemoryStorage = testFixture.getMemoryStorage,
-        newProjectName = 'newProject';
+        projectName = 'newProject';
 
     it('should create an instance of getMemoryStorage', function () {
         var memoryStorage = getMemoryStorage(logger);
@@ -95,6 +95,74 @@ describe('Memory storage', function () {
 
     describe('project operations', function () {
 
+        it('should fail to open a project if not connected to database', function (done) {
+            var memoryStorage = getMemoryStorage(logger);
+
+            memoryStorage.openProject({projectName: 'something'})
+                .then(function () {
+                    done(new Error('should have failed to openProject'));
+                })
+                .catch(function (err) {
+                    if (err instanceof Error) {
+                        // TODO: check error message
+                        done();
+                    } else {
+                        done(new Error('should have failed to openProject'));
+                    }
+                });
+        });
+
+        it('should fail to delete a project if not connected to database', function (done) {
+            var memoryStorage = getMemoryStorage(logger);
+
+            memoryStorage.deleteProject({projectName: 'something'})
+                .then(function () {
+                    done(new Error('should have failed to deleteProject'));
+                })
+                .catch(function (err) {
+                    if (err instanceof Error) {
+                        // TODO: check error message
+                        done();
+                    } else {
+                        done(new Error('should have failed to deleteProject'));
+                    }
+                });
+        });
+
+        it('should fail to create a project if not connected to database', function (done) {
+            var memoryStorage = getMemoryStorage(logger);
+
+            memoryStorage.createProject({projectName: 'something'})
+                .then(function () {
+                    done(new Error('should have failed to createProject'));
+                })
+                .catch(function (err) {
+                    if (err instanceof Error) {
+                        // TODO: check error message
+                        done();
+                    } else {
+                        done(new Error('should have failed to createProject'));
+                    }
+                });
+        });
+
+        it('should fail to get project names if not connected to database', function (done) {
+            var memoryStorage = getMemoryStorage(logger);
+
+            memoryStorage.getProjectNames({})
+                .then(function () {
+                    done(new Error('should have failed to getProjectNames'));
+                })
+                .catch(function (err) {
+                    if (err instanceof Error) {
+                        // TODO: check error message
+                        done();
+                    } else {
+                        done(new Error('should have failed to getProjectNames'));
+                    }
+                });
+        });
+
         it('should get project names', function (done) {
             var memoryStorage = getMemoryStorage(logger);
 
@@ -119,16 +187,47 @@ describe('Memory storage', function () {
                 })
                 .then(function (projectNames) {
                     expect(projectNames).deep.equal([]);
-                    return memoryStorage.createProject({projectName: newProjectName});
+                    return memoryStorage.createProject({projectName: projectName});
                 })
                 .then(function () {
                     return memoryStorage.getProjectNames({});
                 })
                 .then(function (projectNames) {
-                    expect(projectNames).deep.equal([newProjectName]);
+                    expect(projectNames).deep.equal([projectName]);
                     done();
                 })
                 .catch(done);
+        });
+
+        it('should fail to create a project if it already exists', function (done) {
+            var memoryStorage = getMemoryStorage(logger);
+
+            memoryStorage.openDatabase()
+                .then(function () {
+                    return memoryStorage.getProjectNames({});
+                })
+                .then(function (projectNames) {
+                    expect(projectNames).deep.equal([]);
+                    return memoryStorage.createProject({projectName: projectName});
+                })
+                .then(function () {
+                    return memoryStorage.getProjectNames({});
+                })
+                .then(function (projectNames) {
+                    expect(projectNames).deep.equal([projectName]);
+                    return memoryStorage.createProject({projectName: projectName});
+                })
+                .then(function () {
+                    done(new Error('should have failed to createProject'));
+                })
+                .catch(function (err) {
+                    if (err instanceof Error) {
+                        // TODO: check error message
+                        done();
+                    } else {
+                        done(new Error('should have failed to createProject'));
+                    }
+                });
         });
 
         it('should create and delete a project', function (done) {
@@ -140,14 +239,14 @@ describe('Memory storage', function () {
                 })
                 .then(function (projectNames) {
                     expect(projectNames).deep.equal([]);
-                    return memoryStorage.createProject({projectName: newProjectName});
+                    return memoryStorage.createProject({projectName: projectName});
                 })
                 .then(function () {
                     return memoryStorage.getProjectNames({});
                 })
                 .then(function (projectNames) {
-                    expect(projectNames).deep.equal([newProjectName]);
-                    return memoryStorage.deleteProject({projectName: newProjectName});
+                    expect(projectNames).deep.equal([projectName]);
+                    return memoryStorage.deleteProject({projectName: projectName});
                 })
                 .then(function () {
                     return memoryStorage.getProjectNames({});
@@ -168,14 +267,14 @@ describe('Memory storage', function () {
                 })
                 .then(function (projectNames) {
                     expect(projectNames).deep.equal([]);
-                    return memoryStorage.createProject({projectName: newProjectName});
+                    return memoryStorage.createProject({projectName: projectName});
                 })
                 .then(function () {
                     return memoryStorage.getProjectNames({});
                 })
                 .then(function (projectNames) {
-                    expect(projectNames).deep.equal([newProjectName]);
-                    return memoryStorage.getBranches({projectName: newProjectName});
+                    expect(projectNames).deep.equal([projectName]);
+                    return memoryStorage.getBranches({projectName: projectName});
                 })
                 .then(function (branches) {
                     // expect names of branches
@@ -194,18 +293,18 @@ describe('Memory storage', function () {
                 })
                 .then(function (projectNames) {
                     expect(projectNames).deep.equal([]);
-                    return memoryStorage.createProject({projectName: newProjectName});
+                    return memoryStorage.createProject({projectName: projectName});
                 })
                 .then(function () {
                     return memoryStorage.getProjectNames({});
                 })
                 .then(function (projectNames) {
-                    expect(projectNames).deep.equal([newProjectName]);
-                    return memoryStorage.openProject({projectName: newProjectName});
+                    expect(projectNames).deep.equal([projectName]);
+                    return memoryStorage.openProject({projectName: projectName});
                 })
                 .then(function (project) {
                     
-                    expect(project.name).equal(newProjectName);
+                    expect(project.name).equal(projectName);
 
                     expect(project).to.have.property('closeProject');
                     expect(project).to.have.property('loadObject');
@@ -221,6 +320,55 @@ describe('Memory storage', function () {
                 .catch(done);
         });
 
-    });
 
+        it('should fail to open a non-existing project', function (done) {
+            var memoryStorage = getMemoryStorage(logger, gmeConfig);
+
+            memoryStorage.openDatabase()
+                .then(function () {
+                    return memoryStorage.getProjectNames({});
+                })
+                .then(function (projectNames) {
+                    expect(projectNames).deep.equal([]);
+                    return memoryStorage.openProject({projectName: 'project_does_not_exist'});
+                })
+                .then(function () {
+                    done(new Error('expected to fail'));
+                })
+                .catch(function (err) {
+                    if (err instanceof Error) {
+                        // TODO: check error message
+                        done();
+                    } else {
+                        done(new Error('should have failed to openProject'));
+                    }
+                });
+        });
+
+        it('should import, open, and close a project', function (done) {
+            var storage = getMemoryStorage(logger, gmeConfig);
+
+            storage.openDatabase()
+                .then(function () {
+                    return storage.deleteProject({projectName: projectName});
+                })
+                .then(function () {
+                    return testFixture.importProject(storage, {
+                        projectSeed: 'seeds/EmptyProject.json',
+                        projectName: projectName,
+                        gmeConfig: gmeConfig,
+                        logger: logger
+                    });
+                })
+                .then(function (result) {
+                    //console.log(result);
+                    return storage.openProject({projectName: projectName});
+                })
+                .then(function (project) {
+                    return project.closeProject();
+                })
+                .then(done)
+                .catch(done);
+        });
+    });
 });
