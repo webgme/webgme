d39774fe0567a48bef51d114a5ff62178a1485e1
Zsolt Lattmann <zsolt.lattmann@gmail.com>
2015-06-04 14:36:08 -0500

    #370 add first test for server/storage/websocket.js

--- test/server/storage/safestorage.spec.js
+++ test/server/storage/safestorage.spec.js
@@ -6,7 +6,7 @@
 
 var testFixture = require('../../_globals.js');
 
-describe('Safestorage', function () {
+describe('SafeStorage', function () {
     'use strict';
     var gmeConfig = testFixture.getGmeConfig(),
         expect = testFixture.expect,
---
+++ test/server/storage/websocket.spec.js
@@ -0,0 +1,136 @@
+/*globals*/
+/*jshint node:true, newcap:false, mocha:true*/
+/**
+ * @author lattmann / https://github.com/lattmann
+ */
+
+var testFixture = require('../../_globals.js');
+
+describe('WebSocket', function () {
+    'use strict';
+    var gmeConfig = testFixture.getGmeConfig(),
+        expect = testFixture.expect,
+        logger = testFixture.logger.fork('websocket.spec'),
+        Q = testFixture.Q,
+        WebGME = testFixture.WebGME,
+
+        superagent = testFixture.superagent,
+
+        gmeAuth,
+        projectName = 'webSocketTest1',
+        guestAccount = gmeConfig.authentication.guestAccount,
+
+        safeStorage,
+
+        server,
+        serverBaseUrl,
+        agent,
+
+        logIn = function (callback) {
+            agent.post(serverBaseUrl + '/login?redirect=%2F')
+                .type('form')
+                .send({username: guestAccount})
+                .send({password: guestAccount})
+                .end(function (err, res) {
+                    if (err) {
+                        return callback(err);
+                    }
+                    expect(res.status).to.equal(200);
+                    callback(err, res);
+                });
+        },
+        openSocketIo = function () {
+            var io = require('socket.io-client');
+            return Q.nfcall(logIn)
+                .then(function (res) {
+                    var socket,
+                        socketReq = {url: serverBaseUrl},
+                        defer = Q.defer();
+
+                    agent.attachCookies(socketReq);
+
+                    socket = io.connect(serverBaseUrl,
+                        {
+                            query: 'webGMESessionId=' + /webgmeSid=s:([^;]+)\./.exec(
+                                decodeURIComponent(socketReq.cookies))[1],
+                            transports: gmeConfig.socketIO.transports,
+                            multiplex: false
+                        });
+
+                    socket.on('error', function (err) {
+                        logger.error(err);
+                        defer.reject(err || 'could not connect');
+                        socket.disconnect();
+                    });
+                    socket.on('connect', function () {
+                        defer.resolve(socket);
+                    });
+
+                    return defer.promise;
+                });
+        };
+
+
+    before(function (done) {
+        server = WebGME.standaloneServer(gmeConfig);
+        serverBaseUrl = server.getUrl();
+        server.start(function (err) {
+            if (err) {
+                done(new Error(err));
+                return;
+            }
+
+            testFixture.clearDBAndGetGMEAuth(gmeConfig, [projectName])
+                .then(function (gmeAuth_) {
+                    gmeAuth = gmeAuth_;
+                    safeStorage = testFixture.getMongoStorage(logger, gmeConfig, gmeAuth);
+
+                    return safeStorage.openDatabase();
+                })
+                .then(function () {
+                    return safeStorage.deleteProject({projectName: projectName});
+                })
+                .then(function () {
+                    return testFixture.importProject(safeStorage, {
+                        projectSeed: 'seeds/EmptyProject.json',
+                        projectName: projectName,
+                        gmeConfig: gmeConfig,
+                        logger: logger
+                    });
+                })
+                .nodeify(done);
+        });
+    });
+
+    after(function (done) {
+        server.stop(function (err) {
+            if (err) {
+                done(new Error(err));
+                return;
+            }
+
+            Q.all([
+                gmeAuth.unload(),
+                safeStorage.closeDatabase()
+            ])
+                .nodeify(done);
+        });
+    });
+
+    beforeEach(function () {
+        agent = superagent.agent();
+    });
+
+    it('should getUserId', function (done) {
+        openSocketIo()
+            .then(function (socket) {
+                return Q.ninvoke(socket, 'emit', 'getUserId');
+            })
+            .then(function (data) {
+                expect(data).to.equal(guestAccount);
+            })
+            .nodeify(done);
+    });
+
+
+});
