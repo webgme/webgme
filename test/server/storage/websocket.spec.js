ee46eea8729a08d81cab51228688d4cf60fc4036
Zsolt Lattmann <zsolt.lattmann@gmail.com>
2015-06-04 16:00:04 -0500

    #370 increase test coverage for server/storage/websocket

--- test/server/storage/websocket.spec.js
+++ test/server/storage/websocket.spec.js
@@ -70,8 +70,8 @@ describe('WebSocket', function () {
                     return defer.promise;
                 });
         };
-
-    describe('valid sessionId as a guest user', function () {
+    
+    describe('with valid sessionId as a guest user', function () {
         before(function (done) {
             server = WebGME.standaloneServer(gmeConfig);
             serverBaseUrl = server.getUrl();
@@ -81,7 +81,12 @@ describe('WebSocket', function () {
                     return;
                 }
 
-                testFixture.clearDBAndGetGMEAuth(gmeConfig, [projectName, projectNameUnauthorized])
+                testFixture.clearDBAndGetGMEAuth(gmeConfig, [
+                    projectName,
+                    projectNameUnauthorized,
+                    'webSocketTest_NewProject',
+                    'webSocketTest_ProjectToBeDeleted'
+                ])
                     .then(function (gmeAuth_) {
                         gmeAuth = gmeAuth_;
                         safeStorage = testFixture.getMongoStorage(logger, gmeConfig, gmeAuth);
@@ -99,7 +104,9 @@ describe('WebSocket', function () {
                     .then(function () {
                         return Q.all([
                             safeStorage.deleteProject({projectName: projectName}),
-                            safeStorage.deleteProject({projectName: projectNameUnauthorized})
+                            safeStorage.deleteProject({projectName: projectNameUnauthorized}),
+                            safeStorage.deleteProject({projectName: 'webSocketTest_NewProject'}),
+                            safeStorage.deleteProject({projectName: 'webSocketTest_ProjectToBeDeleted'})
                         ]);
                     })
                     .then(function () {
@@ -162,6 +169,84 @@ describe('WebSocket', function () {
                 .nodeify(done);
         });
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Project related tests
+
+        it('should getProjectNames', function (done) {
+            openSocketIo()
+                .then(function (socket) {
+                    var data = {};
+
+                    return Q.ninvoke(socket, 'emit', 'getProjectNames', data);
+                })
+                .then(function (result) {
+                    expect(result).to.deep.equal([projectName]);
+                })
+                .nodeify(done);
+        });
+
+        it('should getProjects', function (done) {
+            openSocketIo()
+                .then(function (socket) {
+                    var data = {};
+
+                    return Q.ninvoke(socket, 'emit', 'getProjects', data);
+                })
+                .then(function (result) {
+                    expect(result.length).to.greaterThan(1);
+                    // TODO: add more specific check for the actual project existence
+                })
+                .nodeify(done);
+        });
+
+
+        it('should getProjectsAndBranches', function (done) {
+            openSocketIo()
+                .then(function (socket) {
+                    var data = {};
+
+                    return Q.ninvoke(socket, 'emit', 'getProjectsAndBranches', data);
+                })
+                .then(function (result) {
+                    expect(result.length).to.equal(1);
+                    expect(result[0].name).to.equal(projectName);
+                    expect(result[0].branches).to.have.property('master');
+                })
+                .nodeify(done);
+        });
+
+        it('should createProject', function (done) {
+            var data = {
+                projectName: 'webSocketTest_NewProject'
+            };
+
+            openSocketIo()
+                .then(function (socket) {
+                    return Q.ninvoke(socket, 'emit', 'createProject', data);
+                })
+                .then(function (result) {
+                    expect(result.name).to.equal(data.projectName);
+                })
+                .nodeify(done);
+        });
+
+        it('should create and delete a project', function (done) {
+            var socket,
+                data = {
+                    projectName: 'webSocketTest_ProjectToBeDeleted'
+                };
+
+            openSocketIo()
+                .then(function (socket_) {
+                    socket = socket_;
+                    return Q.ninvoke(socket, 'emit', 'createProject', data);
+                })
+                .then(function (result) {
+                    expect(result.name).to.equal(data.projectName);
+                    return Q.ninvoke(socket, 'emit', 'deleteProject', data);
+                })
+                .nodeify(done);
+        });
 
         it('should open an existing project', function (done) {
             openSocketIo()
@@ -174,6 +259,24 @@ describe('WebSocket', function () {
                 })
                 .then(function (result) {
                     expect(result).to.have.property('master');
+                })
+                .nodeify(done);
+        });
+
+        it('should open and close an existing project', function (done) {
+            var socket,
+                data = {
+                    projectName: projectName
+                };
+
+            openSocketIo()
+                .then(function (socket_) {
+                    socket = socket_;
+                    return Q.ninvoke(socket, 'emit', 'openProject', data);
+                })
+                .then(function (result) {
+                    expect(result).to.have.property('master');
+                    return Q.ninvoke(socket, 'emit', 'closeProject', data);
                 })
                 .nodeify(done);
         });
@@ -242,5 +345,129 @@ describe('WebSocket', function () {
                 .nodeify(done);
         });
 
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Branch related tests
+
+        it('should open an existing branch', function (done) {
+            openSocketIo()
+                .then(function (socket) {
+                    var data = {
+                        projectName: projectName,
+                        branchName: 'master'
+                    };
+
+                    return Q.ninvoke(socket, 'emit', 'openBranch', data);
+                })
+                .then(function (result) {
+                    expect(result.branchName).to.equal('master');
+                    expect(result.projectName).to.equal(projectName);
+                })
+                .nodeify(done);
+        });
+
+        it('should fail to open a non-existing branch', function (done) {
+            openSocketIo()
+                .then(function (socket) {
+                    var data = {
+                        projectName: projectName,
+                        branchName: 'branch_does_not_exist'
+                    };
+
+                    return Q.ninvoke(socket, 'emit', 'openBranch', data);
+                })
+                .then(function () {
+                    throw new Error('should have failed to openBranch');
+                })
+                .catch(function (err) {
+                    if (typeof err === 'string' && err.indexOf('does not exist') > -1) {
+                        return;
+                    } else {
+                        throw new Error('should have failed to openBranch');
+                    }
+                })
+                .nodeify(done);
+        });
+
+        it('should open and close branch', function (done) {
+            var socket,
+                data = {
+                    projectName: projectName,
+                    branchName: 'master'
+                };
+
+            openSocketIo()
+                .then(function (socket_) {
+                    socket = socket_;
+                    return Q.ninvoke(socket, 'emit', 'openBranch', data);
+                })
+                .then(function (result) {
+                    expect(result.branchName).to.equal('master');
+                    expect(result.projectName).to.equal(projectName);
+                    return Q.ninvoke(socket, 'emit', 'closeBranch', data);
+                })
+                .nodeify(done);
+        });
+
+        it('should getBranches', function (done) {
+            openSocketIo()
+                .then(function (socket) {
+                    var data = {
+                        projectName: projectName
+                    };
+
+                    return Q.ninvoke(socket, 'emit', 'getBranches', data);
+                })
+                .then(function (result) {
+                    expect(result).to.have.property('master');
+                })
+                .nodeify(done);
+        });
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Commit related tests
+
+        it('should getCommits', function (done) {
+            openSocketIo()
+                .then(function (socket) {
+                    var data = {
+                        projectName: projectName,
+                        before: (new Date()).getTime(), // current time
+                        number: 100
+                    };
+
+                    return Q.ninvoke(socket, 'emit', 'getCommits', data);
+                })
+                .then(function (result) {
+                    expect(result.length).to.equal(1);
+                    expect(result[0]).to.have.property('message');
+                    expect(result[0]).to.have.property('parents');
+                    expect(result[0]).to.have.property('root');
+                    expect(result[0]).to.have.property('time');
+                    expect(result[0]).to.have.property('type');
+                    expect(result[0]).to.have.property('updater');
+                    expect(result[0]).to.have.property('_id');
+                })
+                .nodeify(done);
+        });
+
+        it('should getLatestCommitData', function (done) {
+            openSocketIo()
+                .then(function (socket) {
+                    var data = {
+                        projectName: projectName,
+                        branchName: 'master'
+                    };
+
+                    return Q.ninvoke(socket, 'emit', 'getLatestCommitData', data);
+                })
+                .then(function (result) {
+                    expect(result.projectName).to.equal(projectName);
+                    expect(result.branchName).to.equal('master');
+                    expect(result).to.have.property('commitObject');
+                    expect(result).to.have.property('coreObjects');
+                })
+                .nodeify(done);
+        });
     });
 });
