0988a3b7c73b7d3cb503b7beb8b0495585abbb77
pmeijer <patrik85@isis.vanderbilt.edu>
2015-06-15 14:54:56 -0500

    Test for BRANCH_HASH_UPDATED and queued commits.

--- test-karma/client/js/branchstatus.spec.js
+++ test-karma/client/js/branchstatus.spec.js
@@ -4,7 +4,7 @@
  * @author pmeijer / https://github.com/pmeijer
  */
 
-describe('branch status', function () {
+describe.only('branch status', function () {
     'use strict';
     var client,
         storage,
@@ -261,6 +261,80 @@ describe('branch status', function () {
                                     }
                                 );
                                 client.setAttributes('', 'name', 'newRootyName', 'conflicting change');
+                            }
+                        );
+                    }
+                }
+
+                userGuid = client.addUI({}, nodeEventHandler);
+                client.updateTerritory(userGuid, {'': {children: 0}});
+            });
+        }
+    );
+
+    it('should go from AHEAD_NOT_SYNC to AHEAD_NOT_SYNC and increase commitQueue when making more changes.',
+        function (done) {
+            var branchName = 'sync_aheadSync_aheadNotSync_aheadNotSync',
+                currentHash,
+                prevStatus;
+
+            currentBranchName = branchName;
+
+            function removeHandler() {
+                client.removeEventListener(client.CONSTANTS.BRANCH_STATUS_CHANGED, eventHandler);
+            }
+
+            function eventHandler(__client, eventData) {
+                if (prevStatus === client.CONSTANTS.BRANCH_STATUS.SYNC) {
+                    prevStatus = eventData.status;
+                    expect(eventData.status).to.equal(client.CONSTANTS.BRANCH_STATUS.AHEAD_SYNC);
+                } else if (prevStatus === client.CONSTANTS.BRANCH_STATUS.AHEAD_SYNC) {
+                    prevStatus = eventData.status;
+                    expect(eventData.status).to.equal(client.CONSTANTS.BRANCH_STATUS.AHEAD_NOT_SYNC);
+                    expect(eventData.details.length).to.equal(1);
+                    client.setAttributes('', 'name', 'newRootyName2Test2', 'change when ahead not sync');
+                } else if (prevStatus === client.CONSTANTS.BRANCH_STATUS.AHEAD_NOT_SYNC) {
+                    expect(eventData.status).to.equal(client.CONSTANTS.BRANCH_STATUS.AHEAD_NOT_SYNC);
+                    expect(eventData.details.length).to.equal(2);
+                    removeHandler();
+                    currentBranchHash = client.getActiveCommitHash();
+                    done();
+                } else {
+                    removeHandler();
+                    done(new Error('Unexpected BranchStatus ' + eventData.status));
+                }
+            }
+
+            createSelectBranch(branchName, function (err) {
+                expect(err).to.equal(null);
+
+                prevStatus = client.getBranchStatus();
+                expect(prevStatus).to.equal(client.CONSTANTS.BRANCH_STATUS.SYNC);
+
+                currentHash = client.getActiveCommitHash();
+
+                client.addEventListener(client.CONSTANTS.BRANCH_STATUS_CHANGED, eventHandler);
+
+                var loaded = false,
+                    userGuid;
+
+                function nodeEventHandler(events) {
+                    if (loaded) {
+                        done(new Error('More than one event'));
+                    } else {
+                        loaded = true;
+                        expect(events.length).to.equal(2);
+                        client.removeUI(userGuid);
+                        changeRootNodeName('newConflictRootName', currentHash, 'externalChange',
+                            function (err, newHash) {
+                                expect(err).to.equal(null);
+                                project.setBranchHash(branchName, newHash, currentHash,
+                                    function (err, result) {
+                                        expect(err).to.equal(null);
+                                        expect(result.status).to.equal(client.CONSTANTS.STORAGE.SYNCH);
+                                    }
+                                );
+                                client.setAttributes('', 'name', 'newRootyNameTest2', 'conflicting change');
                             }
                         );
                     }
--- test-karma/client/js/client.spec.js
+++ test-karma/client/js/client.spec.js
@@ -1160,6 +1160,66 @@ describe('GME client', function () {
                 });
             });
         });
+
+        it('should raise BRANCH_HASH_UPDATED when the branch is updated', function (done) {
+            var branchName = 'b3',
+                triggered = false,
+                newHash,
+                handler = function (storage, eventData) {
+                    expect(triggered).to.equal(false);
+                    expect(eventData).to.not.equal(null);
+                    expect(eventData).to.include.keys('etype', 'projectName', 'branchName', 'newHash', 'oldHash');
+                    expect(eventData.etype).to.equal(client.CONSTANTS.STORAGE.BRANCH_HASH_UPDATED);
+                    expect(eventData.projectName).to.equal(projectName);
+                    expect(eventData.branchName).to.equal(branchName);
+                    expect(eventData.oldHash).to.equal(masterHash);
+                    expect(eventData.newHash).to.not.equal(masterHash);
+                    newHash = eventData.newHash;
+
+                    triggered = true;
+                    unwatch();
+                },
+                unwatch = function () {
+                    client.unwatchProject(projectName, handler, function (err) {
+                        expect(err).to.equal(null);
+
+                        client.deleteBranch(projectName, branchName, newHash, function (err) {
+                            expect(err).to.equal(null);
+                            done(err);
+                        });
+                    });
+                };
+
+            client.createBranch(projectName, branchName, masterHash, function (err) {
+                expect(err).to.equal(null);
+
+                client.selectBranch(branchName, null, function (err) {
+                    expect(err).to.equal(null);
+
+                    client.watchProject(projectName, handler, function (err) {
+                        expect(err).to.equal(null);
+                        var loaded = false,
+                            userGuid;
+
+                        function nodeEventHandler(events) {
+                            if (loaded) {
+                                done(new Error('More than one node event'));
+                            } else {
+                                loaded = true;
+                                expect(events.length).to.equal(2);
+                                client.removeUI(userGuid);
+
+                                client.setAttributes('', 'name', 'newRootName',
+                                    'should raise BRANCH_HASH_UPDATED when the branch is updated.');
+                            }
+                        }
+
+                        userGuid = client.addUI({}, nodeEventHandler);
+                        client.updateTerritory(userGuid, {'': {children: 0}});
+                    });
+                });
+            });
+        });
     });
 
     describe('client/node tests', function () {
