4cae595ee469d159424ebdac8a980d2ecf772340
kecso <kecsoloopy@gmail.com>
2015-08-27 09:17:15 -0500

    #504 adding success tests to check project creation API

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -591,7 +591,7 @@ function createAPI(app, mountPath, middlewareOpts) {
                 return Q.nfcall(middlewareOpts.workerManager.result,requestId);
             })
             .then(function(){
-                res.sendStatus(200);
+                res.sendStatus(204);
             })
             .catch(next); //TODO do we need special error handling???
     });
