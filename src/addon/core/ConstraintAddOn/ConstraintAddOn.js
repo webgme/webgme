fc04826919b2adc937a97a5324ac213abf29cee4
pmeijer <patrik85@isis.vanderbilt.edu>
2015-09-08 13:04:40 -0500

    #543 Create metarules.js and constraintchecker.js ConstraintAddOn reduced in size.

--- src/addon/core/ConstraintAddOn/ConstraintAddOn.js
+++ src/addon/core/ConstraintAddOn/ConstraintAddOn.js
@@ -1,23 +1,20 @@
 /*globals define*/
-/*jshint node:true, evil:true*/
+/*jshint node:true*/
 
 /**
  * @author kecso / https://github.com/kecso
  */
 
-define(['addon/AddOnBase'], function (AddOnBase) {
+define(['addon/AddOnBase', 'common/core/users/constraintchecker'], function (AddOnBase, constraint) {
 
     'use strict';
     var ConstraintAddOn = function (Core, storage, gmeConfig, logger, userId) {
         AddOnBase.call(this, Core, storage, gmeConfig, logger, userId);
+        this.constraintChecker = null;
     };
 
     ConstraintAddOn.prototype = Object.create(AddOnBase.prototype);
     ConstraintAddOn.prototype.constructor = ConstraintAddOn;
-
-    ConstraintAddOn.prototype.root = null;
-    ConstraintAddOn.prototype.contraints = {};
-    ConstraintAddOn.prototype.contraintsStorage = {};
 
     ConstraintAddOn.prototype.getName = function () {
         return 'ConstraintAddOn';
@@ -26,6 +23,7 @@ define(['addon/AddOnBase'], function (AddOnBase) {
     ConstraintAddOn.prototype.update = function (root, callback) {
         //TODO if we would like a continuous constraint checking we should use this function as well
         this.root = root;
+        this.constraintChecker.initialize(this.root, this.commit, constraint.TYPES.CUSTOM);
         callback(null);
     };
 
@@ -34,22 +32,24 @@ define(['addon/AddOnBase'], function (AddOnBase) {
         //several query will be available but the first is the simple run constraint
         switch (parameters.querytype) {
             case 'checkProject':
-                this.checkProject(callback);
+                self.constraintChecker.checkModel(self.root, callback);
                 break;
             case 'checkModel':
-                self.loadNode(parameters.path, function (err, node) {
+                self._loadNode(parameters.path, function (err, node) {
                     if (err) {
-                        return callback(err);
+                        callback(err);
+                        return;
                     }
-                    self.checkModel(node, callback);
+                    self.constraintChecker.checkModel(node, callback);
                 });
                 break;
             case 'checkNode':
-                self.loadNode(parameters.path, function (err, node) {
+                self._loadNode(parameters.path, function (err, node) {
                     if (err) {
-                        return callback(err);
+                        callback(err);
+                        return;
                     }
-                    self.checkNode(node, callback);
+                    self.constraintChecker.checkNode(node, callback);
                 });
                 break;
             default:
@@ -57,175 +57,21 @@ define(['addon/AddOnBase'], function (AddOnBase) {
         }
     };
 
-    /*ConstraintAddOn.prototype.start = function (parameters, callback) {
+    ConstraintAddOn.prototype.start = function (parameters, callback) {
         var self = this;
-        AddOnBase.prototype.start.call(this, parameters, function (err) {
+        AddOnBase.prototype.start.call(self, parameters, function (err) {
             if (err) {
-                return callback(err);
+                callback(err);
+            } else {
+                self.constraintChecker = new constraint.Checker(self.core, self.logger);
+                self.constraintChecker.initialize(self.root, self.commit, constraint.TYPES.CUSTOM);
+                callback(null);
             }
-            self.project.getBranchHash(self.branchName, '#hack', function (err, commitHash) {
-                if (err) {
-                    return callback(err);
-                }
-                self.project.loadObject(commitHash, function (err, commit) {
-                    if (!err && commit) {
-                        self.commit = commit;
-                        self.core.loadRoot(commit.root, function (err, root) {
-                            if (!err && root) {
-                                self.root = root;
-                                callback(null);
-                            } else {
-                                callback(err || 'cannot load initial root');
-                            }
-                        });
-                    } else {
-                        callback(err || 'cannot find the starting commit');
-                    }
-                });
-            });
-        });
-    };*/
-
-    ConstraintAddOn.prototype.stop = function (callback) {
-        AddOnBase.prototype.stop.call(this, callback);
-    };
-
-    ConstraintAddOn.prototype.checkProject = function (callback) {
-        this.checkModel(this.root, callback);
-    };
-    ConstraintAddOn.prototype.checkModel = function (root, callback) {
-        var self = this,
-            error = null,
-            message = {},
-            checkChild = function (node, cb) {
-                var needed = 2,
-                    children = [],
-                    nextChild = function (index) {
-                        if (index >= children.length) {
-                            return cb(error, message);
-                        }
-
-                        checkChild(children[index], function () {
-                            nextChild(index + 1);
-                        });
-                    },
-                    childrenLoaded = function () {
-                        needed = children.length;
-                        if (!needed || needed === 0) {
-                            return cb(error, message);
-                        }
-                        nextChild(0);
-                    };
-                self._checkNode(node, function (err, msg) {
-                    error = error || err;
-                    if (msg.hasViolation === true) {
-                        message.hasViolation = true;
-                    }
-                    message[self.core.getGuid(node)] = msg;
-                    if (--needed === 0) {
-                        childrenLoaded();
-                    }
-                });
-                self.core.loadChildren(node, function (err, c) {
-                    children = c || [];
-                    error = error || err;
-                    if (--needed === 0) {
-                        childrenLoaded();
-                    }
-                });
-            };
-
-        if (self.core.getPath(root) === self.core.getPath(self.core.getRoot(root))) {
-            message.info = 'project validation';
-        } else {
-            message.info = 'model [' + self.core.getPath(root) + '] validation';
-        }
-        message.commit = self.commit;
-        checkChild(root, callback);
-    };
-
-    ConstraintAddOn.prototype.checkNode = function (node, callback) {
-        var self = this,
-            message = {},
-            error = null;
-
-        self._checkNode(node, function (err, msg) {
-            error = error || err;
-            message[self.core.getGuid(node)] = msg;
-            if (msg) {
-                message.info = 'node [' + (self.core.getAttribute(node, 'name') || '') + '] validation';
-                //TODO what should be the proper identification
-                message.commit = self.commit;
-                message.hasViolation = msg.hasViolation === true;
-            }
-            callback(error, message);
         });
     };
 
-    ConstraintAddOn.prototype._checkNode = function (node, callback) {
-        var self = this,
-            message = {},
-            error = null,
-            names = self.core.getConstraintNames(node),
-            needed = names.length,
-            i,
-            check = function (name) {
-                self.executeConstraint(node, name, function (err, msg) {
-                    error = error || err;
-                    message[name] = msg;
-
-                    if (err) {
-                        message.hasViolation = true;
-                    }
-                    if (msg) {
-                        if (msg.hasViolation === true) {
-                            message.hasViolation = true;
-                        }
-                    }
-
-                    if (--needed === 0) {
-                        callback(error, message);
-                    }
-                });
-            };
-
-        message.info = 'node [' + self.core.getPath(node) + '] validation';
-        message.commit = self.commit;
-        message._path = self.core.getPath(node);
-        message._name = self.core.getAttribute(node, 'name') || 'N/A';
-        if (needed > 0) {
-            for (i = 0; i < names.length; i++) {
-                check(names[i]);
-            }
-        } else {
-            callback(error, message);
-        }
-    };
-
-    ConstraintAddOn.prototype.loadNode = function (path, callback) {
+    ConstraintAddOn.prototype._loadNode = function (path, callback) {
         this.core.loadByPath(this.root, path, callback);
-    };
-
-    ConstraintAddOn.prototype.executeConstraint = function (node, name, callback) {
-        var self = this,
-            script = self.core.getConstraint(node, name).script;
-
-        if (!self.contraints[script]) {
-            var a = '';
-            eval('a = ' + script + ';');
-            self.contraints[script] = function (core, node, callback) {
-                try {
-                    a(core, node, callback);
-                } catch (e) {
-                    callback(null, {
-                        hasViolation: true,
-                        message: 'Exception was thrown during constraint execution:\n' + e.toString()
-                    });
-                }
-            };
-            self.contraintsStorage[script] = {};
-        }
-        self.contraints[script].call(self.contraintsStorage[script], self.core, node, callback);
     };
 
     return ConstraintAddOn;
---
+++ src/common/core/users/constraintchecker.js
@@ -0,0 +1,238 @@
+/*globals define*/
+/*jshint node:true, browser: true, evil:true*/
+/**
+ * @author pmeijer / https://github.com/pmeijer
+ */
+
+define(['common/core/users/metarules', 'q'], function (metaRules, Q) {
+    'use strict';
+
+    var CONSTRAINT_TYPES = {
+        META: 'META_RULES',
+        CUSTOM: 'CUSTOM_CONSTRAINTS',
+        BOTH: 'BOTH'
+    };
+
+    /**
+     * @param {object} core
+     * @param {object} mainLogger
+     * @constructor
+     */
+    function ConstraintChecker(core, mainLogger) {
+        this.core = core;
+        this.logger = mainLogger.fork('ConstraintChecker');
+
+        this.customContraints = {};
+        this.customContraintsStorage = {};
+
+        // Set after initialize
+        this.initialized = false;
+
+        this.rootNode = null;
+        this.type = null;
+        this.commitHash = null;
+    }
+
+    ConstraintChecker.prototype.initialize = function (rootNode, commitHash, constraintType) {
+        this.rootNode = rootNode;
+        this.logger.debug('ConstraintChecker constraintType', constraintType);
+        this.type = constraintType || CONSTRAINT_TYPES.META;
+        this.commitHash = commitHash;
+        this.initialized = true;
+    };
+
+    ConstraintChecker.prototype._checkNode = function (node, callback) {
+        var self = this,
+            deferred = Q.defer(),
+        //TODO: These messages need some sort of type/class!!
+            message = {
+                info: 'node [' + self.core.getPath(node) + '] validation',
+                _path: self.core.getPath(node),
+                _name: self.core.getAttribute(node, 'name') || 'N/A',
+                commit: self.commitHash,
+                hasViolation: false
+            },
+            promises = [],
+            customNames,
+            checkCustom = function (name) {
+                return self._executeCustomConstraint(node, name);
+            };
+
+        if (self.type === CONSTRAINT_TYPES.META) {
+            promises.push(metaRules(self.core, node));
+            customNames = [];
+        } else if (self.type === CONSTRAINT_TYPES.CUSTOM) {
+            customNames = self.core.getConstraintNames(node);
+            promises = customNames.map(checkCustom);
+        } else if (self.type === CONSTRAINT_TYPES.BOTH) {
+            customNames = self.core.getConstraintNames(node);
+            promises = customNames.map(checkCustom);
+            promises.push(metaRules(self.core, node));
+        } else {
+            deferred.reject(new Error('Unknown CONSTRAINT_TYPE' + self.type));
+        }
+
+        Q.allSettled(promises)
+            .then(function (results) {
+                var customCountDown = customNames.length;
+                results.map(function (result) {
+                    var msg = {
+                        hasViolation: false,
+                        message: ''
+                    };
+                    customCountDown -= 1;
+                    if (result.state === 'rejected') {
+                        msg.message = result.reason instanceof Error ? result.reason.message : result.reason;
+                        msg.hasViolation = true;
+                    } else if (result.state === 'resolved') {
+                        msg.message = result.value.message;
+                        msg.hasViolation = result.value.hasViolation;
+                    } else {
+                        msg.message = 'Unknown Q promise state ' + result.state;
+                        msg.hasViolation = true;
+                    }
+
+                    if (customCountDown > -1) {
+                        message[customNames[customCountDown]] = msg;
+                    } else {
+                        // This is the meta constraint.
+                        message.META_RULES = msg;
+                    }
+
+                    if (msg.hasViolation) {
+                        // Propagate the violation
+                        message.hasViolation = true;
+                    }
+                });
+
+                deferred.resolve(message);
+            })
+            .catch(deferred.reject);
+
+        return deferred.promise.nodeify(callback);
+    };
+
+    ConstraintChecker.prototype._executeCustomConstraint = function (node, name, callback) {
+        var self = this,
+            deferred = Q.defer(),
+            script = self.core.getConstraint(node, name).script;
+
+        if (!self.customContraints[script]) {
+            var a = '';
+            eval('a = ' + script + ';');
+            self.customContraints[script] = function (core, node, constraintCallback) {
+                try {
+                    a(core, node, constraintCallback);
+                } catch (e) {
+                    constraintCallback('Exception was thrown during "' + name + '" constraint execution:\n' +
+                        e.toString());
+                }
+            };
+            self.customContraintsStorage[script] = {};
+        }
+
+        self.customContraints[script].call(self.customContraintsStorage[script], self.core, node,
+            function (err, result) {
+                if (err) {
+                    deferred.reject(err);
+                } else {
+                    deferred.resolve(result);
+                }
+            });
+
+        return deferred.promise.nodeify(callback);
+    };
+
+    ConstraintChecker.prototype.checkModel = function (node, callback) {
+        var self = this,
+            deferred = Q.defer(),
+            error = null,
+            message = {
+                info: '',
+                commit: self.commitHash,
+                hasViolation: false
+            };
+
+        function checkChild(node, cb) {
+            var needed = 2,
+                children = [],
+                nextChild = function (index) {
+                    if (index >= children.length) {
+                        return cb(error, message);
+                    }
+
+                    checkChild(children[index], function () {
+                        nextChild(index + 1);
+                    });
+                },
+                childrenLoaded = function () {
+                    needed = children.length;
+                    if (!needed || needed === 0) {
+                        return cb(error, message);
+                    }
+                    nextChild(0);
+                };
+            self._checkNode(node, function (err, msg) {
+                error = error || err;
+                if (msg.hasViolation === true) {
+                    message.hasViolation = true;
+                }
+                message[self.core.getGuid(node)] = msg;
+                if (--needed === 0) {
+                    childrenLoaded();
+                }
+            });
+            self.core.loadChildren(node, function (err, c) {
+                children = c || [];
+                error = error || err;
+                if (--needed === 0) {
+                    childrenLoaded();
+                }
+            });
+        }
+
+        if (self.core.getPath(node) === self.core.getPath(self.core.getRoot(node))) {
+            message.info = 'project validation';
+        } else {
+            message.info = 'model [' + self.core.getPath(node) + '] validation';
+        }
+
+        checkChild(node, function (err, message_) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(message_);
+            }
+        });
+
+        return deferred.promise.nodeify(callback);
+    };
+
+    ConstraintChecker.prototype.checkNode = function (node, callback) {
+        var self = this,
+            deferred = Q.defer(),
+            //TODO what should be the proper identification
+            message = {
+                info: 'node [' + (self.core.getAttribute(node, 'name') || '') + '] validation',
+                commit: self.commitHash,
+                hasViolation: false
+            };
+
+        self._checkNode(node)
+            .then(function (result) {
+                message[self.core.getGuid(node)] = result;
+                message.hasViolation = result.hasViolation;
+                deferred.resolve(result);
+            })
+            .catch(function (err) {
+                deferred.reject(err);
+            });
+
+        return deferred.promise.nodeify(callback);
+    };
+
+    return {
+        Checker: ConstraintChecker,
+        TYPES: CONSTRAINT_TYPES
+    };
+});
