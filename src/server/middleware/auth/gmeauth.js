248c86acb48974bfef115318b94dbc0e636367ba
Patrik Meijer <patrik85@isis.vanderbilt.edu>
2015-10-08 15:52:53 -0500

    Use bcryptjs rather than brcypt to avoid build.

--- src/server/middleware/auth/gmeauth.js
+++ src/server/middleware/auth/gmeauth.js
@@ -8,7 +8,8 @@
 
 var Mongodb = require('mongodb'),
     Q = require('q'),
-    bcrypt = require('bcrypt'),
+    //bcrypt = require('bcrypt'), include bcrypt and uncomment this line for faster encryption/decryption.
+    bcrypt = require('bcryptjs'),
 
     GUID = requireJS('common/util/guid'),
 
