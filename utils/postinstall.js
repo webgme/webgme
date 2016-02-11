5dd27c3589effade6c5b90bff5a884a085774244
Patrik Meijer <patrik85@isis.vanderbilt.edu>
2016-02-11 11:42:46 -0600

    #830 Generate jsdocs as part of post-install

---
+++ utils/postinstall.js
@@ -0,0 +1,24 @@
+/*jshint node:true*/
+/**
+ * Arguments passed to this script are propagated to jsdoc.
+ * @author pmeijer / https://github.com/pmeijer
+ */
+
+var child_process = require('child_process'),
+    //isWin = /^win/.test(process.platform);
+    webgmeBuild = require('./build/webgme.classes/build_classes.js');
+
+console.log('Generating webgme.classes.build.js ...');
+
+webgmeBuild(function (err, data) {
+    if (err) {
+        console.error('Failed generating webgme.classes.build.js!', err);
+    } else {
+        //console.log(data);
+        console.log('Done with webgme.classes.build.js!');
+    }
+});
+
+console.log('Generating webgme docs/source ...');
+require('jsdoc/jsdoc');
+console.log('Done with webgme docs/source!');
