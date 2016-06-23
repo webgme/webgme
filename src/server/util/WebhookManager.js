9223c92ea11af78124b86461f108ce64b1674a78
Tamas Kecskes <tkecskes@isis.vanderbilt.edu>
2016-06-23 16:12:08 -0500

    #1031 Used fork instead of spawn for better interoperability.

--- src/server/util/WebhookManager.js
+++ src/server/util/WebhookManager.js
@@ -3,7 +3,7 @@
  */
 var MessageSender = require('webhook-manager/src/hookMessager'),
     CONSTANTS = requireJS('common/storage/constants'),
-    spawn = require('child_process').spawn,
+    fork = require('child_process').fork,
     path = require('path'),
     Q = require('q');
 
@@ -81,39 +81,41 @@ function memoryManager(storage, logger, gmeConfig) {
 }
 
 function redisManager(logger, gmeConfig) {
-    var managerProc = null,
+    var managerProcess = null,
         initialized = false;
 
     function start(callback) {
         var deferred = Q.defer(),
-            spawnParameters = [require.resolve('webhook-manager'), gmeConfig.mongo.uri];
+            forkParameters = [gmeConfig.mongo.uri];
 
-        if (managerProc === null) {
+        if (managerProcess === null) {
             if (gmeConfig.socketIO.adapter.options && gmeConfig.socketIO.adapter.options.uri) {
-                spawnParameters.push('redis://' + gmeConfig.socketIO.adapter.options.uri);
+                forkParameters.push('redis://' + gmeConfig.socketIO.adapter.options.uri);
             }
-            managerProc = spawn('node', spawnParameters);
+            managerProcess = fork(require.resolve('webhook-manager'), forkParameters, {silent: true});
 
-            managerProc.stdout.on('data', function (data) {
+            managerProcess.on('exit', function (code, signal) {
+                if (code !== 0 && signal !== 'SIGINT') {
+                    logger.warn('manager has exited abnormally with code ' + code +
+                        ', signal', signal);
+                }
+
+                if (!initialized) {
+                    deferred.reject(new Error('manager exited without initialization'));
+                }
+            });
+
+            managerProcess.stdout.on('data', function (data) {
                 if (!initialized) {
                     initialized = true;
                     deferred.resolve();
                 }
             });
 
-            managerProc.stderr.on('data', function (data) {
+            managerProcess.stderr.on('data', function (data) {
                 logger.error(data.toString('utf-8'));
             });
 
-            managerProc.on('close', function (code) {
-                if (initialized) {
-                    managerProc = null;
-                    logger.info('webhookManager process has been exited with code:', code);
-                    initialized = false;
-                } else {
-                    deferred.reject(new Error('failed to start webhook manager process!'));
-                }
-            });
         } else {
             deferred.resolve();
         }
@@ -122,8 +124,8 @@ function redisManager(logger, gmeConfig) {
 
     function stop(callback) {
         var deferred = Q.defer();
-        managerProc.kill();
-        managerProc = null;
+        managerProcess.kill('SIGINT');
+        managerProcess = null;
 
         deferred.resolve();
         return deferred.promise.nodeify(callback);
