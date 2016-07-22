8d113d782382d0fc53df7558928a153990006ca8
Patrik Meijer <patrik85@isis.vanderbilt.edu>
2016-07-22 11:28:23 -0500

    WIP set the Content-Length when running under node

--- src/common/blob/BlobClient.js
+++ src/common/blob/BlobClient.js
@@ -178,9 +178,11 @@ define([
         if (this.webgmeToken) {
             req.set('Authorization', 'Bearer ' + this.webgmeToken);
         }
-        // if (typeof data !== 'string' && !(data instanceof String)) {
-        //     req.set('Content-Length', contentLength);
-        // }
+
+        if (typeof data !== 'string' && !(data instanceof String) && typeof window === 'undefined') {
+            req.set('Content-Length', contentLength);
+        }
+
         req.set('Content-Type', 'application/octet-stream')
             .send(data)
             .end(function (err, res) {
@@ -222,10 +224,10 @@ define([
 
         if (typeof window === 'undefined') {
             req.agent(this.keepaliveAgent);
+            req.set('Content-Length', contentLength);
         }
 
         req.set('Content-Type', 'application/octet-stream')
-            //.set('Content-Length', contentLength)
             .send(blob)
             .end(function (err, res) {
                 if (err || res.status > 399) {
