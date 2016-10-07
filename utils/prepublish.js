fe8b8791dfe3f8f3d5a342b0107815d9c7ee2a6c
Patrik Meijer <patrik85@isis.vanderbilt.edu>
2016-10-07 11:38:14 -0500

    Add generation of REST api docs to prepublish (#1199)
    * WIP Gen REST API docs in prepublish
    
    * WIP add gitignore to docs/REST
    
    * WIP Remove commented out code
    
    * WIP Generate API docs during tests.

--- utils/prepublish.js
+++ utils/prepublish.js
@@ -7,11 +7,27 @@
 'use strict';
 
 function prepublish() {
+    var raml2html = require('raml2html'),
+        path = require('path'),
+        fs = require('fs'),
+        configWithDefaultTemplates = raml2html.getDefaultConfig();
+
+    console.log('Generating REST API docs ...');
+
+    raml2html.render(path.join(__dirname, '..', 'src', 'server', 'api', 'webgme-api.raml'), configWithDefaultTemplates)
+        .then(function (indexHtml) {
+            fs.writeFileSync(path.join(__dirname, '..', 'docs', 'REST', 'index.html'), indexHtml);
+            console.log('Done with REST API docs!');
+        }, function (err) {
+            console.error('Failed generating REST API docs!', err);
+        });
+
     if (process.env.TEST_FOLDER) {
-        console.warn('TEST_FOLDER environment valiable is set, skipping post install script.');
+        console.warn('TEST_FOLDER environment variable is set, skipping distribution scripts.');
     } else {
         var webgmeBuild = require('./build/webgme.classes/build_classes.js'),
             webgmeDist = require('./build/dist/build.js');
+
 
         console.log('Generating webgme.classes.build.js ...');
 
