045d68daf288c13fe33c6382b19c41e161bdecf9
Patrik Meijer <patrik85@isis.vanderbilt.edu>
2017-02-15 10:09:38 -0600

    WIP add failing test-cases w.r.t. to children and symmetry.

--- test/common/core/corediff.scenarios.spec.js
+++ test/common/core/corediff.scenarios.spec.js
@@ -88,7 +88,7 @@ describe.skip('corediff scenarios', function () {
             });
     }
 
-    it('should assign a new relid when child', function (done) {
+    it('should assign a new relid when child created in both trees', function (done) {
         var originRoot,
             basePath,
             instancePath;
@@ -120,30 +120,23 @@ describe.skip('corediff scenarios', function () {
                 // We've loaded two trees from the same rootHash
                 // now let's make changes.
 
-                // core.setAttribute(trees[0].fco, 'name', 'name0');
-                // core.setAttribute(trees[1].fco, 'name', 'name1');
-
-                //core.setAttribute(
-                    core.createNode({
+                core.createNode({
                     parent: trees[0][basePath],
                     base: trees[0].fco,
-                    relid: 'conflictRelid'});
-                //}), 'name', 'baseChild');
+                    relid: 'conflictRelid'
+                });
 
-                //core.setAttribute(
-                    core.createNode({
+                core.createNode({
                     parent: trees[1][basePath],
                     base: trees[1].fco,
-                    relid: 'conflictRelid'});
-                //}), 'name', 'instanceChild');
-
-                //core.setPointer(trees[0][basePath], 'pppp', trees[0].fco);
+                    relid: 'conflictRelid'
+                });
 
 
                 // Save to ensure the added nodes are persisted.
                 return Q.all([
-                    save(trees[1].root),
-                    save(trees[0].root)
+                    save(trees[0].root),
+                    save(trees[1].root)
                 ])
                     .then(function () {
                         return Q.all([
@@ -155,29 +148,287 @@ describe.skip('corediff scenarios', function () {
             .then(function (diffs) {
                 var concatChanges = core.tryToConcatChanges(diffs[0], diffs[1]);
 
-                console.log(diffs[0]);
-
                 expect(concatChanges.items.length).to.equal(0); // No conflicts detected
-                //console.log(concatChanges.merge);
+
                 return core.applyTreeDiff(originRoot, concatChanges.merge);
             })
-            // .then(function () {
-            //     return save(originRoot);
-            // })
-            // .then(function (newRootHash) {
-            //     return loadRootAndFCO(newRootHash);
-            // })
-            .then(function (r) {
-                //console.log(originRoot.data.ovr);
+            .then(function () {
                 return core.loadSubTree(originRoot);
             })
             .then(function (st) {
-                st.forEach(function (node) {
-                    // console.log(core.getPath(node), core.getAttribute(node, 'someAttr'));
-                    if (core.getParent(node) && core.getPath(core.getParent(node)) === instancePath) {
-                        console.log('\n', core.getPath(node), '-', core.getBase(node) ? core.getPath(core.getBase(node)) : null);
-                    }
+                // Root, fco, base, instance, 2 children
+                expect(st.length).to.equal(6);
+            })
+            .nodeify(done);
+    });
+
+    it('should assign a new relid when child created in one base and one instance', function (done) {
+        var originRoot,
+            basePath,
+            instancePath;
+
+        loadRootAndFCO(context.rootHash)
+            .then(function (r) {
+                var base = core.createNode({
+                        parent: r.root,
+                        base: r.fco
+                    }),
+                    instance = core.createNode({
+                        parent: r.root,
+                        base: base
+                    });
+
+                basePath = core.getPath(base);
+                instancePath = core.getPath(instance);
+
+                originRoot = r.root;
+                return save(r.root);
+            })
+            .then(function (rootHash) {
+                return Q.all([
+                    loadRootAndFCO(rootHash, [basePath, instancePath]),
+                    loadRootAndFCO(rootHash, [basePath, instancePath]),
+                ]);
+            })
+            .then(function (trees) {
+                // We've loaded two trees from the same rootHash
+                // now let's make changes.
+
+                core.createNode({
+                    parent: trees[0][basePath],
+                    base: trees[0].fco,
+                    relid: 'conflictRelid'
                 });
+
+                core.createNode({
+                    parent: trees[1][instancePath],
+                    base: trees[1].fco,
+                    relid: 'conflictRelid'
+                });
+
+
+                // Save to ensure the added nodes are persisted.
+                return Q.all([
+                    save(trees[0].root),
+                    save(trees[1].root)
+                ])
+                    .then(function () {
+                        return Q.all([
+                            core.generateTreeDiff(originRoot, trees[0].root),
+                            core.generateTreeDiff(originRoot, trees[1].root),
+                        ]);
+                    });
+            })
+            .then(function (diffs) {
+                var concatChanges = core.tryToConcatChanges(diffs[0], diffs[1]);
+
+                expect(concatChanges.items.length).to.equal(0); // No conflicts detected
+                return core.applyTreeDiff(originRoot, concatChanges.merge);
+            })
+            .then(function () {
+                return core.loadSubTree(originRoot);
+            })
+            .then(function (st) {
+                // Root, fco, base, instance, 3 children
+                expect(st.length).to.equal(7);
+            })
+            .nodeify(done);
+    });
+
+    it('should assign a new relids when child created in bases and modified in instances', function (done) {
+        var originRoot,
+            basePath,
+            instancePath;
+
+        loadRootAndFCO(context.rootHash)
+            .then(function (r) {
+                var base = core.createNode({
+                        parent: r.root,
+                        base: r.fco
+                    }),
+                    instance = core.createNode({
+                        parent: r.root,
+                        base: base
+                    });
+
+                basePath = core.getPath(base);
+                instancePath = core.getPath(instance);
+
+                originRoot = r.root;
+                return save(r.root);
+            })
+            .then(function (rootHash) {
+                return Q.all([
+                    loadRootAndFCO(rootHash, [basePath, instancePath]),
+                    loadRootAndFCO(rootHash, [basePath, instancePath]),
+                ]);
+            })
+            .then(function (trees) {
+                // We've loaded two trees from the same rootHash
+                // now let's make changes.
+
+                core.createNode({
+                    parent: trees[0][basePath],
+                    base: trees[0].fco,
+                    relid: 'conflictRelid'
+                });
+
+                core.createNode({
+                    parent: trees[1][basePath],
+                    base: trees[1].fco,
+                    relid: 'conflictRelid'
+                });
+
+
+                // Save to ensure the added nodes are persisted.
+                return Q.all([
+                    core.loadChildren(trees[0][instancePath]),
+                    core.loadChildren(trees[1][instancePath]),
+                ])
+                    .then(function (children) {
+                        expect(children[0].length).to.equal(1);
+                        expect(children[1].length).to.equal(1);
+
+                        core.setAttribute(children[0][0], 'name', 'instanceChildWithData0');
+                        core.setAttribute(children[1][0], 'name', 'instanceChildWithData1');
+                        return Q.all([
+                            save(trees[0].root),
+                            save(trees[1].root)
+                        ]);
+                    })
+                    .then(function () {
+                        return Q.all([
+                            core.generateTreeDiff(originRoot, trees[0].root),
+                            core.generateTreeDiff(originRoot, trees[1].root),
+                        ]);
+                    });
+            })
+            .then(function (diffs) {
+                var concatChanges = core.tryToConcatChanges(diffs[0], diffs[1]);
+
+                expect(concatChanges.items.length).to.equal(0); // No conflicts detected
+                return core.applyTreeDiff(originRoot, concatChanges.merge);
+            })
+            .then(function () {
+                return core.loadSubTree(originRoot);
+            })
+            .then(function (st) {
+                // Root, fco, base, instance, 4 children
+                expect(st.length).to.equal(8);
+            })
+            .nodeify(done);
+    });
+
+    it('should give conflict when del base in one tree and mod instance in other', function (done) {
+        var originRoot,
+            basePath,
+            instancePath;
+
+        loadRootAndFCO(context.rootHash)
+            .then(function (r) {
+                var base = core.createNode({
+                        parent: r.root,
+                        base: r.fco
+                    }),
+                    instance = core.createNode({
+                        parent: r.root,
+                        base: base
+                    });
+
+                basePath = core.getPath(base);
+                instancePath = core.getPath(instance);
+
+                originRoot = r.root;
+                return save(r.root);
+            })
+            .then(function (rootHash) {
+                return Q.all([
+                    loadRootAndFCO(rootHash, [basePath, instancePath]),
+                    loadRootAndFCO(rootHash, [basePath, instancePath]),
+                ]);
+            })
+            .then(function (trees) {
+                // We've loaded two trees from the same rootHash
+                // now let's make changes.
+
+                core.deleteNode(trees[0][basePath]);
+
+                core.setAttribute(trees[1][instancePath], 'name', 'newName');
+
+
+                // Save to ensure the added nodes are persisted.
+                return Q.all([
+                    save(trees[0].root),
+                    save(trees[1].root)
+                ])
+                    .then(function () {
+                        return Q.all([
+                            core.generateTreeDiff(originRoot, trees[0].root),
+                            core.generateTreeDiff(originRoot, trees[1].root),
+                        ]);
+                    });
+            })
+            .then(function (diffs) {
+                var concatChanges = core.tryToConcatChanges(diffs[0], diffs[1]);
+
+                expect(concatChanges.items.length).to.equal(1);
+            })
+            .nodeify(done);
+    });
+
+    it('should give conflict when del base in one tree and mod instance in other (reverse)', function (done) {
+        var originRoot,
+            basePath,
+            instancePath;
+
+        loadRootAndFCO(context.rootHash)
+            .then(function (r) {
+                var base = core.createNode({
+                        parent: r.root,
+                        base: r.fco
+                    }),
+                    instance = core.createNode({
+                        parent: r.root,
+                        base: base
+                    });
+
+                basePath = core.getPath(base);
+                instancePath = core.getPath(instance);
+
+                originRoot = r.root;
+                return save(r.root);
+            })
+            .then(function (rootHash) {
+                return Q.all([
+                    loadRootAndFCO(rootHash, [basePath, instancePath]),
+                    loadRootAndFCO(rootHash, [basePath, instancePath]),
+                ]);
+            })
+            .then(function (trees) {
+                // We've loaded two trees from the same rootHash
+                // now let's make changes.
+
+                core.deleteNode(trees[1][basePath]);
+
+                core.setAttribute(trees[0][instancePath], 'name', 'newName');
+
+
+                // Save to ensure the added nodes are persisted.
+                return Q.all([
+                    save(trees[0].root),
+                    save(trees[1].root)
+                ])
+                    .then(function () {
+                        return Q.all([
+                            core.generateTreeDiff(originRoot, trees[0].root),
+                            core.generateTreeDiff(originRoot, trees[1].root),
+                        ]);
+                    });
+            })
+            .then(function (diffs) {
+                var concatChanges = core.tryToConcatChanges(diffs[0], diffs[1]);
+
+                expect(concatChanges.items.length).to.equal(1);
             })
             .nodeify(done);
     });
