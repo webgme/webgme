59422e1525395c8a714d40c9f3710faf7f8a3d48
kecso <tkecskes@isis.vanderbilt.edu>
2017-05-11 09:08:06 -0500

    WIP - implementing utility functions

--- src/client/js/Utils/svg.js
+++ src/client/js/Utils/svg.js
@@ -7,17 +7,40 @@ define(['common/util/ejs'], function (ejs) {
 
     var def = '<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><!-- Created with Method Draw - http://github.com/duopixel/Method-Draw/ --><g><title>background</title><rect fill="#fff" id="canvas_background" height="22" width="22" y="-1" x="-1"/><g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid"><rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/></g></g><g><title>Layer 1</title><ellipse ry="49" rx="43.5" id="svg_1" cy="104" cx="119" stroke-width="1.5" stroke="#000" fill="#fff"/><text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="24" id="svg_2" y="260" x="309.5" stroke-width="0" stroke="#000" fill="#000000">Hello World</text> <text font-weight="bold" xml:space="preserve" text-anchor="start" font-family="\'Courier New\', Courier, monospace" font-size="12" id="svg_3" y="13" x="4" stroke-width="0" stroke="#000" fill="#000000"><%=getAttribute("name")%></text></g></svg>';
 
-    function uri(clientNodeObj, registryId) {
-        var raw = clientNodeObj.getEditableRegistry(registryId) || '',
-            rendered = ejs.render(def, clientNodeObj),
-            xml = /*(new XMLSerializer()).serializeToString(rendered)*/ rendered,
-            encoded = "data:image/svg+xml;base64," + window.btoa(xml);
+    var SVG_CACHE = {};
 
-            console.log(rendered);
-        return encoded;
+    function isSvg(text) {
+        return $(text).is('svg');
+    }
+
+    function uri(clientNodeObj, registryId) {
+        var data = clientNodeObj.getEditableAttribute(registryId);
+
+        if (typeof data === 'string') {
+            if (isSvg(data)) {
+                data = ejs.render(data, clientNodeObj);
+                data = $(data).find('svg').first().html();
+                return "data:image/svg+xml;base64," + window.btoa(data);
+            } else {
+                return '/assets/DecoratorSVG/' + data;
+            }
+        }
+
+        return null;
     }
 
     function content(clientNodeObj, registryId) {
+        var data = clientNodeObj.getEditableAttribute(registryId);
+
+        if (typeof data === 'string') {
+            if (isSvg(data)) {
+                data = ejs.render(data, clientNodeObj);
+                return $(data).find('svg').first().html();
+            } else {
+                return '/assets/DecoratorSVG/' + data;
+            }
+        }
+
         return null;
     }
 
