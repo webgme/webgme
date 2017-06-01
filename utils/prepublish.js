6f42ea079c49aa7f84e03bed37ddbdd825608f6f
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-06-01 17:44:15 -0500

    Do not call bower from postinstall. Prepublish already installs them at prepublish.

--- utils/prepublish.js
+++ utils/prepublish.js
@@ -11,6 +11,7 @@ function prepublish(jsdocConfigPath) {
         path = require('path'),
         fs = require('fs'),
         childProcess = require('child_process'),
+        bower = require('bower'),
         configWithDefaultTemplates = raml2html.getDefaultConfig();
 
     console.log('Generating REST API docs ...');
@@ -29,35 +30,39 @@ function prepublish(jsdocConfigPath) {
         var webgmeBuild = require('./build/webgme.classes/build_classes.js'),
             webgmeDist = require('./build/dist/build.js');
 
-        console.log('Generating webgme.classes.build.js ...');
-
-        webgmeBuild(function (err, data) {
-            if (err) {
-                console.error('Failed generating webgme.classes.build.js!', err);
-            } else {
-                //console.log(data);
-                console.log('Done with webgme.classes.build.js!');
-            }
-        });
-
-        console.log('Generating webgme.dist.build.js ...');
-
-        webgmeDist(function (err, data) {
-            if (err) {
-                console.error('Failed generating webgme.dist.build.js!', err);
-            } else {
-                //console.log(data);
-                console.log('Done with webgme.dist.build.js!');
-            }
-        });
-
-        if (jsdocConfigPath !== false) {
-            console.log('Generating webgme docs/source ...');
-            childProcess.execFile(process.execPath, [
-                path.join(__dirname, './jsdoc_build.js'),
-                '-c', jsdocConfigPath || './jsdoc_conf.json']);
-            console.log('Done with webgme docs/source!');
-        }
+        console.log('Installing bower components...');
+        bower.commands.install(undefined, undefined, {cwd: process.cwd()})
+            .on('end', function (/*installed*/) {
+                console.log('Done!');
+                console.log('Generating webgme.classes.build.js ...');
+                webgmeBuild(function (err/*, data*/) {
+                    if (err) {
+                        console.error('Failed generating webgme.classes.build.js!', err);
+                    } else {
+                        //console.log(data);
+                        console.log('Done!');
+                        console.log('Generating webgme.dist.build.js ...');
+                        webgmeDist(function (err/*, data*/) {
+                            if (err) {
+                                console.error('Failed generating webgme.dist.build.js!', err);
+                            } else {
+                                //console.log(data);
+                                console.log('Done!');
+                                if (jsdocConfigPath !== false) {
+                                    console.log('Generating webgme source code documentation ...');
+                                    childProcess.execFile(process.execPath, [
+                                        path.join(__dirname, './jsdoc_build.js'),
+                                        '-c', jsdocConfigPath || './jsdoc_conf.json']);
+                                    console.log('Done!');
+                                }
+                            }
+                        });
+                    }
+                });
+            })
+            .on('error', function (err) {
+                console.error(err);
+            });
     }
 }
 
