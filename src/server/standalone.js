9dbfa283a733cfd2f44106154341eea12b2b9ee7
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-06-19 11:05:26 -0500

    WIP add config option for user management page.

--- src/server/standalone.js
+++ src/server/standalone.js
@@ -539,8 +539,9 @@ function StandAloneServer(gmeConfig) {
     }
 
     function mountUserManagementPage() {
-        var userComponent = require('webgme-user-management-page');
+        var userComponent = require(gmeConfig.authentication.userManagementPage);
         userComponent.initialize(middlewareOpts);
+        routeComponents.push(userComponent);
         __app.use('/profile', userComponent.router);
     }
 
