a9675ca8c0b7cb2b18636471ec23dca02df0f4b2
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-12-20 14:29:05 -0600

    FIXES #1658 expose overridefromenv

---
+++ config/overridefromenv.js
@@ -0,0 +1,8 @@
+/*jshint node: true*/
+/**
+ * This simply acts as a proxy for overridefrom env in webgme-engine.
+ * This needs to be included and called inside the config/index.js of the repository using webgme.
+ *
+ * @author pmeijer / https://github.com/pmeijer
+ */
+module.exports = require('webgme-engine/config/overridefromenv');
