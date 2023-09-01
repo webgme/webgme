d854bf86f202f10be426e8597d2b717ae3199819
Patrik Meijer <6518904+pmeijer@users.noreply.github.com>
2023-09-01 11:19:35 -0500

    webgme-engine v2.26.0 and node_modules bump (#1776)
    * Update webgme-engine to 2.26.0 (branch for now)
    
    * Bump node_modules and remove require-unached
    
    * remove config tmp
    
    * Point to new webgme-engine
    
    ---------
    
    Co-authored-by: Patrik Meijer <patrik.meijer@vanderbilt.edu>

--- config/components.default.js
+++ config/components.default.js
@@ -4,8 +4,6 @@
  * @author pmeijer / https://github.com/pmeijer
  */
 
-// You need to add require-uncached as node-module if you wanna use the same approach.
-var requireUncached = require('require-uncached'),
-    components = requireUncached('./components.json');
+var components = require('./components.json');
 
 module.exports = components;
